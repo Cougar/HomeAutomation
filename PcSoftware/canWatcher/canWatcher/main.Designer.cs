510f9e51b133bd88c552dfd24c7946a9231a7912
johboh <johboh@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2006-10-22 15:19:36 +0000

    canWatcher: Tog bort en feature
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@44 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/canWatcher/canWatcher/main.Designer.cs
+++ PcSoftware/canWatcher/canWatcher/main.Designer.cs
@@ -29,10 +29,10 @@ namespace canWatcher
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
             this.dg_incomming = new System.Windows.Forms.DataGridView();
             this.ident = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -65,13 +65,13 @@ namespace canWatcher
             this.cmd_edit_message = new System.Windows.Forms.ToolStripButton();
             this.cmd_delete_message = new System.Windows.Forms.ToolStripButton();
             this.cmd_send_message = new System.Windows.Forms.ToolStripButton();
+            this.cmd_settings = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.strip_status = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.lab_status = new System.Windows.Forms.ToolStripStatusLabel();
             this.label3 = new System.Windows.Forms.Label();
             this.cmd_send = new System.Windows.Forms.Button();
-            this.cmd_settings = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             ((System.ComponentModel.ISupportInitialize)(this.dg_incomming)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dg_outgoing)).BeginInit();
             this.strip_tool.SuspendLayout();
@@ -83,8 +83,8 @@ namespace canWatcher
             this.dg_incomming.AllowUserToAddRows = false;
             this.dg_incomming.AllowUserToDeleteRows = false;
             this.dg_incomming.AllowUserToResizeRows = false;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.dg_incomming.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dg_incomming.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dg_incomming.BackgroundColor = System.Drawing.SystemColors.InactiveBorder;
             this.dg_incomming.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dg_incomming.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -96,15 +96,15 @@ namespace canWatcher
             this.count,
             this.hash,
             this.cmI});
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dg_incomming.DefaultCellStyle = dataGridViewCellStyle6;
-            this.dg_incomming.Location = new System.Drawing.Point(15, 25);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dg_incomming.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dg_incomming.Location = new System.Drawing.Point(15, 41);
             this.dg_incomming.MultiSelect = false;
             this.dg_incomming.Name = "dg_incomming";
             this.dg_incomming.Size = new System.Drawing.Size(600, 238);
@@ -159,7 +159,7 @@ namespace canWatcher
             // 
             // txt_debug_out
             // 
-            this.txt_debug_out.Location = new System.Drawing.Point(344, 526);
+            this.txt_debug_out.Location = new System.Drawing.Point(344, 542);
             this.txt_debug_out.Name = "txt_debug_out";
             this.txt_debug_out.Size = new System.Drawing.Size(202, 20);
             this.txt_debug_out.TabIndex = 9;
@@ -173,8 +173,8 @@ namespace canWatcher
             this.dg_outgoing.AllowUserToAddRows = false;
             this.dg_outgoing.AllowUserToDeleteRows = false;
             this.dg_outgoing.AllowUserToResizeRows = false;
-            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.dg_outgoing.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dg_outgoing.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
             this.dg_outgoing.BackgroundColor = System.Drawing.SystemColors.InactiveBorder;
             this.dg_outgoing.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dg_outgoing.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -186,15 +186,15 @@ namespace canWatcher
             this.dataGridViewTextBoxColumn5,
             this.dataGridViewTextBoxColumn6,
             this.cmO});
-            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle8.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dg_outgoing.DefaultCellStyle = dataGridViewCellStyle8;
-            this.dg_outgoing.Location = new System.Drawing.Point(15, 282);
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dg_outgoing.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dg_outgoing.Location = new System.Drawing.Point(15, 298);
             this.dg_outgoing.MultiSelect = false;
             this.dg_outgoing.Name = "dg_outgoing";
             this.dg_outgoing.Size = new System.Drawing.Size(600, 238);
@@ -262,7 +262,7 @@ namespace canWatcher
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 14;
@@ -272,7 +272,7 @@ namespace canWatcher
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 266);
+            this.label2.Location = new System.Drawing.Point(12, 282);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 15;
@@ -308,6 +308,7 @@ namespace canWatcher
             // 
             // cmd_disconnect
             // 
+            this.cmd_disconnect.Enabled = false;
             this.cmd_disconnect.Image = ((System.Drawing.Image)(resources.GetObject("cmd_disconnect.Image")));
             this.cmd_disconnect.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.cmd_disconnect.Name = "cmd_disconnect";
@@ -363,12 +364,28 @@ namespace canWatcher
             this.cmd_send_message.Text = "Send now";
             this.cmd_send_message.Click += new System.EventHandler(this.cmd_send_message_Click);
             // 
+            // cmd_settings
+            // 
+            this.cmd_settings.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.cmd_settings.Image = ((System.Drawing.Image)(resources.GetObject("cmd_settings.Image")));
+            this.cmd_settings.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmd_settings.Name = "cmd_settings";
+            this.cmd_settings.Size = new System.Drawing.Size(66, 22);
+            this.cmd_settings.Text = "Settings";
+            this.cmd_settings.Click += new System.EventHandler(this.cmd_settings_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
             // strip_status
             // 
             this.strip_status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.lab_status});
-            this.strip_status.Location = new System.Drawing.Point(0, 554);
+            this.strip_status.Location = new System.Drawing.Point(0, 571);
             this.strip_status.Name = "strip_status";
             this.strip_status.Size = new System.Drawing.Size(620, 22);
             this.strip_status.TabIndex = 19;
@@ -388,7 +405,7 @@ namespace canWatcher
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(231, 529);
+            this.label3.Location = new System.Drawing.Point(231, 545);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(107, 13);
             this.label3.TabIndex = 20;
@@ -396,7 +413,7 @@ namespace canWatcher
             // 
             // cmd_send
             // 
-            this.cmd_send.Location = new System.Drawing.Point(552, 526);
+            this.cmd_send.Location = new System.Drawing.Point(552, 542);
             this.cmd_send.Name = "cmd_send";
             this.cmd_send.Size = new System.Drawing.Size(56, 20);
             this.cmd_send.TabIndex = 21;
@@ -404,27 +421,11 @@ namespace canWatcher
             this.cmd_send.UseVisualStyleBackColor = true;
             this.cmd_send.Click += new System.EventHandler(this.cmd_send_Click);
             // 
-            // cmd_settings
-            // 
-            this.cmd_settings.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.cmd_settings.Image = ((System.Drawing.Image)(resources.GetObject("cmd_settings.Image")));
-            this.cmd_settings.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.cmd_settings.Name = "cmd_settings";
-            this.cmd_settings.Size = new System.Drawing.Size(66, 22);
-            this.cmd_settings.Text = "Settings";
-            this.cmd_settings.Click += new System.EventHandler(this.cmd_settings_Click);
-            // 
-            // toolStripSeparator2
-            // 
-            this.toolStripSeparator2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
-            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(620, 576);
+            this.ClientSize = new System.Drawing.Size(620, 593);
             this.Controls.Add(this.cmd_send);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.strip_status);
