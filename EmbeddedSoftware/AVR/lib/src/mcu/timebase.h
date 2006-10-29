def82c10457af2be19622a7fcffdf235599977b9
Jimmy Myhrman <jimmy@myhrman.org>
2006-10-29 18:29:44 +0000

    Added some AVR MCU drivers (UART and Timebase).
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@60 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ EmbeddedSoftware/AVR/lib/src/mcu/timebase.h
@@ -0,0 +1,27 @@
+#ifndef timebase_h_
+#define timebase_h_
+
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+#include <inttypes.h>
+
+
+/*-----------------------------------------------------------------------------
+ * Defines
+ *---------------------------------------------------------------------------*/
+#define TIMEBASE_PRESCALE (64)
+#define TIMEBASE_HITS_PER_1MS (F_OSC/TIMEBASE_PRESCALE/1000)
+#define TIMEBASE_RELOAD ((uint8_t)(0xff-TIMEBASE_HITS_PER_1MS+1))
+
+
+/*-----------------------------------------------------------------------------
+ * Public Function Prototypes
+ *---------------------------------------------------------------------------*/
+void TimebaseInit(void);
+uint32_t TimebaseCurrentTime(void);
+uint32_t TimebasePassedTimeMS(uint32_t t0);
+
+
+#endif
