bc2b1358b69b93980a5e59eaece0b2cc071c5c57
johboh <johboh@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2006-11-11 20:19:27 +0000

    canBoot: bugfix
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@79 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ EmbeddedSoftware/PIC/canBoot/Include/boot.h
@@ -0,0 +1,10 @@
+// $IDI$
+
+#ifndef BOOT_H
+#define BOOT_H
+ 
+    #define RM_RESET_VECTOR             0x001000
+    #define RM_HIGH_INTERRUPT_VECTOR    0x001008
+    #define RM_LOW_INTERRUPT_VECTOR     0x001018
+    
+#endif //BOOT_H
