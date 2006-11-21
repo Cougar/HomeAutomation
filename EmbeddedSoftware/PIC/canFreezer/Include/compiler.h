824676df1354418f1c4660a159403b594ba35bc5
johboh <johboh@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2006-11-21 09:57:15 +0000

    canFreezer: almost done
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@141 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/PIC/canFreezer/Include/compiler.h
+++ EmbeddedSoftware/PIC/canFreezer/Include/compiler.h
@@ -20,5 +20,16 @@
 #define LED0_TRIS		(TRISCbits.TRISC1)
 #define LED0_IO			(PORTCbits.RC1)
 
+#define DOOR_OPEN 1
+#define DOOR_CLOSED 0
+
+#define DOOR_FREEZER_TRIS 		(TRISAbits.TRISA4)
+#define DOOR_FREEZER_IO 		(PORTAbits.RA4)
+#define DOOR_REFRIGERATOR_TRIS 	(TRISAbits.TRISA5)
+#define DOOR_REFRIGERATOR_IO 	(PORTAbits.RA5)
+
+
+#define TEMPERATURE_FREEZER 0b0000
+#define TEMPERATURE_REFRIGERATOR 0b0001
 
 #endif //compiler.h
