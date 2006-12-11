d7d5b8e720f6f431ba829cf90db1e3bdad1e21ee
eqlazer <eqlazer@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2006-12-11 17:17:45 +0000

    CanLCD
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@161 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/EmbeddedSoftware/AVR/CanLCD/main.c
@@ -0,0 +1,85 @@
+/**
+ * CAN Test. This program sends a CAN message once every second. The ID of the
+ * message is increased each time for testing purposes.
+ * Added LCD output
+ * 
+ * @date	2006-11-21, LCD addition 2006-12-11
+ * @author	Jimmy Myhrman, Erik Larsson
+ *   
+ */
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+/* system files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <stdio.h>
+/* lib files */
+#include <can.h>
+#include <serial.h>
+#include <timebase.h>
+#include <lcd.h>
+
+/*-----------------------------------------------------------------------------
+ * Main Program
+ *---------------------------------------------------------------------------*/
+int main(void) {
+	Timebase_Init();
+	Serial_Init();
+	sei();
+	lcd_init( LCD_DISP_ON );
+
+	printf("\n------------------------------------------------------------\n");
+	printf(  "   CAN Test: Periodic Transmission\n");
+	printf(  "------------------------------------------------------------\n");
+
+    lcd_clrscr();
+    lcd_puts("CAN LCD test\n");
+
+	printf("CanInit...");
+    lcd_puts("CanInit... ");
+	if (Can_Init() != CAN_OK) {
+		printf("FAILED!\n");
+        lcd_puts("FAILED!\n");
+	}
+	else {
+		printf("OK!\n");
+        lcd_puts("OK!\n");
+	}
+	
+	uint32_t timeStamp = 0;
+	
+	Can_Message_t txMsg;
+	Can_Message_t rxMsg;
+	txMsg.Id = 0;
+	txMsg.RemoteFlag = 0;
+	txMsg.ExtendedFlag = 1; //DataLength and the databytes are just what happens to be in the memory. They are never set.
+	
+	/* main loop */
+	while (1) {
+		/* service the CAN routines */
+		Can_Service();
+		
+		/* send CAN message and check for CAN errors once every second */
+		if (Timebase_PassedTimeMillis(timeStamp) >= 1000) {
+			timeStamp = Timebase_CurrentTime();
+			/* send txMsg */
+			txMsg.Id++;
+			Can_Send(&txMsg);
+		}
+		
+		/* check if any messages have been received */
+		while (Can_Receive(&rxMsg) == CAN_OK) {
+			printf("MSG Received: ID=%lx, DLC=%u, EXT=%u, RTR=%u, ", rxMsg.Id, (uint16_t)(rxMsg.DataLength), (uint16_t)(rxMsg.ExtendedFlag), (uint16_t)(rxMsg.RemoteFlag));
+    		printf("data={ ");
+    		for (uint8_t i=0; i<rxMsg.DataLength; i++) {
+    			printf("%x ", rxMsg.Data.bytes[i]);
+    		}
+    		printf("}\n");
+		}
+	}
+	
+	return 0;
+}
