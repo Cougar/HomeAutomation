c9511d0ef9853046e143bfdca203c7cc0572a37c
eqlazer <eqlazer@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2006-12-12 20:25:11 +0000

    förslag på funcdefs-hierarki
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@165 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/EmbeddedSoftware/AVR/lib/funcdefs/eqlazer_funcdefs.h
@@ -0,0 +1,24 @@
+﻿/*********************************************************************
+ *
+ *   Defines functs and funccs and other global definitions.
+ *
+ *********************************************************************
+ * Funcdefs for eqlazers net
+ * 
+ * 
+ * 
+ ********************************************************************/
+
+
+//<FUNCC> 10 bits, 0x3FF
+#define FUNCC_SENSORS_IR 						0x001
+
+#define FUNCC_SENSORS_TEMPERATURE_INSIDE 		0x010
+#define FUNCC_SENSORS_TEMPERATURE_OUTSIDE 		0x011
+#define FUNCC_SENSORS_TEMPERATURE_FREEZER 		0x012
+#define FUNCC_SENSORS_TEMPERATURE_REFRIGERATOR 	0x013
+
+//#define FUNCC_SENSORS_DOORS_FREEZER 			0x020
+//#define FUNCC_SENSORS_DOORS_REFRIGERATOR 		0x021
+
+ 
