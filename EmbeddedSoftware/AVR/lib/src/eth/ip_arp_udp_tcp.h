6c3caeacc8d4dd110d8e58b030faac6cdf027899
Anders Runeson <runeson@gmail.com>
2007-01-04 22:05:11 +0000

    En funktion tillagd, denna skickar udp men plockar inte destmac/ip från förra paketet, man anger dessa som parametrar
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@206 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/lib/src/eth/ip_arp_udp_tcp.h
+++ EmbeddedSoftware/AVR/lib/src/eth/ip_arp_udp_tcp.h
@@ -21,6 +21,7 @@ extern uint8_t eth_type_is_ip_and_my_ip(uint8_t *buf,uint16_t len);
 extern void make_arp_answer_from_request(uint8_t *buf);
 extern void make_echo_reply_from_request(uint8_t *buf,uint16_t len);
 extern void make_udp_reply_from_request(uint8_t *buf,char *data,uint8_t datalen,uint16_t port);
+extern void send_udp(uint8_t *buf,char *data,uint8_t datalen,uint16_t port, uint8_t *remotemac, uint8_t *remoteip);
 
 
 extern void make_tcp_synack_from_syn(uint8_t *buf);
