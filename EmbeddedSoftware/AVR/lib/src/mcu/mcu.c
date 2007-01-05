2d7e8d7c85b913a5df1995adc9b0783f5f52fa47
Jimmy Stridh <jimmy@stridh.nu>
2007-01-05 15:37:44 +0000

    Added Mcu_Init function that initializes the internal OSC in mega88.
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@208 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/lib/src/mcu/mcu.c
+++ EmbeddedSoftware/AVR/lib/src/mcu/mcu.c
@@ -26,6 +26,20 @@
  *---------------------------------------------------------------------------*/
 
 /**
+ * Initialize MCU. If the MCU is mega88, the internal clock frequency will be
+ * set to 8MHz. Interrupts must not be enabled when calling this function!
+ */
+void Mcu_Init() {
+	#if defined(__AVR_ATmega88__)
+	// set the clock speed to 8MHz
+	// set the clock prescaler. First write CLKPCE to enable setting of clock the
+	// next four instructions.
+	CLKPR=(1<<CLKPCE);
+	CLKPR=0; // 8 MHZ
+	#endif
+}
+
+/**
  * Enable interrupts.
  */
 void Mcu_EnableIRQ() {
