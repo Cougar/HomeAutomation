dcac42ac5aa3376690bfc54b44c8769644e787df
eqlazer <eqlazer@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2007-01-05 20:52:24 +0000

    rc_servo lib: finished but still untested
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@209 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/lib/src/electromechanic/rc_servo.h
+++ trunk/EmbeddedSoftware/AVR/lib/src/electromechanic/rc_servo.h
@@ -5,18 +5,28 @@
  * Includes
  * ---------------------------------------------*/
 #include <stdlib.h>
+#include <stdio.h>
 #include <inttypes.h>
 
 /*-----------------------------------------------
  * Defines
  * --------------------------------------------*/
 
+/*
+ * If using both OC1A and OC1B set DOUBLE_PWM
+ * On NodeGeneric it's only possible to use OC1A
+ */
+#define DOUBLE_PWM 0
+
 /* 
  * For width of the pulse that control the RC servo.
- * Milliseconds.
+ * Clock pulses.
+ * This is adjusted for a 8 MHz clock, to use different speeds recalculate.
  */
-#define PWM_MAX_PULSE 100
-#define PWM_MIN_PULSE 200
+#define PWM_MAX_PULSE   1000
+#define PWM_MIN_PULSE   2000
+#define PWM_CENTER_PULSE ((PWM_MAX_PULSE - PWM_MIN_PULSE)/2 + PWM_MIN_PULSE)
+#define STEP            ((PWM_MAX_PULSE - PWM_MIN_PULSE)/256)
 
 /*-----------------------------------------------
  * Functions
