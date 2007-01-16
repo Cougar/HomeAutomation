9ec0748b1858362cde5906ebbcbe7d7d304d904f
Anders Runeson <runeson@gmail.com>
2007-01-16 20:45:03 +0000

    * Simple bootloaderfunctionallity in bios
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@248 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/AVR-Bios/bios.c
+++ EmbeddedSoftware/AVR/AVR-Bios/bios.c
@@ -10,6 +10,25 @@
 #include "vectors.h" // Must be included after sfr_defs.h but before any ISR()
 #include "can.h"
 
+#define FUNCTmask		0x1E000000
+#define FUNCTshift		25
+#define FUNCCmask		0x01FF8000
+#define FUNCCshift		15
+#define NIDmask			0x00007E00
+#define NIDshift		9
+#define SIDmask			0x000001FF
+#define SIDshift		0
+
+#define FUNCTbios		0x00
+
+#define FUNCCstartprg	0x81
+#define FUNCCprgdata	0x82
+#define FUNCCstopprg	0x83
+
+#define PRG_STATE_IDLE 			0
+#define PRG_STATE_PROGRAMMING	1
+
+
 //---------------------------------------------------------------------------
 // Globals
 
@@ -97,16 +116,6 @@ int main() {
 	
 	printf("AVR BIOS\n");
 	
-	if (pgm_read_word(0) != 0xffff) {
-		printf("Starting application.\n");
-		app_reset();
-	}
-	
-	printf("No application found.\n");
-	
-	printf("\n------------------------------------------------------------\n");
-	printf(  "   CAN Test: Periodic Transmission\n");
-	printf(  "------------------------------------------------------------\n");
 	
 	printf("CanInit...");
 	if (Can_Init() != CAN_OK) {
@@ -123,16 +132,13 @@ int main() {
 	//The databytes are just what happens to be in the memory. They are never set.
 	txMsg.RemoteFlag = 0;
 	txMsg.ExtendedFlag = 1; 
-#define NODENUMBER 2
-#if NODENUMBER == 1
-	txMsg.Id = 16;
+
+	txMsg.Id = 15;
 	txMsg.DataLength = 2;
-#elif NODENUMBER == 2
-	txMsg.Id = 32;
-	txMsg.DataLength = 5;
-#else
-# error NODENUMBER not set!
-#endif
+	
+	uint16_t address;
+	uint16_t lastoffset=0x0;
+	uint8_t prg_state=PRG_STATE_IDLE;
 	
 	uint8_t i;
 	/* main loop */
@@ -141,11 +147,20 @@ int main() {
 		Can_Service();
 		
 		/* send CAN message and check for CAN errors once every second */
-		if ((timebase_get() - timeStamp) >= 1000) {
+		if ((timebase_get() - timeStamp) >= 10000) {
 			timeStamp = timebase_get();
+
 			/* send txMsg */
-			txMsg.Id++;
+			//txMsg.Id++;
 			Can_Send(&txMsg);
+			
+			if (prg_state==PRG_STATE_IDLE) {
+				if (pgm_read_word(0) != 0xffff) {
+					printf("Starting application.\n");
+					app_reset();
+				}
+				printf("No application found.\n");
+			}
 		}
 		
 		/* check if any messages have been received */
@@ -157,7 +172,43 @@ int main() {
     		}
     		printf("}\n");
     		
-    		if (rxMsg.Id % 20 == 19) reset();
+    		if ((rxMsg.Id & FUNCTmask) == ((uint32_t)FUNCTbios<<FUNCTshift)) {
+    			//printf("Got 'bios package'\n");
+    			//om mottagen data är bois-data (detta borde redan filtrerats i can_receive?)
+    			if ((prg_state==PRG_STATE_IDLE) && ((rxMsg.Id & FUNCCmask) == ((uint32_t)FUNCCstartprg<<FUNCCshift)) && (rxMsg.DataLength == 6)) {
+    				//om mottagen data har funktionskoden starta programmering
+
+	    			//ska kolla om nod-id i IDENT stämmer med nodens id
+	    			//om ok:
+	    			prg_state=PRG_STATE_PROGRAMMING;
+	    			address = rxMsg.Data.bytes[2];
+	    			address |= rxMsg.Data.bytes[3];
+	    			printf("Got 'start programming' at address %x\n", address);
+	    		} else if (prg_state==PRG_STATE_PROGRAMMING) {
+		    		if ((rxMsg.Id & FUNCCmask) == ((uint32_t)FUNCCprgdata<<FUNCCshift)) {
+		    			//om mottagen data har funktionskoden programdata
+		    			uint16_t tmpaddress = rxMsg.Data.bytes[0] + (rxMsg.Data.bytes[1] << 8);
+		    			if (tmpaddress >= lastoffset) {
+		    				tmpaddress += address;
+			    			printf("Got 'programdatapacket' at offset %x\n", tmpaddress);
+			    			for (i=2; i<rxMsg.DataLength; i+=2) {
+			    				flash_write_word(tmpaddress, rxMsg.Data.bytes[i] + (rxMsg.Data.bytes[i+1] << 8));
+			    				tmpaddress+=2;
+			    			} 
+			    			printf("Setting lastoffset to %x\n", tmpaddress);
+		    				lastoffset = tmpaddress;
+		    			}
+		    		} else if ((rxMsg.Id & FUNCCmask) == ((uint32_t)FUNCCstopprg<<FUNCCshift)) {
+		    			flash_flush_buffer();
+		    			prg_state=PRG_STATE_IDLE;
+		    			lastoffset=0x0;
+		    			//beräkna crc på all programdata mellan address och address+lastoffset
+		    			printf("Got 'end programming'\n");
+		    		}
+	    		}
+    		}
+    		
+    		//if (rxMsg.Id % 20 == 19) reset();
 		}
 	}
 
