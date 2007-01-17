4903b2fb95f1072c7b660c89c1cc2c44715efc33
eqlazer <eqlazer@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2007-01-17 11:33:08 +0000

    rc_servo: Now it is possible to control three servos individually. But with reduced resolution because of the 8 bit timer. CanBlinds: Takes argument that decides which servo to turn.
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@249 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/lib/src/actuator/rc_servo.c
+++ EmbeddedSoftware/AVR/lib/src/actuator/rc_servo.c
@@ -1,14 +1,13 @@
 /*
  * PWM output to control a RC servo
  *
- * It is possible to use both OCR1A and OCR1B, but they uses the same value.
- * If it's desired to control A and B individually some rewriting is necessary.
+ * It is possible to use OC0A, OC0B and OC1A. The others are not aviable because
+ * of the pins is used to interface the mcp2515.
  *
  * Author: Erik Larsson
  * Date: 2007-01-04
  *
  * TODO anpassa för fler hastigheter än 8 MHz
- * testa libbet, det fungerar ännu bara i eqlazers huvud
  */
 
 /*-----------------------------------------------
@@ -31,71 +30,108 @@ void rcServoInit(){
     // set up counter 1 (16 bit) to act as a dual channel PWM generator
     // we want OC1A and B to be set on reaching BOTTOM, clear on reaching
     // compare match, use ICR1 as TOP and have a prescale of 8.
-#if DOUBLE_PWM
-    TCCR1A |= (1<<COM1A1) // set OC1A/B at TOP
-        | (1<<COM1B1) // clear OC1A/B when match
-        | (1<<WGM11); // mode 14 (fast PWM, clear TCNT1 on match ICR1)
-    TCCR1B |= (1<<WGM13)
-        | (1<<WGM12)
-        | (1<<CS11); // timer uses main system clock with 1/8 prescale
-    ICR1 = 20000; // used for TOP, makes for 50 hz PWM
-    OCR1A = 1500; // servo at center
-    OCR1B = 1500; // servo at center
-    DDRB |= (1<<PB1)
-        | (1<<PB2);  // have to set up pins as outputs
-#else
-    TCCR1A |= (1<<COM1A1) // set OC1A/B at TOP
-        // clear OC1A/B when match
-        | (1<<WGM11); // mode 14 (fast PWM, clear TCNT1 on match ICR1)
-    TCCR1B |= (1<<WGM13)
-        | (1<<WGM12)
-        | (1<<CS11); // timer uses main system clock with 1/8 prescale
-    ICR1 = 20000; // used for TOP, makes for 50 hz PWM
-    OCR1A = 1500; // servo at center
-    DDRB |= (1<<PB1);  // have to set up pins as outputs
+
+#if NUMBER_OF_RCS > 0
+    /* Use OC1A */
+    TCCR1A |= (1<<COM1A1) | (1<<WGM11); /* Set OC1A at TOP, mode 14 */
+    TCCR1B |= (1<<WGM13) | (1<<WGM12) | (1<<CS11); /* Timer uses main system clock with 1/8 prescale */
+    ICR1 = 20000; /* Used for TOP, makes for 50 Hz PWM */
+    OCR1A = PWM1_CENTER_PULSE; /* Servo at center */
+    DDRB |= (1<<PB1); /* Enable pin as output */
 #endif
+#if NUMBER_OF_RCS > 1
+    /* Use OC0A */
+    TCCR0A |= (1<<COM0A1)|(1<<WGM01)|(1<<WGM00); /* Set OC0A at TOP, Mode 7 */
+    TCCR0B |= (1<<CS02);//(1<<CS02)|(1<<CS00); /* Prescaler 1/1024 */
+    OCR0A = PWM2_CENTER_PULSE;
+    DDRD |= (1<<PD6);
+#endif
+#if NUMBER_OF_RCS > 2
+    /* Use OC0B */
+    TCCR0A |= (1<<COM0B1);
+    OCR0B = PWM3_CENTER_PULSE;
+    DDRD |= (1<<PD5);
+#endif
+
 }
 
 /*
  * Set the servo to an absolute position.
  * Interval -128 to 127.
  */
-void setPosition(int8_t abs_pos){
+void setPosition(int8_t abs_pos, uint8_t servo){
     uint16_t temp_pos;
-    temp_pos = (uint16_t)PWM_CENTER_PULSE + abs_pos*STEP;
-#if DOUBLE_PWM
-    OCR1A = temp_pos;
-    OCR1B = temp_pos;
-#else
-    OCR1A = temp_pos;
+
+    switch( servo ){
+#if NUMBER_OF_RCS > 0
+        case 1:
+            temp_pos = (uint16_t)PWM1_CENTER_PULSE + abs_pos*STEP1;
+            if(temp_pos<PWM1_MIN_PULSE){
+                OCR1A = PWM1_MIN_PULSE;
+            }else if(temp_pos>PWM1_MAX_PULSE){
+                OCR1A = PWM1_MAX_PULSE;
+            }else{
+                OCR1A = temp_pos;
+            }
+            break;
 #endif
+#if NUMBER_OF_RCS > 1
+        case 2:
+            temp_pos = (uint8_t)PWM2_CENTER_PULSE + (int8_t)((abs_pos*STEP2)/8);
+            if(temp_pos<PWM2_MIN_PULSE){
+                OCR0A = PWM2_MIN_PULSE;
+            }else if(temp_pos>PWM2_MAX_PULSE){
+                OCR0A = PWM2_MAX_PULSE;
+            }else{
+                OCR0A = temp_pos;
+            }
+            break;
+#endif
+#if NUMBER_OF_RCS > 2
+        case 3:
+            temp_pos = (uint8_t)PWM3_CENTER_PULSE + (int8_t)((abs_pos*STEP3)/8);
+            if(temp_pos<PWM3_MIN_PULSE){
+                OCR0B = PWM3_MIN_PULSE;
+            }else if(temp_pos>PWM3_MAX_PULSE){
+                OCR0B = PWM3_MAX_PULSE;
+            }else{
+                OCR0B = temp_pos;
+            }
+            break;
+#endif
+    }
+
+
 }
 
 /*
  * Alter the position rel_pos steps.
  * Interval -128 to 127.
  */
-void alterPosition(int8_t rel_pos){
+void alterPosition(int8_t rel_pos, uint8_t servo){
     int8_t new_pos, current_pos;
 
-    current_pos = getPosition();
+    current_pos = getPosition( servo );
     new_pos = current_pos + rel_pos;
 
-    if(rel_pos<0 && current_pos<0 && new_pos>0 ){
+    if(rel_pos<0 && current_pos<new_pos ){
         new_pos = MIN_POSITION;
-    }else if(rel_pos>0 && current_pos>0 && new_pos<0 ){
+    }else if(rel_pos>0 && current_pos>new_pos ){
         new_pos = MAX_POSITION;
     }
 
-    setPosition( new_pos );
+    setPosition( new_pos, servo );
 }
 
 /*
  * Get current position of the servo.
  */
-int8_t getPosition(){
-
-    return (int8_t)((int16_t)(OCR1A - PWM_CENTER_PULSE)/STEP);
+int8_t getPosition(uint8_t servo){
+    switch( servo ){
+        case 1: return (int8_t)((int16_t)(OCR1A - PWM1_CENTER_PULSE)/STEP1); break;
+        case 2: return (int8_t)((OCR0A*8 - PWM2_CENTER_PULSE*8)/STEP2); break;
+        case 3: return (int8_t)((OCR0B*8 - PWM3_CENTER_PULSE*8)/STEP3); break;
+    }
 }
 
 /*
--- EmbeddedSoftware/AVR/lib/src/actuator/rc_servo.h
+++ EmbeddedSoftware/AVR/lib/src/actuator/rc_servo.h
@@ -13,20 +13,34 @@
  * --------------------------------------------*/
 
 /*
- * If using both OC1A and OC1B set DOUBLE_PWM
- * On NodeGeneric it's only possible to use OC1A
+ * It is possible to use up to three PWM outputs.
+ * In order: OC1A, OC0A and OC0B.
+ * They will be named RCS0-RCS2.
+ * Define how many that will be used.
  */
-#define DOUBLE_PWM 0
+#define NUMBER_OF_RCS 3
 
 /* 
  * For width of the pulse that control the RC servo.
  * Clock pulses.
  * This is adjusted for a 8 MHz clock, to use different speeds recalculate.
  */
-#define PWM_MAX_PULSE   1000
-#define PWM_MIN_PULSE   2000
-#define PWM_CENTER_PULSE ((PWM_MAX_PULSE - PWM_MIN_PULSE)/2 + PWM_MIN_PULSE)
-#define STEP            ((PWM_MAX_PULSE - PWM_MIN_PULSE)/256)
+#define PWM1_MIN_PULSE   800 // 1000
+#define PWM1_MAX_PULSE   2200 // 2000
+#define PWM2_MIN_PULSE   20
+#define PWM2_MAX_PULSE   50
+#define PWM3_MIN_PULSE   20
+#define PWM3_MAX_PULSE   50
+
+//#define PWM1_CENTER_PULSE ((PWM1_MAX_PULSE - PWM1_MIN_PULSE)/2 + PWM1_MIN_PULSE) /* Pretty much standard to use 1500 */
+#define PWM1_CENTER_PULSE 1500
+#define PWM2_CENTER_PULSE 35
+#define PWM3_CENTER_PULSE 35
+
+#define STEP1   ((PWM1_MAX_PULSE - PWM1_MIN_PULSE)/256)
+#define STEP2   1
+#define STEP3   1
+
 #define MAX_POSITION 127
 #define MIN_POSITION -128
 
@@ -34,8 +48,8 @@
  * Functions
  * ---------------------------------------------*/
 void rcServoInit();
-void setPosition(int8_t abs_pos);
-void alterPosition(int8_t rel_pos);
-int8_t getPosition();
+void setPosition(int8_t abs_pos, uint8_t servo);
+void alterPosition(int8_t rel_pos, uint8_t servo);
+int8_t getPosition(uint8_t servo);
 
 #endif
