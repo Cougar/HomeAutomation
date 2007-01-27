1d90c485b95b7e8f70afba963f2b245bea575ea8
Anders Runeson <runeson@gmail.com>
2007-01-27 22:19:50 +0000

    Liten remake, Sorry Johboh
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@257 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/canBootloader/canBootloader/CanPacket.cs
+++ trunk/PcSoftware/canBootloader/canBootloader/CanPacket.cs
@@ -6,10 +6,11 @@ namespace canBootloader
 {
     public class CanPacket
     {
-        private byte funct = 0;
-        private uint funcc = 0;
+        private byte pktclass = 0;
+        private byte type = 0;
         private byte nid = 0;
-        private uint sid = 0;
+        private byte sid = 0;
+        private byte rid = 0;
         private byte data_length = 0;
         private byte[] data = new byte[8];
 
@@ -18,39 +19,36 @@ namespace canBootloader
 	        // 17 bytes, a packet.
 	        // UART_START_BYTE id[0] id[1] id[2] id[3] extended remote_request data_length d[0] d[1] d[2] d[3] d[4] d[5] d[6] d[7] UART_END_BYTE
             /*
-	        * 000TTTTC CCCCCCCC CNNNNNNS SSSSSSSS
+	        * 000CCCCT TTTTNNNN SSSSSSSS RRRRRRRR
 	        *
-	        * <FUNCT> = 00011110 00000000 00000000 00000000 = 0x1E000000	max(0xF, 15)
-	        * <FUNCC> = 00000001 11111111 10000000 00000000 = 0x01FF8000    max(0x3FF, 1023)
-	        * <NID>   = 00000000 00000000 01111110 00000000 = 0x00007E00	max(0x3F, 63)
-	        * <SID>   = 00000000 00000000 00000001 11111111 = 0x000001FF	max(0x1FF, 511)
+	        * <CLASS> = 00011110 00000000 00000000 00000000 = 0x1E000000	NMT, ...
+	        * <TYPE>  = 00000001 11110000 00000000 00000000 = 0x01F00000	NMT[CAN_NMT_PGM_START, CAN_ID_NMT_PGM_ACK, ...]
+	        * <NID>   = 00000000 00001111 00000000 00000000 = 0x000F0000	Network ID
+	        * <SID>   = 00000000 00000000 11111111 00000000 = 0x0000FF00	Sender ID
+	        * <RID>   = 00000000 00000000 00000000 11111111 = 0x000000FF	Receiver ID
 	        * 
-	        <FUNCT>, 4 bitar = Function Type, funktionstyp.
-	        <FUNCC>, 10 bitar = Function Code, funktionskod.
-	        <NID>, 6 bitar = Network ID, nätverksid.
-	        <SID>, 9 bitar = Sender ID, sändarid.
 	        */
 
 
             ulong addr = (((ulong)raw[startIndex + 3]) << 24) + (((ulong)raw[startIndex + 2]) << 16) + (((ulong)raw[startIndex + 1]) << 8) + (((ulong)raw[startIndex + 0]));
 
-            this.funct = (byte)((addr & 0x1E000000)>>25);
-            this.funcc = (uint)((addr & 0x01FF8000) >> 15);
-            this.nid = (byte)((addr & 0x00007E00) >> 9);
-            this.sid = (uint)((addr & 0x000001FF));
+            this.pktclass = (byte)((addr & 0x1E000000) >> 25);
+            this.type =     (byte)((addr & 0x01F00000) >> 20);
+            this.nid =      (byte)((addr & 0x000F0000) >> 16);
+            this.sid =      (byte)((addr & 0x0000FF00) >> 8);
+            this.rid =      (byte)((addr & 0x000000FF));
             this.data_length = raw[startIndex + 6];
 
             for (int i = 0; i < 8; i++) this.data[i] = raw[startIndex + 7 + i];
-
-
         }
 
-        public CanPacket(byte funct, uint funcc, byte nid, uint sid, byte data_length, byte[] data)
+        public CanPacket(byte pktclass, byte type, byte nid, byte sid, byte rid, byte data_length, byte[] data)
         { 
-            this.funct=funct;
-	        this.funcc=funcc;
+            this.pktclass=pktclass;
+	        this.type=type;
 	        this.nid=nid;
 	        this.sid=sid;
+	        this.rid=rid;
 	        this.data_length=data_length;
 	        for(int i=0;i<8;i++) this.data[i]=data[i];
         }
@@ -68,14 +66,15 @@ namespace canBootloader
 
 	        for(int i=0;i<8;i++) if (this.data[i]!=bytes[i]) return false;
 
-	        return (this.funct==cpm.getFunct() && this.funcc==cpm.getFuncc() && this.nid==cpm.getNid() && this.sid==cpm.getSid() && this.data_length==cpm.getDataLength());
+	        return (this.pktclass==cpm.getPktClass() && this.type==cpm.getType() && this.nid==cpm.getNid() && this.sid==cpm.getSid() && this.sid==cpm.getRid() && this.data_length==cpm.getDataLength());
 
         }
 
-        public byte getFunct(){ return this.funct; }
-        public uint getFuncc(){ return this.funcc; }
+        public byte getPktClass(){ return this.pktclass; }
+        public uint getType(){ return this.type; }
         public byte getNid(){ return this.nid; }
         public uint getSid(){ return this.sid; }
+        public uint getRid(){ return this.sid; }
         public byte getDataLength(){ return this.data_length; }
         public byte[] getData(){ return this.data; }
 
@@ -83,7 +82,7 @@ namespace canBootloader
         {
             string str = "";
 
-            str = "funct: "+funct.ToString()+", funcc: "+funcc.ToString()+", nid: "+nid.ToString()+", sid: "+sid.ToString()+",data_length: "+data_length.ToString()+", data: ";
+            str = "pktclass: "+pktclass.ToString()+", type: "+type.ToString()+", nid: "+nid.ToString()+", sid: "+sid.ToString()+", rid: "+rid.ToString()+",data_length: "+data_length.ToString()+", data: ";
             for (int i = 0; i < 8; i++) str+=data[i].ToString()+" ";
 
             return str;
@@ -97,20 +96,17 @@ namespace canBootloader
 	        // 17 bytes, a packet.
 	        // UART_START_BYTE id[0] id[1] id[2] id[3] extended remote_request data_length d[0] d[1] d[2] d[3] d[4] d[5] d[6] d[7] UART_END_BYTE
             /*
-	        * 000TTTTC CCCCCCCC CNNNNNNS SSSSSSSS
+	        * 000CCCCT TTTTNNNN SSSSSSSS RRRRRRRR
 	        *
-	        * <FUNCT> = 00011110 00000000 00000000 00000000 = 0x1E000000
-	        * <FUNCC> = 00000001 11111111 10000000 00000000 = 0x01FF8000
-	        * <NID>   = 00000000 00000000 01111110 00000000 = 0x00007E00
-	        * <SID>   = 00000000 00000000 00000001 11111111 = 0x000001FF
+	        * <CLASS> = 00011110 00000000 00000000 00000000 = 0x1E000000	NMT, ...
+	        * <TYPE>  = 00000001 11110000 00000000 00000000 = 0x01F00000	NMT[CAN_NMT_PGM_START, CAN_ID_NMT_PGM_ACK, ...]
+	        * <NID>   = 00000000 00001111 00000000 00000000 = 0x000F0000	Network ID
+	        * <SID>   = 00000000 00000000 11111111 00000000 = 0x0000FF00	Sender ID
+	        * <RID>   = 00000000 00000000 00000000 11111111 = 0x000000FF	Receiver ID
 	        * 
-	        <FUNCT>, 4 bitar = Function Type, funktionstyp.
-	        <FUNCC>, 10 bitar = Function Code, funktionskod.
-	        <NID>, 6 bitar = Network ID, nätverksid.
-	        <SID>, 9 bitar = Sender ID, sändarid.
 	        */
 
-            ulong addr = (((ulong)this.funct)<<25)+(((ulong)this.funcc)<<15)+(((ulong)this.nid)<<9)+((ulong)this.sid);
+            ulong addr = (((ulong)this.pktclass)<<25)+(((ulong)this.type)<<20)+(((ulong)this.nid)<<16)+(((ulong)this.sid)<<8)+((ulong)this.rid);
 
             bytes[3] = (byte)((addr & 0xFF000000) >> 24);
             bytes[2] = (byte)((addr & 0x00FF0000) >> 16);
@@ -120,7 +116,6 @@ namespace canBootloader
 	        bytes[5]=0;
 	        bytes[6]=this.data_length;
 	        for(int i=0;i<8;i++) bytes[i+7]=this.data[i];
-            
 
             return bytes;
         }
