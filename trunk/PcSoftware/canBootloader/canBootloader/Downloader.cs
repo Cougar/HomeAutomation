ebc7a74f192e696fa18e9657d9558b5faa3210db
Anders Runeson <runeson@gmail.com>
2007-01-28 15:36:18 +0000

    *makefile added *all tab-spaces converted to tabs *use 6 bytes data when sending programdata
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@262 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/canBootloader/canBootloader/Downloader.cs
+++ trunk/PcSoftware/canBootloader/canBootloader/Downloader.cs
@@ -5,289 +5,255 @@ using System.Threading;
 
 namespace canBootloader
 {
-    public class Downloader
-    {
-        private HexFile hf;
-        private SerialConnection sc;
-        private Thread down;
-        private byte receiveID;
+	public class Downloader
+	{
+		private HexFile hf;
+		private SerialConnection sc;
+		private Thread down;
+		private byte receiveID;
 
-        private enum dState { SEND_START, WAIT_ACK_PRG, SEND_PGM_DATA, WAIT_ACK_DATA, RESEND_ADDR, WAIT_DONE, SEND_DONE, DONE, DEBUG_STATE1, DEBUG_STATE2 };
+		private enum dState { SEND_START, WAIT_ACK_PRG, SEND_PGM_DATA, WAIT_ACK_DATA, RESEND_ADDR, WAIT_DONE, SEND_DONE, DONE, DEBUG_STATE1, DEBUG_STATE2 };
 		
-        private const byte CAN_NMT				= 0x00;
+		private const byte CAN_NMT				= 0x00;
 		
-        private const byte CAN_NMT_BIOS_START 	= 0x01;
-        private const byte CAN_NMT_PGM_START 	= 0x02;
-        private const byte CAN_NMT_PGM_DATA 	= 0x03;
-        private const byte CAN_NMT_PGM_END 		= 0x04;
-        private const byte CAN_NMT_PGM_ACK 		= 0x05;
-        private const byte CAN_NMT_PGM_NACK 	= 0x06;
-        private const byte CAN_NMT_START_APP 	= 0x07;
-        private const byte CAN_NMT_APP_START 	= 0x08;
-        private const byte CAN_NMT_HEARTBEAT 	= 0x09;
-        
-        private const byte MY_ID = 0xAB;
-        //private const byte TARGET_ID = 0x78;
-        private const byte MY_NID = 0x0;
-        private const byte ADDR0_INDEX = 0;
-        private const byte ADDR1_INDEX = 1;
-        private const byte ADDR2_INDEX = 2;
-        private const byte ADDR3_INDEX = 3;
-        private const byte RID_INDEX = 4;
-        private const byte ERR_INDEX = 4;
-        private const byte ERR_NO_ERROR = 0x00; //= inget fel
-        private const byte ERR_ERROR = 0x01; //= fel
+		private const byte CAN_NMT_BIOS_START 	= 0x01;
+		private const byte CAN_NMT_PGM_START 	= 0x02;
+		private const byte CAN_NMT_PGM_DATA 	= 0x03;
+		private const byte CAN_NMT_PGM_END 		= 0x04;
+		private const byte CAN_NMT_PGM_ACK 		= 0x05;
+		private const byte CAN_NMT_PGM_NACK 	= 0x06;
+		private const byte CAN_NMT_START_APP 	= 0x07;
+		private const byte CAN_NMT_APP_START 	= 0x08;
+		private const byte CAN_NMT_HEARTBEAT 	= 0x09;
+		
+		private const byte MY_ID = 0x00;
+		//private const byte TARGET_ID = 0x78;
+		private const byte MY_NID = 0x0;
+		private const byte ADDR0_INDEX = 0;
+		private const byte ADDR1_INDEX = 1;
+		private const byte ADDR2_INDEX = 2;
+		private const byte ADDR3_INDEX = 3;
+		//private const byte RID_INDEX = 4;
+		//private const byte ERR_INDEX = 4;
+		//private const byte ERR_NO_ERROR = 0x00; //= inget fel
+		//private const byte ERR_ERROR = 0x01; //= fel
 
-        private const int TIMEOUT_MS = 1000;
+		private const int TIMEOUT_MS = 2000;
 
-        private long timeStart = 0;
-
-
-        public Downloader(HexFile hf, SerialConnection sc, byte receiveID) 
-        {
-            this.hf = hf;
-            this.sc = sc;
-            this.receiveID = receiveID;
-        }
-
-        public bool go() 
-        {
-            if (sc==null || hf==null || hf.getLength()==0 || !sc.isOpen()) return false;
-            down = new Thread(new ThreadStart(downloader));
-            down.Start();
-            return true;
-        }
-
-        public void abort()
-        {
-            if (down != null && down.IsAlive) down.Abort();
-        }
-
-        public void downloader()
-        {
-            dState pgs = dState.SEND_START;
-            int t = 0;
-            CanPacket outCm = null;
-            CanPacket cm = null;
-            byte[] data = new byte[8];
-            uint byteSent = 0;
-            ulong currentAddress = 0;
+		private long timeStart = 0;
 
 
-            try
-            {
-                while (true)
-                {
-                    bool hasMessage = sc.getPacket(out cm);
-                    if (hasMessage)
-                    {
-                        Array.Copy(cm.getData(), data, 8);
-                    }
-                    
-                   
-                    switch (pgs)
-                    {
-                        case dState.SEND_START:
-                            // Send boot start packet to target.
-                            // and wait for ack.
-                            currentAddress = hf.getAddrLower();
-                            //data[RID_INDEX] = TARGET_ID;
-                            data[ADDR0_INDEX] = (byte)(currentAddress & 0x0000FF);
-                            data[ADDR1_INDEX] = (byte)((currentAddress & 0x00FF00) >> 8);
-                            data[ADDR2_INDEX] = (byte)((currentAddress & 0xFF0000) >> 16);
-                            data[ADDR3_INDEX] = 0;
-                            outCm = new CanPacket(CAN_NMT, CAN_NMT_PGM_START, MY_NID, MY_ID, receiveID, 4, data);
-                            sc.writePacket(outCm);
-                            t = Environment.TickCount;
-                            pgs = dState.WAIT_ACK_PRG;
-                            timeStart = Environment.TickCount;
-                            break;
+		public Downloader(HexFile hf, SerialConnection sc, byte receiveID) 
+		{
+			this.hf = hf;
+			this.sc = sc;
+			this.receiveID = receiveID;
+		}
+
+		public bool go() 
+		{
+			if (sc==null || hf==null || hf.getLength()==0 || !sc.isOpen()) return false;
+			down = new Thread(new ThreadStart(downloader));
+			down.Start();
+			return true;
+		}
+
+		public void abort()
+		{
+			if (down != null && down.IsAlive) down.Abort();
+		}
+
+		public void downloader()
+		{
+			dState pgs = dState.SEND_START;
+			int t = 0;
+			CanPacket outCm = null;
+			CanPacket cm = null;
+			byte[] data = new byte[8];
+			uint byteSent = 0;
+			ulong currentAddress = 0;
 
 
-                        case dState.WAIT_ACK_PRG:
-                            
-                            // Check for timeout, resend start packet in that case..
-                            if ((Environment.TickCount - t) > TIMEOUT_MS)
-                            {
-                            	Console.WriteLine("Timeout while waiting for start prg ack.");
-                                pgs = dState.DONE;
-                            }
-
-                            // If received ack.
-                            if (hasMessage && cm.getPktClass() == CAN_NMT && cm.getNid() == MY_NID && cm.getSid() == receiveID)
-                            {
-                                // If no error
-                                if (cm.getType() == CAN_NMT_PGM_ACK)
-                                {
-                                    // Start sending program data..
-                                    byteSent = 0;
-                                    pgs = dState.SEND_PGM_DATA;
-                                }
-                                else if (cm.getType() == CAN_NMT_PGM_NACK )
-                                { 
-                                    // else ..
-                                    Console.WriteLine("Nack on CAN_NMT_PGM_START.");
-                                    pgs = dState.DONE;
-                                }
-                            }
-
-                            break;
-
-                        case dState.SEND_PGM_DATA:
-	                        Console.WriteLine("byteSent = "+byteSent);
-                            // Send program data.
-                            for (ulong i = 0; i < 4; i++)
-                            { 
-                                // Populate data.
-                                data[i+2] = hf.getByte(currentAddress + i + byteSent);
-                            }
-                            data[0] = (byte)(byteSent & 0x00FF);
-                            data[1] = (byte)((byteSent & 0xFF00) >> 8);
-                            outCm = new CanPacket(CAN_NMT, CAN_NMT_PGM_DATA, MY_NID, MY_ID, receiveID, 6, data);
-                            sc.writePacket(outCm);
-                            t = Environment.TickCount;
-                            byteSent += 4;
-                            pgs = dState.WAIT_ACK_DATA;
-                            break;
+			try
+			{
+				while (true)
+				{
+					bool hasMessage = sc.getPacket(out cm);
+					if (hasMessage)
+					{
+						Array.Copy(cm.getData(), data, 8);
+					}
+					
+				   
+					switch (pgs)
+					{
+						case dState.SEND_START:
+							// Send boot start packet to target.
+							// and wait for ack.
+							currentAddress = hf.getAddrLower();
+							//data[RID_INDEX] = TARGET_ID;
+							data[ADDR0_INDEX] = (byte)(currentAddress & 0x0000FF);
+							data[ADDR1_INDEX] = (byte)((currentAddress & 0x00FF00) >> 8);
+							data[ADDR2_INDEX] = (byte)((currentAddress & 0xFF0000) >> 16);
+							data[ADDR3_INDEX] = 0;
+							outCm = new CanPacket(CAN_NMT, CAN_NMT_PGM_START, MY_NID, MY_ID, receiveID, 4, data);
+							sc.writePacket(outCm);
+							t = Environment.TickCount;
+							pgs = dState.WAIT_ACK_PRG;
+							timeStart = Environment.TickCount;
+							break;
 
 
-                        case dState.WAIT_ACK_DATA:
-                            // Wait for pgm ack.
-                            if ((Environment.TickCount - t) > 2*TIMEOUT_MS)
-                            {
-                                // Woops, error. 
-                                Console.WriteLine("Timeout while waiting for data ack.");
-                                pgs = dState.DONE;
-                            }
+						case dState.WAIT_ACK_PRG:
+							// Check for timeout, resend start packet in that case..
+							if ((Environment.TickCount - t) > TIMEOUT_MS)
+							{
+								Console.WriteLine("Timeout while waiting for start prg ack.");
+								pgs = dState.DONE;
+							}
 
-                            // If received ack.
-                            if (hasMessage && cm.getPktClass() == CAN_NMT && cm.getNid() == MY_NID && cm.getSid() == receiveID)
-                            {
-                                // If no error
-                                if (cm.getType() == CAN_NMT_PGM_ACK)
-                                {
-                                    //currentAddress += 2;
-                                    // Check if end.
-                                    if (currentAddress + byteSent > hf.getAddrUpper())
-                                    {
-                                        // Yes, we are done, send done.
-                                        pgs = dState.SEND_DONE;
-                                    }
-                                    else
-                                    { 
-                                        // More to write.
-                                        //byteSent = 0;
-                                        pgs = dState.SEND_PGM_DATA;
-                                    }
-                                }
-                                else if (cm.getType() == CAN_NMT_PGM_NACK)
-                                {
-                                    // else ..
-                                    Console.WriteLine("Nack on CAN_NMT_PGM_DATA.");
-                                    pgs = dState.DONE;
-                                }
-                            }
+							// If received ack.
+							if (hasMessage && cm.getPktClass() == CAN_NMT && cm.getNid() == MY_NID && cm.getSid() == receiveID)
+							{
+								// If no error
+								if (cm.getType() == CAN_NMT_PGM_ACK)
+								{
+									// Start sending program data..
+									byteSent = 0;
+									pgs = dState.SEND_PGM_DATA;
+								}
+								else if (cm.getType() == CAN_NMT_PGM_NACK )
+								{ 
+									// else ..
+									Console.WriteLine("Nack on CAN_NMT_PGM_START.");
+									pgs = dState.DONE;
+								}
+							}
 
-                            break;
+							break;
 
-
-                        case dState.SEND_DONE:
-                            // Send done with crc
-                            data[0] = 0;
-                            data[1] = 0;
-                            outCm = new CanPacket(CAN_NMT, CAN_NMT_PGM_END, MY_NID, MY_ID, receiveID, 2, data);
-                            sc.writePacket(outCm);
-                            t = Environment.TickCount;
-                            pgs = dState.WAIT_DONE;
-                            break;
-
-                        //case dState.RESEND_ADDR:
-                            // Resend addr.
-                            //data[RID_INDEX] = TARGET_ID;
-                            //data[ADDRU_INDEX] = (byte)((currentAddress & 0xFF0000) >> 16);
-                            //data[ADDRH_INDEX] = (byte)((currentAddress & 0xFF00) >> 8);
-                            //data[ADDRL_INDEX] = (byte)(currentAddress & 0xFF);
-                            //outCm = new CanPacket(FUNCT_BOOTLOADER, FUNCC_BOOT_ADDR, MY_NID, MY_ID, 8, data);
-                            //sc.writePacket(outCm);
-                            //t = Environment.TickCount;
-                            //pgs = dState.WAIT_ADDR_ACK;
-                            //break;
+						case dState.SEND_PGM_DATA:
+							//Console.WriteLine("byteSent = "+byteSent);
+							Console.Write(".");
+							// Send program data.
+							for (ulong i = 0; i < 6; i++)
+							{ 
+								// Populate data.
+								data[i+2] = hf.getByte(currentAddress + i + byteSent);
+							}
+							data[0] = (byte)(byteSent & 0x00FF);
+							data[1] = (byte)((byteSent & 0xFF00) >> 8);
+							outCm = new CanPacket(CAN_NMT, CAN_NMT_PGM_DATA, MY_NID, MY_ID, receiveID, 8, data);
+							sc.writePacket(outCm);
+							t = Environment.TickCount;
+							byteSent += 6;
+							pgs = dState.WAIT_ACK_DATA;
+							break;
 
 
-                        //case dState.WAIT_ADDR_ACK:
-                            // Check for timeout, resend addr packet in that case..
-                        //    if ((Environment.TickCount - t) > TIMEOUT_MS)
-                        //    {
-                        //        pgs = dState.RESEND_ADDR;
-                        //    }
+						case dState.WAIT_ACK_DATA:
+							// Wait for pgm ack.
+							if ((Environment.TickCount - t) > 2*TIMEOUT_MS)
+							{
+								// Woops, error. 
+								Console.WriteLine("Timeout while waiting for data ack.");
+								pgs = dState.DONE;
+							}
 
-                            // If received ack.
-                        //    if (hasMessage && cm.getFunct() == FUNCT_BOOTLOADER && cm.getFuncc() == FUNCC_BOOT_ACK && cm.getNid() == MY_NID && cm.getSid() == TARGET_ID)
-                        //    {
-                                // If no error
-                        //        if (data[ERR_INDEX] == ERR_NO_ERROR)
-                        //        {
-                                    // Start sending program data..
-                        //            byteSent = 0;
-                        //            pgs = dState.SEND_PGM_DATA;
-                        //        }
-                        //        else
-                        //        {
-                                    // else resend addr..
-                        //            pgs = dState.RESEND_ADDR;
-                        //        }
-                        //    }
-                        //    break;
+							// If received ack.
+							if (hasMessage && cm.getPktClass() == CAN_NMT && cm.getNid() == MY_NID && cm.getSid() == receiveID)
+							{
+								// If no error
+								if (cm.getType() == CAN_NMT_PGM_ACK)
+								{
+									//currentAddress += 2;
+									// Check if end.
+									if (currentAddress + byteSent > hf.getAddrUpper())
+									{
+										// Yes, we are done, send done.
+										pgs = dState.SEND_DONE;
+									}
+									else
+									{ 
+										// More to write.
+										//byteSent = 0;
+										pgs = dState.SEND_PGM_DATA;
+									}
+								}
+								else if (cm.getType() == CAN_NMT_PGM_NACK)
+								{
+									// else ..
+									Console.WriteLine("Nack on CAN_NMT_PGM_DATA.");
+									pgs = dState.DONE;
+								}
+							}
 
-                        case dState.WAIT_DONE:
-                            // Check for timeout, resend done packet in that case..
-                            if ((Environment.TickCount - t) > TIMEOUT_MS)
-                            {
-                            	Console.WriteLine("Timeout while waiting for end prg ack.");
-                                pgs = dState.DONE;
-                            }
+							break;
 
-                            // If received ack.
-                            if (hasMessage && cm.getPktClass() == CAN_NMT && cm.getNid() == MY_NID && cm.getSid() == receiveID)
-                            {
-                                // If no error
-                                if (cm.getType() == CAN_NMT_PGM_ACK)
-                                {
-                                    // Start sending program data..
-                                    byteSent = 0;
-                                    pgs = dState.DONE;
-                                }
-                                else if (cm.getType() == CAN_NMT_PGM_NACK)
-                                {
-                                    // else resend addr..
-                                    Console.WriteLine("Nack on CAN_NMT_PGM_END.");
-                                    pgs = dState.DONE;
-                                }
-                            }
 
-                            break;
+						case dState.SEND_DONE:
+							// Send done with crc
+							data[0] = 0;
+							data[1] = 0;
+							outCm = new CanPacket(CAN_NMT, CAN_NMT_PGM_END, MY_NID, MY_ID, receiveID, 2, data);
+							sc.writePacket(outCm);
+							t = Environment.TickCount;
+							pgs = dState.WAIT_DONE;
+							break;
+							
+							
+						case dState.WAIT_DONE:
+							// Check for timeout, resend done packet in that case..
+							if ((Environment.TickCount - t) > TIMEOUT_MS)
+							{
+								Console.WriteLine("Timeout while waiting for end prg ack.");
+								pgs = dState.DONE;
+							}
 
-                        case dState.DONE:
-                            down.Abort();
-                            break;
-                    }
-                }
+							// If received ack.
+							if (hasMessage && cm.getPktClass() == CAN_NMT && cm.getNid() == MY_NID && cm.getSid() == receiveID)
+							{
+								// If no error
+								if (cm.getType() == CAN_NMT_PGM_ACK)
+								{
+									Console.WriteLine("Done, successfully programmed application.");
+									// Start sending program data..
+									byteSent = 0;
+									pgs = dState.DONE;
+								}
+								else if (cm.getType() == CAN_NMT_PGM_NACK)
+								{
+									// else resend addr..
+									Console.WriteLine("Nack on CAN_NMT_PGM_END.");
+									pgs = dState.DONE;
+								}
+							}
 
-            } catch(Exception e) 
-            {
-                Console.WriteLine("Aborted or done.");
+							break;
 
-                long tsecs = ((Environment.TickCount - timeStart) / 1000);
+						case dState.DONE:
+							down.Abort();
+							break;
+					}
+				}
 
-                double bitRate = hf.getLength() / ((double)(Environment.TickCount - timeStart) / 1000.0);
+			} catch(Exception e) 
+			{
+				Console.WriteLine("Aborted or done.");
+				//Console.WriteLine(e);
 
-                if (tsecs <= 0) tsecs = 1;
+				long tsecs = ((Environment.TickCount - timeStart) / 1000);
 
-                long mins = tsecs / 60;
-                long secs = tsecs % 60;
+				double bitRate = hf.getLength() / ((double)(Environment.TickCount - timeStart) / 1000.0);
 
-                Console.WriteLine("Time spent: " + mins + ":" + secs + ", " + Math.Round(bitRate, 2) + " Bps");
-            }
-        }
-    }
+				if (tsecs <= 0) tsecs = 1;
+
+				long mins = tsecs / 60;
+				long secs = tsecs % 60;
+
+				Console.WriteLine("Time spent: " + mins + ":" + secs + ", " + Math.Round(bitRate, 2) + " Bps");
+				Console.Write("> ");
+			}
+		}
+	}
 }
--- trunk/PcSoftware/canBootloader/canBootloader/Program.cs
+++ trunk/PcSoftware/canBootloader/canBootloader/Program.cs
@@ -4,114 +4,114 @@ using System.Text;
 
 namespace canBootloader
 {
-    class Program
-    {
-        static HexFile hf;
-        static SerialConnection sc;
-        static Downloader dl;
+	class Program
+	{
+		static HexFile hf;
+		static SerialConnection sc;
+		static Downloader dl;
 		
 		static private string argPort;
 		static private string argBaud;
 		static private string argHexfile;
 		static private byte argReceiverID;
 				
-        static void Main(string[] args)
-        {
-        	bool error = false;
-        	if (args.Length != 4) {
-        		Console.WriteLine("Syntax: program.exe <port> <baudrate> <hexfile> <nodeaddress>");
-        		Console.WriteLine("port        - can be inform of /dev/ttyS0 or com2");
-        		Console.WriteLine("baudrate    - any baudrate like 19200");
-        		Console.WriteLine("hexfile     - path to the hexfile to program");
-        		Console.WriteLine("nodeaddress - address of the node to program, in hex or decimal");
-        		Console.WriteLine("");
-        		Console.WriteLine("example: program.exe /dev/ttyUSB0 19200 /tmp/program.hex 24");
-        		error = true;
-        	} 
-	        	
-        	//Console.WriteLine("arg0: " + args[0] + "\n");
-        	argPort = args[0];
-        	argBaud = args[1];
-        	argHexfile = args[2];
-        	if (args[3].Length > 1) {
-	        	if (args[3].Substring(0, 2).Equals("0x")) {
-	        		if (args[3].Length == 3) {
-			        	argReceiverID = byte.Parse(args[3].Substring(2, 1), System.Globalization.NumberStyles.HexNumber);
-			        } else if (args[3].Length == 4) {
-			        	argReceiverID = byte.Parse(args[3].Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
-			        } else {
-			        	Console.WriteLine("nodeadress error!");
-			        	error = true;
-			        }
-	    	    } else {
-	        		argReceiverID = byte.Parse(args[3]);
-		        }
-		    } else {
-		    	argReceiverID = byte.Parse(args[3]);
-		    }
-		    if (!error) {
-	            Console.WriteLine("canBootloader");
-	        	Console.WriteLine("Nodeaddress: "+argReceiverID);
-	            Console.WriteLine("Commands:");
-	            Console.WriteLine("load - Load predefined hexfile.");
-	            Console.WriteLine("go - download hexfile.");
-	            Console.WriteLine("exit - exit program.");
+		static void Main(string[] args)
+		{
+			bool error = false;
+			if (args.Length != 4) {
+				Console.WriteLine("Syntax: program.exe <port> <baudrate> <hexfile> <nodeaddress>");
+				Console.WriteLine("port        - can be in form of /dev/ttyS0 or com2");
+				Console.WriteLine("baudrate    - any baudrate like 19200");
+				Console.WriteLine("hexfile     - path to the hexfile to program");
+				Console.WriteLine("nodeaddress - address of the node to program, in hex or decimal");
+				Console.WriteLine("");
+				Console.WriteLine("example: program.exe /dev/ttyUSB0 19200 /tmp/program.hex 24");
+				error = true;
+			} 
+				
+			//Console.WriteLine("arg0: " + args[0] + "\n");
+			argPort = args[0];
+			argBaud = args[1];
+			argHexfile = args[2];
+			if (args[3].Length > 1) {
+				if (args[3].Substring(0, 2).Equals("0x")) {
+					if (args[3].Length == 3) {
+						argReceiverID = byte.Parse(args[3].Substring(2, 1), System.Globalization.NumberStyles.HexNumber);
+					} else if (args[3].Length == 4) {
+						argReceiverID = byte.Parse(args[3].Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+					} else {
+						Console.WriteLine("nodeadress error!");
+						error = true;
+					}
+				} else {
+					argReceiverID = byte.Parse(args[3]);
+				}
+			} else {
+				argReceiverID = byte.Parse(args[3]);
+			}
+			if (!error) {
+				Console.WriteLine("canBootloader");
+				Console.WriteLine("Nodeaddress: "+argReceiverID);
+				Console.WriteLine("Commands:");
+				Console.WriteLine("load - Load predefined hexfile.");
+				Console.WriteLine("go - download hexfile.");
+				Console.WriteLine("exit - exit program.");
 	
-	            string instr;
-	            do
-	            {
-	                Console.Write("> ");
-	                instr = Console.ReadLine().ToLower();
-	            } while (parseInput(instr));
-	        }
-        }
+				string instr;
+				do
+				{
+					Console.Write("> ");
+					instr = Console.ReadLine().ToLower();
+				} while (parseInput(instr));
+			}
+		}
 
-        static private bool parseInput(string instr)
-        {
-            if (instr.Equals("load"))
-            {
-                Console.WriteLine("loading...");
-                //string filepath = "/home/arune/eclipse/c/AVR-App/main.hex";
-                string filepath = argHexfile;
-                hf = new HexFile();
-                if (hf.loadHex(filepath))
-                {
-                    Console.WriteLine("done!");
-                    Console.WriteLine("File "+filepath+" loaded.");
-                    Console.WriteLine("Size: " + hf.getLength().ToString() + " bytes, end adress " + hf.getAddrUpper().ToString() + "(0x" + hf.getAddrUpper().ToString("X") + ").");
-                }
-                else Console.WriteLine("Error loading " + filepath + "!");
-                
-            }
-            else if (instr.Equals("go"))
-            {
-                Console.WriteLine("Connecting..");
-                sc = new SerialConnection();
-                try
-                {
-                    sc.setConfiguration(Int32.Parse(argBaud), System.IO.Ports.Parity.None, argPort, System.IO.Ports.StopBits.One, 8, false);
-                }
-                catch (Exception e) { Console.WriteLine("Error: " + e.Message); return true; }
-                if (!sc.open()) { Console.WriteLine("Error opening port."); return true; }
-                dl = new Downloader(hf, sc, argReceiverID);
-                if (!dl.go()) { Console.WriteLine("Error gooing..."); return true; }
+		static private bool parseInput(string instr)
+		{
+			if (instr.Equals("load"))
+			{
+				Console.WriteLine("loading...");
+				//string filepath = "/home/arune/eclipse/c/AVR-App/main.hex";
+				string filepath = argHexfile;
+				hf = new HexFile();
+				if (hf.loadHex(filepath))
+				{
+					Console.WriteLine("done!");
+					Console.WriteLine("File "+filepath+" loaded.");
+					Console.WriteLine("Size: " + hf.getLength().ToString() + " bytes, end adress " + hf.getAddrUpper().ToString() + "(0x" + hf.getAddrUpper().ToString("X") + ").");
+				}
+				else Console.WriteLine("Error loading " + filepath + "!");
+				
+			}
+			else if (instr.Equals("go"))
+			{
+				Console.WriteLine("Connecting..");
+				sc = new SerialConnection();
+				try
+				{
+					sc.setConfiguration(Int32.Parse(argBaud), System.IO.Ports.Parity.None, argPort, System.IO.Ports.StopBits.One, 8, false);
+				}
+				catch (Exception e) { Console.WriteLine("Error: " + e.Message); return true; }
+				if (!sc.open()) { Console.WriteLine("Error opening port."); return true; }
+				dl = new Downloader(hf, sc, argReceiverID);
+				if (!dl.go()) { Console.WriteLine("Error gooing..."); return true; }
 
-            }
-            else if (instr.Equals("abort"))
-            {
-                if (dl != null) dl.abort();
-            }
-            else if (instr.Equals("exit"))
-            {
-                return false;
-            }
-            else
-            {
-                Console.WriteLine("Unknown command.");
-            }
+			}
+			else if (instr.Equals("abort"))
+			{
+				if (dl != null) dl.abort();
+			}
+			else if (instr.Equals("exit"))
+			{
+				return false;
+			}
+			else
+			{
+				Console.WriteLine("Unknown command.");
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-    }
+	}
 }
