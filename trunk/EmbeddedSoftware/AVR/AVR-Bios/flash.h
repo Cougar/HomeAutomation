d6bdb4888e366c440d8470fabc3d41d99ee4c043
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2007-02-08 21:42:30 +0000

    * EmbeddedSoftware/AVR/AVR-Bios/flash.c: Made flash_copy_data independent and located in top two pages of flash. All other code should be possible to update now. * EmbeddedSoftware/AVR/AVR-Bios/can.h: Corrected defines for additional communication classes.
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@299 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/AVR-Bios/flash.h
+++ trunk/EmbeddedSoftware/AVR/AVR-Bios/flash.h
@@ -4,7 +4,8 @@
 #define BOOTLOADER __attribute__ ((section (".bootloader")))
 
 //#define FLASH_LOAD_BUFFER
-#define FLASH_COPY_DATA
+//#define FLASH_COPY_DATA
+#define FLASH_COPY_DATA_NEW
 
 //---------------------------------------------------------------------------
 // Function prototypes for flash modifying functions
@@ -14,6 +15,9 @@ void flash_write_word(uint16_t addr, uint16_t word) BOOTLOADER;
 void flash_flush_buffer(void) BOOTLOADER;
 #ifdef FLASH_COPY_DATA
 void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) BOOTLOADER;
+#endif
+#ifdef FLASH_COPY_DATA_NEW
+void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) __attribute__ ((noreturn)) __attribute__ ((section (".flash_code")));
 #endif
 
 #endif /*FLASH_H_*/
