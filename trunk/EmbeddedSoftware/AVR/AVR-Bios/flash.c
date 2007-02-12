c0ea144af394030702c0c43a4fa506d011b68ee2
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2007-02-12 13:51:06 +0000

    EmbeddedSoftware/AVR/AVR-Bios    * bios_funs.h: Made __bios_start void instead of uint16_t.    * bios.c: CAN_NMT_TIME resets watchdog.              Comments cleanup.              Improved crc calculation loop and address validity check.              Pass page buffer to flash_init.    * can.h: Added CAN_NMT_TIME and some space between the rest of the NMT types.    * flash.c: Rewrite to use SRAM page buffer instead.               Reimplemented flash_load_buffer.               Deleted old unused code and cleaned up comments.    * flash.h: Some cleaning.
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@315 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/AVR-Bios/flash.c
+++ trunk/EmbeddedSoftware/AVR/AVR-Bios/flash.c
@@ -6,132 +6,96 @@
 
 static uint16_t flash_prev_addr;
 static uint8_t flash_buffer_dirty = 0;
+static uint8_t* flash_pagebuf;
 
 void flash_flush_buffer() {
 	uint8_t sreg;
+	uint16_t word;
+	uint8_t i;
+	uint16_t* buf = (uint16_t*)flash_pagebuf; // Access buffer on a word basis.
 	
-	if (!flash_buffer_dirty) return; // Nothing to flush
+	if (!flash_buffer_dirty) return; // Nothing to flush.
+	
+	// Copy the sram buffer to the temporary page buffer.
+	for (i = 0; i < SPM_PAGESIZE/2; i++) {
+		word = buf[i];
+		boot_page_fill(i*2, word); // Absolute addressing is not neccessary.
+	}
 	
 	// Disable interrupts.
-	
 	sreg = SREG;
 	cli();
 	
-	eeprom_busy_wait();
+	eeprom_busy_wait(); // Make sure any eeprom access has finished.
 	
 	boot_page_erase(flash_prev_addr);
-	boot_spm_busy_wait();      // Wait until the memory is erased.
+	boot_spm_busy_wait();  // Wait until the memory is erased.
 	
-	boot_page_write(flash_prev_addr);     // Store buffer in flash page.
-	boot_spm_busy_wait();       // Wait until the memory is written.
+	boot_page_write(flash_prev_addr); // Store buffer in flash page.
+	boot_spm_busy_wait();  // Wait until the memory is written.
 
 	flash_buffer_dirty = 0;
 
-	// Reenable RWW-section again. We need this if we want to jump back
-	// to the application after bootloading.
-
+	// Reenable RWW-section again so we can return to BIOS after flashing.
 	boot_rww_enable ();
+	
 	// Re-enable interrupts (if they were ever enabled).
-
 	SREG = sreg;
 }
 
-#ifdef FLASH_LOAD_BUFFER
-void flash_load_buffer(uint16_t addr) BOOTLOADER;
 void flash_load_buffer(uint16_t addr) {
-	//TODO: Load current flash page contents into temporary buffer to implement
-	// flash read-modify-writes with word granularity. (If possible, the data
-	// sheet is a little fuzzy about this.)
-	// Update: It seems it isn't possible to write any location in the temporary
-	// buffer more than once without destroying the buffer. To implement this
-	// functionality, a sram based buffer is probably neccessary. Problem is, 
-	// how should it be allocated? A global buffer uses valuable .bss space.
-	// All functions should probably take a pointer to a buffer allocated by
-	// main code.
 	uint8_t i;
-	uint16_t data;
+	uint8_t byte;
 	uint16_t page = (addr / SPM_PAGESIZE) * SPM_PAGESIZE;
 	
-	for (i = 0; i < SPM_PAGESIZE; i += 2) {
-		data = pgm_read_word(page + i);
-		boot_page_fill(page + i, data);
+	// Load current flash page contents into sram page buffer to implement
+	// flash read-modify-writes with word granularity.
+	for (i = 0; i < SPM_PAGESIZE; i++) {
+		byte = pgm_read_byte(page + i);
+		flash_pagebuf[i] = byte;
 	}
 }
-#endif
 
 void flash_write_word(uint16_t addr, uint16_t word) {
+	uint16_t* buf = (uint16_t*)flash_pagebuf; // Access buffer on a word basis.
 	
+	// Check if page address has changed since last write
 	if ((addr / SPM_PAGESIZE) != (flash_prev_addr / SPM_PAGESIZE)) {
-		flash_flush_buffer();
-#ifdef FLASH_LOAD_BUFFER
-		flash_load_buffer(addr);
-#endif
+		flash_flush_buffer(); // Flash buffer contents to previous page
+		flash_load_buffer(addr); // Read new page contents into buffer
 	}
 	
-	boot_page_fill(addr, word);
+	buf[addr % SPM_PAGESIZE] = word;
 	flash_prev_addr = addr;
 	flash_buffer_dirty = 1;
 }     
 
-void flash_init() {
+void flash_init(uint8_t* buf) {
+	// 0xffff is never in any page that can be written on devices with <= 64K flash,
+	// so the first flash_write_word will always trigger a buffer load.
 	flash_prev_addr = 0xffff;
+	flash_pagebuf = buf;
 	flash_buffer_dirty = 0;
 }
 
-#ifdef FLASH_COPY_DATA
-void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) {
-	uint16_t i;
-	flash_init();
-	cli(); // From this point we're on our own
-	for (i = 0; i < len; i+=2) {
-		flash_write_word(dst + i, pgm_read_word(src + i));
-		wdt_reset();
-	}
-	flash_flush_buffer();
-	while (1); // Nothing more we can do, hopefully there is a watchdog active
-}
-#endif
-
-#ifdef FLASH_COPY_DATA_NEW
-extern uint16_t __flash_code_start;
+extern void __flash_code_start; // Start of .flash_code from ld-script
 
 void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) {
-#if 0
-	uint16_t offset;
-#endif
 	uint16_t data;
-
-	//eeprom_busy_wait();		// Make sure any current writes to eeprom
-	//boot_spm_busy_wait();	// or flash has completed before updating.
 
 	cli(); // From this point we're on our own
 	
-#if 0
-	for (offset = 0; offset < ((len | (SPM_PAGESIZE - 1)) + 1); offset += 2) {
-		if (dst + offset >= (uint16_t)&__flash_code_start) break;
-		data = pgm_read_word(src + offset);
-		boot_page_fill(dst + offset, data);
-		if ((dst + offset + 2) % SPM_PAGESIZE == 0) {
-			boot_page_erase(dst + offset);
-			boot_spm_busy_wait();      // Wait until the memory is erased.
-			
-			boot_page_write(dst + offset);     // Store buffer in flash page.
-			boot_spm_busy_wait();       // Wait until the memory is written.
-			boot_rww_enable();
-		}
-			
-		wdt_reset();
-	}
-#else
 	len = (len | (SPM_PAGESIZE - 1)) + 3; // Round len up to a whole page + 2
 	
 	while (len -= 2) {
+		if (dst >= (uint16_t)&__flash_code_start) break;
+
 		wdt_reset();
 
 		data = pgm_read_word(src); // Read source word
-		boot_page_fill_safe(dst, data); // Write word to destination
+		boot_page_fill(dst, data); // Write word to destination
 		
-		if (((uint8_t)dst + 2) % SPM_PAGESIZE == 0) { // If we just wrote the last word of a page
+		if ((dst + 2) % SPM_PAGESIZE == 0) { // If we just wrote the last word of a page
 			boot_page_erase(dst);
 			boot_spm_busy_wait(); // Wait until the memory is erased.
 			boot_page_write(dst); // Store buffer in flash page.
@@ -140,12 +104,8 @@ void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) {
 		}
 		 
 		src += 2;
-		dst += 2;
-		
-		if (dst >= (uint16_t)&__flash_code_start) break;
+		dst += 2;		
 	}
-		 
-#endif
+
 	while (1); // Nothing more we can do, hopefully there is a watchdog active
 }
-#endif
--- trunk/EmbeddedSoftware/AVR/AVR-Bios/flash.h
+++ trunk/EmbeddedSoftware/AVR/AVR-Bios/flash.h
@@ -2,22 +2,18 @@
 #define FLASH_H_
 
 #define BOOTLOADER __attribute__ ((section (".bootloader")))
-
-//#define FLASH_LOAD_BUFFER
-//#define FLASH_COPY_DATA
-#define FLASH_COPY_DATA_NEW
+#define FLASH_CODE __attribute__ ((section (".flash_code")))
 
 //---------------------------------------------------------------------------
 // Function prototypes for flash modifying functions
 
-void flash_init(void) BOOTLOADER;
+// Only flash_flush_buffer uses the spm instruction and has to be in the
+// .bootloader section, but there is room for the other functions which
+// would otherwise use space in .text.
+void flash_init(uint8_t* buf) BOOTLOADER;
 void flash_write_word(uint16_t addr, uint16_t word) BOOTLOADER;
 void flash_flush_buffer(void) BOOTLOADER;
-#ifdef FLASH_COPY_DATA
-void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) BOOTLOADER;
-#endif
-#ifdef FLASH_COPY_DATA_NEW
-void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) __attribute__ ((noreturn)) __attribute__ ((section (".flash_code")));
-#endif
+// Tell gcc that flash_copy_data never returns, so it can be further optimized.
+void flash_copy_data(uint16_t src, uint16_t dst, uint16_t len) __attribute__ ((noreturn)) FLASH_CODE;
 
 #endif /*FLASH_H_*/
