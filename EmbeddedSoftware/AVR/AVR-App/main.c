ed035a24ba118786c13709fcd0981cc99d76e056
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2007-02-16 10:41:23 +0000

    * EmbeddedSoftware/AVR/AVR-App/Makefile: Changed to other type of Makefile. * EmbeddedSoftware/AVR/AVR-App/main.c: Rewrote to use its own serial routines (from lib) and to send more reasonable candata.
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@325 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/AVR-App/main.c
+++ EmbeddedSoftware/AVR/AVR-App/main.c
@@ -2,21 +2,10 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <bios.h>
+#include <serial.h>
 
-static int app_putchar(char c, FILE *stream);
-static int app_getchar(FILE *stream);
-
-static FILE mystdio = FDEV_SETUP_STREAM(app_putchar, app_getchar,
-                                         _FDEV_SETUP_RW);
-
-static int app_putchar(char c, FILE *stream) {
-	bios->debug_putchar(c);
-	return 0; 
-}
-    
-static int app_getchar(FILE *stream) {
-	return bios->debug_getchar();
-}
+#define APP_TYPE    0xf001
+#define APP_VERSION 0x0001
 
 void can_receive(Can_Message_t *msg) {
 	printf("candata!\n");
@@ -30,34 +19,36 @@ int main(void)
 	
 	sei();
 	
-	stdout = &mystdio;
-	stdin = &mystdio;
-
+	Serial_Init();
+	
 	Can_Message_t txMsg;
-	txMsg.Id = 100;
-	txMsg.DataLength = 0;
+	txMsg.Id = (CAN_NMT_APP_START << CAN_SHIFT_NMT_TYPE) | (NODE_ID << CAN_SHIFT_NMT_SID);
+	txMsg.DataLength = 4;
 	txMsg.RemoteFlag = 0;
 	txMsg.ExtendedFlag = 1;
+	txMsg.Data.words[0] = APP_TYPE;
+	txMsg.Data.words[1] = APP_VERSION;
 	
 	//set up callback for candata
 	bios->can_callback = &can_receive;
+	// Send CAN_NMT_APP_START
+	bios->can_send(&txMsg);
 	
 	printf("AVR Test Application\n");
 	printf("Using AVR BIOS version %x\n", bios->version);
 	
+	txMsg.Id = (CAN_TST << CAN_SHIFT_CLASS) | NODE_ID;
+	txMsg.Data.dwords[0] = 0x01020304;
+	txMsg.Data.dwords[1] = 0xfffefdfc;
+	txMsg.DataLength = 8;
+
 	while (1) {
 		time = bios->timebase_get();
 		if ((time - time1) >= 2000) {
 			//test-send a canmessage
 			bios->can_send(&txMsg);
 			time1 = time;
-			printf("Two seconds passed, time is now %d.\n", (int)time);
-		}
-
-		if ((time - time2) >= 20000) {
-			time2 = time;
-			printf("Will now hang node.\n");
-			cli();
+			printf("Two seconds passed, time is now %ld.\n", time);
 		}
 	}
 	
