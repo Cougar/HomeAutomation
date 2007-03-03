36247ef7aca9bd2ae124c6fc1b62b45f1dcb3c48
eqlazer <eqlazer@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2007-03-03 10:18:19 +0000

    CanRelay: Works with bios.
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@351 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/CanRelay/main.c
+++ EmbeddedSoftware/AVR/CanRelay/main.c
@@ -5,8 +5,6 @@
  * @date    2006-12-17
  * @author  Erik Larsson
  *
- *   Observera! Applikationen är endast testad på labplatta med m88, pot och lysdiod
- *
  *   ADC=Vin*1024/Vref
  *   Vout=( 10 mV/C )( Temperature C ) + 500 mV
  *
@@ -15,8 +13,7 @@
  *
  */
 
-#define DEBUG           1 /* Prints debug messages to uart */
-#define TWO_BUTTON_MODE 1 /* If using two (or just one but nothing more) auxiliary buttons */
+#define TWO_BUTTON_MODE 0 /* If using two (or just one but nothing more) auxiliary buttons */
 
 /*-----------------------------------------------------------------------------
  * Includes
@@ -24,34 +21,30 @@
 /* system files */
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/wdt.h>
 #include <stdio.h>
 /* lib files */
-#include <can.h>
-#include <serial.h>
-#include <timebase.h>
-
+#include <bios.h>
 #include <tc1047.h>
+#include <funcdefs.h>
 
 /* defines */
-#define RELAY_OFF           0x01
-#define RELAY_ON            0x02
-#define FAILSAFE_MODE       0x0F
-#define FAILSAFE_TRESHOLD   60  /* Degrees when nodeRelay goes into failsafe mode */
+#define GROUP_ID		0x01
 
-#define RELAY_CMD_GET       0x1200
-#define RELAY_CMD_SEND      0x1201
-#define RELAY_CMD_ON        0x01
-#define RELAY_CMD_OFF       0x02
-#define RELAY_CMD_TOGGLE    0x03
-#define RELAY_CMD_STATUS    0x04
-#define RELAY_CMD_RESET     0x05
-#define STATUS_SEND_PERIOD  10000 /* milliseconds */
-#define FAILSAFE_SEND_PERIOD    10000 /* milliseconds */
-#define BOUNCE_TIME         10 /* milliseconds */
-#define BUTTON_ID           0x1202
-#define BUTTON1             1
-#define BUTTON2             2
+#define RELAY_OFF			0x01
+#define RELAY_ON			0x02
+#define FAILSAFE_MODE		0x0F
+#define FAILSAFE_TRESHOLD	60  /* Degrees when nodeRelay goes into failsafe mode */
+
+#define RELAY_CMD_ON		0x01
+#define RELAY_CMD_OFF		0x02
+#define RELAY_CMD_TOGGLE	0x03
+#define RELAY_CMD_STATUS	0x04
+#define RELAY_CMD_RESET		0x05
+#define STATUS_SEND_PERIOD	10000 /* milliseconds */
+#define FAILSAFE_SEND_PERIOD	10000 /* milliseconds */
+#define BOUNCE_TIME			10 /* milliseconds */
+#define BUTTON1				1
+#define BUTTON2				2
 
 /*-------------------------------------------------------------------------
  * Global variables
@@ -60,6 +53,10 @@
 Can_Message_t txMsg_Button;
 #endif
 
+uint8_t relayStatus, failsafe_flag;
+uint32_t boardTemperature = 0;
+Can_Message_t txMsg;
+
 /*----------------------------------------------------------------------------
  * Functions
  * -------------------------------------------------------------------------*/
@@ -67,8 +64,59 @@ uint8_t relayOff();
 uint8_t relayOn();
 void relayFailsafe();
 
+void can_receive(Can_Message_t *msg){
+	uint32_t act;
+	uint8_t m, act_type, group, node;
+
+	if( ((msg->Id & CLASS_MASK)>>CLASS_MASK_BITS) == CLASS_ACT){
+
+		act = (msg->Id & TYPE_MASK) >> TYPE_MASK_BITS;
+		act_type = (act & ACT_TYPE_MASK) >> ACT_TYPE_BITS;
+		group = (act & ACT_GROUP_MASK) >> ACT_GROUP_BITS;
+		node = (act & ACT_NODE_MASK) >> ACT_NODE_BITS;
+
+		if( act_type == ACT_TYPE_RELAY && (group == GROUP_ID || node == NODE_ID) ){
+			if(msg->Data.bytes[0] == RELAY_CMD_ON){
+				// Turn relay on
+				relayStatus = relayOn();
+
+			}else if(msg->Data.bytes[0] == RELAY_CMD_OFF){
+				// Turn relay off
+				relayStatus = relayOff();
+
+			}else if(msg->Data.bytes[0] == RELAY_CMD_TOGGLE ){
+					// Toggle relay
+					if(relayStatus == RELAY_OFF){
+						relayStatus = relayOn();
+					}else{
+						relayStatus = relayOff();
+					}
+
+			}else if(msg->Data.bytes[0] == RELAY_CMD_STATUS){
+				// Send relay status to CAN
+				boardTemperature = getTC1047temperature();
+
+				if( (int32_t)boardTemperature >= FAILSAFE_TRESHOLD ){
+					// Goto failsafe mode
+					relayFailsafe();
+				}else{
+					// Transmitt node status
+					txMsg.Data.bytes[0] = boardTemperature & 0x00FF;
+					txMsg.Data.bytes[1] = (boardTemperature & 0xFF00)>>8;
+					txMsg.Data.bytes[2] = relayStatus;
+					txMsg.DataLength = 3;
+					bios->can_send( &txMsg );
+				}
+			}else if(msg->Data.bytes[0] == RELAY_CMD_RESET){
+				// Return to normal operation
+				failsafe_flag = 1;
+			}
+		}
+	}
+}
+
 /*----------------------------------------------------------------------------
- * Interrupt routines
+ * Interrupt routines // FIXME ordna och testa tryckknappar/rotary encoder
  * -------------------------------------------------------------------------*/
 #if TWO_BUTTON_MODE
 ISR( PCINT2_vect )
@@ -85,261 +133,142 @@ ISR( PCINT2_vect )
     if( (PIND & (1<<PD6)) == 0){
         /* Button pressed */
         buttonTime[0] = Timebase_CurrentTime();
-#if DEBUG
-        printf("Button 1 pressed\n");
-#endif
     }else if( (PIND & (1<<PD6)) == 1){
         if( Timebase_PassedTimeMillis( buttonTime[0] ) >= BOUNCE_TIME ){
             /* No bounce, send message */
             txMsg_Button.Id = BUTTON_ID;
             txMsg_Button.DataLength = 1;
             txMsg_Button.Data.bytes[0] = BUTTON1;
-#if DEBUG
-            printf("Button 1 pressed: message sent\n");
-#endif
             return;
         }else{
             /* Just bounces, ignore it */
-#if DEBUG
-            printf("Button 1 just bounced\n");
-#endif
             return;
         }
     }
     if( (PIND & (1<<PD7)) == 0){
         /* Button pressed */
         buttonTime[1] = Timebase_CurrentTime();
-#if DEBUG
-        printf("Button 2 pressed\n");
-#endif
     }else if( (PIND & (1<<PD7)) == 1){
         if( Timebase_PassedTimeMillis( buttonTime[1] ) >= BOUNCE_TIME ){
             /* No bounce, send message */
             txMsg_Button.Id = BUTTON_ID;
             txMsg_Button.DataLength = 1;
             txMsg_Button.Data.bytes[0] = BUTTON2;
-#if DEBUG
-            printf("Button 2 pressed: message sent\n");
-#endif
             return;
         }else{
             /* Just bounces, ignore it */
-#if DEBUG
-            printf("Button 2 just bounced\n");
-#endif
             return;
         }
     }
 }
 #endif
-
-//ISR( ADC_vect )
-//{
-    /*
-     * ADC conversion completed
-     */
-    // TODO använd denna rutin för att läsa värde när omvandlingen är klar
-
-//}
 
 /*-----------------------------------------------------------------------------
  * Main Program
  *---------------------------------------------------------------------------*/
 int main(void) {
 
-    uint8_t relayStatus;
-    uint32_t boardTemperature = 0;
-
-    adcTemperatureInit();
-
-    Timebase_Init();
-#if DEBUG
-    Serial_Init();
-#endif
+	adcTemperatureInit();
 
 #if TWO_BUTTON_MODE
+#error two button mode not yet tested
     /*
      * Initialize buttons
      * It is possible to use ie. sensors instead
      * but then change this
      */
-    /* Set as input and enable pull-up */
+    // Set as input and enable pull-up
     DDRD &= ~( (1<<DDD6)|(1<<DDD7) );
     PORTD |= (1<<PD6)|(1<<PD7);
 
-    /* Enable IO-pin interrupt */
+    // Enable IO-pin interrupt
     PCICR |= (1<<PCIE1);
-    /* Unmask PD6 and PD7 */
+    // Unmask PD6 and PD7
     PCMSK2 |= (1<<PCINT22) | (1<<PCINT23);
 #endif
-    sei();
 
-    /* Turn relay off */
-    relayStatus = relayOff();
+	// Control channel for relay
+	DDRC |= (1<<DDC1);
 
-#if DEBUG
-    printf("\n------------------------------------------------------------\n");
-    printf(  "   CAN: Relay\n");
-    printf(  "------------------------------------------------------------\n");
+	// Set up callback for CAN messages
+	bios->can_callback = &can_receive;
 
+	sei();
 
-    printf("CanInit...");
-    if (Can_Init() != CAN_OK) {
-        printf("FAILED!\n");
-    }else{
-        printf("OK!\n");
-    }
-#else
-    Can_Init();
-#endif
-    uint32_t timeStamp = 0;
+	// Turn relay off
+	relayStatus = relayOff();
 
-    Can_Message_t txMsg;
-    Can_Message_t rxMsg;
-    txMsg.Id = RELAY_CMD_SEND;
-    txMsg.RemoteFlag = 0;
-    txMsg.ExtendedFlag = 1;
-    txMsg.DataLength = 3;
+	uint32_t timeStamp = 0;
 
-    /* main loop */
-    while (1) {
-        /* service the CAN routines */
-        Can_Service();
+	txMsg.RemoteFlag = 0;
+	txMsg.ExtendedFlag = 1;
+	txMsg.Id = ( CLASS_SNS<<CLASS_MASK_BITS )|( SNS_ACT_STATUS_RELAY<<TYPE_MASK_BITS )|(NODE_ID);
 
-        /* check if any messages have been received */
-        while (Can_Receive(&rxMsg) == CAN_OK) {
-            /* This relay is adressed*/
-            if( rxMsg.Id == RELAY_CMD_GET ){
+	// Main loop
+	while (1) {
 
-                if( rxMsg.Data.bytes[0] == RELAY_CMD_ON ){
-                    /* Turn relay on */
-                    relayStatus = relayOn();
+		if( bios->timebase_get() - timeStamp >= STATUS_SEND_PERIOD ){
+			timeStamp = bios->timebase_get();
+			// Send relay status to CAN
 
-                }else if(rxMsg.Data.bytes[0] == RELAY_CMD_OFF){
-                    /* Turn relay off */
-                    relayStatus = relayOff();
+			boardTemperature = getTC1047temperature();
 
-                }else if(rxMsg.Data.bytes[0] == RELAY_CMD_TOGGLE ){
-                    /* Toggle relay */
-
-                    if(relayStatus == RELAY_OFF){
-                        relayStatus = relayOn();
-                    }else{
-                        relayStatus = relayOff();
-                    }
-
-                }else if(rxMsg.Data.bytes[0] == RELAY_CMD_STATUS){
-                    /* Send relay status to CAN */
-
-                    boardTemperature = getTC1047temperature();
-
-                    if( (int32_t)boardTemperature >= FAILSAFE_TRESHOLD ){
-                        relayFailsafe();
-                    }else{
-                        txMsg.Data.bytes[0] = boardTemperature & 0x00FF;
-                        txMsg.Data.bytes[1] = (boardTemperature & 0xFF00)>>8;
-                        txMsg.Data.bytes[2] = relayStatus;
-
-                        Can_Send( &txMsg );
-                    }
-                }
-            }
-        }
-
-        if( Timebase_PassedTimeMillis(timeStamp) >= STATUS_SEND_PERIOD ){
-            timeStamp = Timebase_CurrentTime();
-            /* Send relay status to CAN */
-
-            boardTemperature = getTC1047temperature();
-
-            if( (int32_t)boardTemperature >= FAILSAFE_TRESHOLD ){
-                relayFailsafe();
-            }else{
-                txMsg.Data.bytes[0] = boardTemperature & 0x00FF;
-                txMsg.Data.bytes[1] = (boardTemperature & 0xFF00)>>8;
-                txMsg.Data.bytes[2] = relayStatus;
-
-                Can_Send( &txMsg );
-#if DEBUG
-                printf("Periodic message sent...\n");
-                printf("Temperature: %d\nRelay status: %u\n", boardTemperature, relayStatus);
-#endif
-            }
-        }
-    }
-    return 0;
+			if( (int32_t)boardTemperature >= FAILSAFE_TRESHOLD ){
+				relayFailsafe();
+			}else{
+				txMsg.Data.bytes[0] = boardTemperature & 0x00FF;
+				txMsg.Data.bytes[1] = (boardTemperature & 0xFF00)>>8;
+				txMsg.Data.bytes[2] = relayStatus;
+				txMsg.DataLength = 3;
+				bios->can_send( &txMsg );
+			}
+		}
+	}
+	return 0;
 }
 
 uint8_t relayOff()
 {
-    /* Set as output and sink */
-    PORTC &= ~(1<<PC1);
-    DDRC |= (1<<DDC1);
+	// Turn off relay
+	PORTC &= ~(1<<PC1);
 
-#if DEBUG
-    printf("relay turned off\n");
-#endif
-
-    return RELAY_OFF;
+	return RELAY_OFF;
 }
 
 uint8_t relayOn()
 {
-    /* Set as input and enable pull-up */
-    DDRC &= ~(1<<DDC1);
-    PORTC |= (1<<PC1);
+	// Turn on relay
+	PORTC |= (1<<PC1);
 
-#if DEBUG
-    printf("relay turned on\n");
-#endif
-
-    return RELAY_ON;
+	return RELAY_ON;
 }
 
 void relayFailsafe()
 {
-    uint8_t relayStatus;
-    uint32_t boardTemperature = 0, timeStamp = 0;
+	uint32_t timeStamp = 0;
 
-    Can_Message_t txMsg;
-    Can_Message_t rxMsg;
-    txMsg.Id = RELAY_CMD_SEND;
-    txMsg.RemoteFlag = 0;
-    txMsg.ExtendedFlag = 1;
-    txMsg.DataLength = 4;
+	relayStatus = relayOff();
+	failsafe_flag = 0;
 
-    relayStatus = relayOff();
+	while(1){
 
-    while(1){
-        /* service the CAN routines */
-        Can_Service();
+		if( failsafe_flag ){
+			// Return from failsafe mode
+			return;
+		}
 
-        /* check if any messages have been received */
-        while (Can_Receive(&rxMsg) == CAN_OK) {
-            /* This relay is adressed*/
-            if( rxMsg.Id == RELAY_CMD_GET ){
-                if( rxMsg.Data.bytes[0] == RELAY_CMD_RESET ){
-                    /* Return from failsafe mode */
-                    return;
-                }
-            }
-        }
+		if( bios->timebase_get() - timeStamp >= FAILSAFE_SEND_PERIOD ){
+			timeStamp = bios->timebase_get();
+			// Send relay status to CAN
+			boardTemperature = getTC1047temperature();
 
-        if( Timebase_PassedTimeMillis(timeStamp) >= FAILSAFE_SEND_PERIOD ){
-            timeStamp = Timebase_CurrentTime();
-            /* Send relay status to CAN */
-#if DEBUG
-            printf("Failsafe mode!\n");
-#endif
-            boardTemperature = getTC1047temperature();
+			txMsg.Data.bytes[0] = boardTemperature & 0x00FF;
+			txMsg.Data.bytes[1] = (boardTemperature & 0xFF00)>>8;
+			txMsg.Data.bytes[2] = relayStatus;
+			txMsg.Data.bytes[3] = FAILSAFE_MODE;
 
-            txMsg.Data.bytes[0] = boardTemperature & 0x00FF;
-            txMsg.Data.bytes[1] = (boardTemperature & 0xFF00)>>8;
-            txMsg.Data.bytes[2] = relayStatus;
-            txMsg.Data.bytes[3] = FAILSAFE_MODE;
-
-            Can_Send( &txMsg );
-        }
-    }
+			bios->can_send( &txMsg );
+		}
+	}
 }
 
