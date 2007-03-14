88853cd2277f61b897f48cdbd4cf6030a431c289
johboh <johboh@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2007-03-14 12:39:20 +0000

    Updated pgmdefs.h. Opt. in canIR eeprom downloading fsm. Project file update for canEthernet/freezer/LCD/Temp.
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@365 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/PIC/canIR/Source/Main.c
+++ trunk/EmbeddedSoftware/PIC/canIR/Source/Main.c
@@ -32,6 +32,8 @@ static void loadIrCommands(void);
 
 #define EE_PACKET_TIMOUT 5*TICK_100MS
 #define IR_TIMOUT 2*TICK_100MS
+#define EE_ACK_MAX 7
+
 
 static BOOL hasCtrlPacket = FALSE;
 static CAN_PACKET ctrlPacket;
@@ -65,8 +67,8 @@ void main()
 	static DWORD ee_pgmAddress = 0;
 	static DWORD ee_newPgmAddress = 0;
 	static PGM_CTRL_TYPE ee_ackType = pctEE_ACK;
+	static BYTE ee_ackCount = 0;
 	
-	static BYTE ee_lastLocalIndex = 0;
 	CAN_PACKET outCp;
 
 	// Inits
@@ -91,7 +93,6 @@ void main()
 
 		static TICK tick_led = 0;
 		static TICK tick_ee = 0;
-		
 
 
 		if ((currentTick-tick_led)>(1*TICK_1S))
@@ -119,6 +120,7 @@ void main()
 					ee_ackType = pctEE_ACK;
 					eesAckNext = eesWAIT_FIRST_DATA;
 					ees = eesSEND_ACK;
+					ee_ackCount = 0;
 				}
 				break;
 
@@ -131,6 +133,8 @@ void main()
 				tick_ee = currentTick;
 				ees	= eesAckNext;
 				while(!canSendMessage(outCp,PRIO_HIGH));
+
+				if (++ee_ackCount>EE_ACK_MAX) ees = eesWAITING_START;
 				break;
 
 			case eesWAIT_FIRST_DATA:
@@ -147,7 +151,6 @@ void main()
 				if (hasCtrlPacket == TRUE && ctrlPacket.pgm.id==pctEE_PGM)
 				{
 					BYTE localIndex = ctrlPacket.data[7];
-					ee_lastLocalIndex = localIndex;
 					pgmData[localIndex+0]=ctrlPacket.data[0]; pgmData[localIndex+1]=ctrlPacket.data[1]; pgmData[localIndex+2]=ctrlPacket.data[2]; pgmData[localIndex+3]=ctrlPacket.data[3];
 					pgmData[localIndex+4]=ctrlPacket.data[4]; pgmData[localIndex+5]=ctrlPacket.data[5]; pgmData[localIndex+6]=ctrlPacket.data[6];
 					ees = eesWAIT_DATA_OR_CRC;
@@ -165,6 +168,7 @@ void main()
 
 				if (hasCtrlPacket==TRUE && ctrlPacket.pgm.id==pctEE_DONE)
 				{
+					irValids = (BYTE)((((WORD)ctrlPacket.data[1]<<8) + ctrlPacket.data[0]) >> 3);
 					ees = eesDONE;
 				}
 
@@ -193,13 +197,13 @@ void main()
 						ee_ackType = pctEE_NACK;
 						eesAckNext = eesWAIT_FIRST_DATA;
 						ees = eesSEND_ACK;
+						ee_ackCount = 0;
 					}
 				}
 				
 				if (hasCtrlPacket==TRUE && ctrlPacket.pgm.id==pctEE_PGM)
 				{
 					BYTE localIndex = ctrlPacket.data[7];
-					ee_lastLocalIndex = localIndex;
 					pgmData[localIndex+0]=ctrlPacket.data[0]; pgmData[localIndex+1]=ctrlPacket.data[1]; pgmData[localIndex+2]=ctrlPacket.data[2]; pgmData[localIndex+3]=ctrlPacket.data[3];
 					pgmData[localIndex+4]=ctrlPacket.data[4]; pgmData[localIndex+5]=ctrlPacket.data[5]; pgmData[localIndex+6]=ctrlPacket.data[6];
 				}
@@ -211,6 +215,7 @@ void main()
 
 				if (hasCtrlPacket==TRUE && ctrlPacket.pgm.id==pctEE_DONE)
 				{
+					irValids = (BYTE)((((WORD)ctrlPacket.data[1]<<8) + ctrlPacket.data[0]) >> 3);
 					ees = eesDONE;
 				}
 
@@ -232,7 +237,6 @@ void main()
 					for(i=0;i<56;i++)
 					{
 						EEWrite(ee_pgmAddress+i,pgmData[i]);
-
 						pgmData[i]=0xFF;
 					}	
 
@@ -242,11 +246,12 @@ void main()
 				ee_pgmAddress	= ee_newPgmAddress;
 				ee_ackType 		= pctEE_ACK;		
 				ees 			= eesSEND_ACK;
+				ee_ackCount 	= 0;
 
 			break;
 
 			case eesDONE:
-				EEWrite(EE_IR_START,32);
+				EEWrite(EE_IR_START,irValids);
 				loadIrCommands();
 
 				ees = eesWAITING_START;
@@ -301,32 +306,6 @@ void loadIrCommands()
 		buf_ptr[i].pgm_id = ((WORD)EERead(EE_IR_START+5+(i<<3)) << 8)+(WORD)EERead(EE_IR_START+6+(i<<3));
 		buf_ptr[i].pgm_data[1] = EERead(EE_IR_START+7+(i<<3));
 		buf_ptr[i].pgm_data[0] = EERead(EE_IR_START+8+(i<<3));
-
-		/*
-		buf_ptr[0].repeat = 5;
-		buf_ptr[0].addr = 5;
-		buf_ptr[0].data = 5;
-		buf_ptr[0].pgm_class = 5;
-		buf_ptr[0].pgm_id = 5;
-		buf_ptr[0].pgm_data[1] = 5;
-		buf_ptr[0].pgm_data[0] = 5;
-
-		buf_ptr[1].repeat = 7;
-		buf_ptr[1].addr = 7;
-		buf_ptr[1].data = 7;
-		buf_ptr[1].pgm_class = 7;
-		buf_ptr[1].pgm_id = 7;
-		buf_ptr[1].pgm_data[1] = 7;
-		buf_ptr[1].pgm_data[0] = 7;
-
-		buf_ptr[2].repeat = 9;
-		buf_ptr[2].addr = 9;
-		buf_ptr[2].data = 9;
-		buf_ptr[2].pgm_class = 9;
-		buf_ptr[2].pgm_id = 9;
-		buf_ptr[2].pgm_data[1] = 9;
-		buf_ptr[2].pgm_data[0] = 9;
-		*/
 	}
 }
 
@@ -414,10 +393,11 @@ void irecParse(IR_TYPE type, BYTE toggle, BYTE addr, BYTE data)
 	outCp.data[0]	= data;
 	while(!canSendMessage(outCp,PRIO_HIGH));
 
+	/*
 	// DUMP
-	if (addr==0x15 && data>=0x00)
+	if (addr==0x15 && data==0x00)
 	{
-		for(i=0;i<7;i++)
+		for(i=0;i<irValids;i++)
 		{
 			outCp.type 		= ptPGM;
 			outCp.pgm.class = 0x05;
@@ -434,29 +414,8 @@ void irecParse(IR_TYPE type, BYTE toggle, BYTE addr, BYTE data)
 			while(!canSendMessage(outCp,PRIO_HIGH));
 		}		
 	}
-
-	/*
-	if (addr==0x15 && data>=0x09)
-	{
-		for(i=0;i<7;i++)
-		{
-			outCp.type 		= ptPGM;
-			outCp.pgm.class = 0x05;
-			outCp.pgm.id 	= 0x91;
-			outCp.length 	= 8;
-			outCp.data[7] = pgmData[i*8];
-			outCp.data[6]=pgmData[i*8+1];
-			outCp.data[5]=pgmData[i*8+2];
-			outCp.data[4]=pgmData[i*8+3];
-			outCp.data[3]=pgmData[i*8+4];
-			outCp.data[2]=pgmData[i*8+5];
-			outCp.data[1]=pgmData[i*8+6];
-			outCp.data[0]=pgmData[i*8+7];
-			while(!canSendMessage(outCp,PRIO_HIGH));
-		}
-	}
-	*/
-
+*/
+	
 	// slow? bad?
 	for(i=0;(i<irValids && i<IR_COUNT);i++)
 	{
@@ -473,7 +432,6 @@ void irecParse(IR_TYPE type, BYTE toggle, BYTE addr, BYTE data)
 			if (buf_ptr[i].repeat>0) irCounts = 0;
 		}
 	}
-
 
 	lastToogle	= toggle;
 
