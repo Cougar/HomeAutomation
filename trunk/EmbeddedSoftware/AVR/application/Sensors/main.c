b7e9fdd9c7d0e5642df954b3132862eb6af24217
Anders Runeson <runeson@gmail.com>
2007-06-05 19:55:25 +0000

    app/sensors: some reworked settings moved to settings-file a bug with ds18s20 temperature in interval -1 < x < 0 fixed application-started-message is now sent small changes in includes
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@515 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/application/Sensors/main.c
+++ trunk/EmbeddedSoftware/AVR/application/Sensors/main.c
@@ -14,16 +14,6 @@
 /*-----------------------------------------------------------------------------
  * Defines
  * --------------------------------------------------------------------------*/
-#define USE_DS18S20
-//#define USE_TC1047
-#define USE_LDR
-
-#define DS_SEND_PERIOD	10000
-#define DS_SEND_DELAY	2000
-#define TC_SEND_PERIOD	10000
-#define LDR_SEND_PERIOD	10000
-
-#include <funcdefs/funcdefs.h>
 
 /*-----------------------------------------------------------------------------
  * Includes
@@ -35,21 +25,26 @@
 /* lib files */
 #include <bios.h>
 #include <config.h>
-#include <timebase.h>
+#include <settings.h>
+
+#include <drivers/timer/timebase.h>
 
 #if defined(USE_DS18S20)
-#include <ds18x20.h>
-#include <onewire.h>
-#include <delay.h>
+#include <drivers/sensor/ds18s20/ds18x20.h>
+#include <drivers/sensor/ds18s20/onewire.h>
+#include <drivers/sensor/ds18s20/delay.h>
 #endif
 
 #if defined(USE_TC1047)
-#include <tc1047.h>
+#include <drivers/sensor/tc1047/tc1047.h>
 #endif
 
 #if defined(USE_LDR)
-#include <ldr.h>
+#include <drivers/sensor/ldr/ldr.h>
 #endif
+
+#define APP_TYPE    CAN_APPTYPES_SENSOR
+#define APP_VERSION 0x0001
 
 /*-----------------------------------------------------------------------------
  * Main Program
@@ -59,9 +54,9 @@ int main(void) {
     uint8_t nSensors, i;
     uint8_t subzero, cel, cel_frac_bits;
     uint32_t timeStamp_DS = 0, timeStamp_DS_del = 0;
-	/* This is to identify and give the sensors correct ID (For eqlazers net)
+	/* This is to identify and give the sensors correct ID 
 	 * TODO read ds18s20 serial id and that way give the "can id" */
-	uint16_t sensor_ds_id[] = {SNS_DS18S20_FREEZER, SNS_DS18S20_INSIDE1, SNS_DS18S20_OUTSIDE, SNS_DS18S20_INSIDE2};
+	uint16_t sensor_ds_id[] = {TEMPSENSORID_1, TEMPSENSORID_2, TEMPSENSORID_3, TEMPSENSORID_4};
 #endif
 #if defined(USE_TC1047)
     uint32_t tcTemperature = 0, timeStamp_TC = 0;
@@ -76,18 +71,21 @@ int main(void) {
 
 	Timebase_Init();
 
+    Can_Message_t txMsg;
+	txMsg.Id = (CAN_NMT_APP_START << CAN_SHIFT_NMT_TYPE) | (NODE_ID << CAN_SHIFT_NMT_SID);
+	txMsg.DataLength = 4;
+	txMsg.RemoteFlag = 0;
+	txMsg.ExtendedFlag = 1;
+	txMsg.Data.words[0] = APP_TYPE;
+	txMsg.Data.words[1] = APP_VERSION;
+	BIOS_CanSend(&txMsg);
+
 #if defined(USE_DS18S20)
     /* Make sure there is no more then 4 DS-sensors. */
     nSensors = search_sensors();
 #endif
 
-    Can_Message_t txMsg;
-
-    txMsg.ExtendedFlag = 1;
     txMsg.DataLength = 2;
-
-	/* Set class and sid (sid=NODE_ID from bios.h) */
-	txMsg.Id = (CLASS_SNS << CLASS_MASK_BITS)|(NODE_ID);
 
     /* main loop */
     while (1) {
@@ -96,17 +94,13 @@ int main(void) {
 		if( Timebase_PassedTimeMillis(timeStamp_DS) >= DS_SEND_PERIOD ){
 			timeStamp_DS = Timebase_CurrentTime();
 
-			txMsg.Id &= ~TYPE_MASK; /* Clear TYPE */
-			txMsg.Id |= (SNS_TEMP_DS18S20 << SNS_TYPE_BITS); /* Set SNS_TYPE */
-
             if ( DS18X20_start_meas( DS18X20_POWER_PARASITE, NULL ) == DS18X20_OK) {
                 delay_ms(DS18B20_TCONV_12BIT);
                 for ( i=0; i<nSensors; i++ ) {
-                    if ( DS18X20_read_meas( &gSensorIDs[i][0], &subzero,
-                        &cel, &cel_frac_bits) == DS18X20_OK ) {
+                    if ( DS18X20_read_meas( &gSensorIDs[i][0], &subzero, &cel, &cel_frac_bits) == DS18X20_OK ) {
 
                         if (subzero) {
-                            txMsg.Data.bytes[0] = -cel;
+                            txMsg.Data.bytes[0] = -cel-1;
                             txMsg.Data.bytes[1] = ~(cel_frac_bits<<4);
                         }else{
                             txMsg.Data.bytes[0] = cel;
@@ -118,8 +112,8 @@ int main(void) {
 					while(Timebase_PassedTimeMillis(timeStamp_DS_del) < DS_SEND_DELAY){}
 
 					/* send txMsg */
-					txMsg.Id &= ~SNS_ID_MASK; /* Clear sensor id */
-					txMsg.Id |= ( sensor_ds_id[i] << SNS_ID_BITS); /* Set sensor id */
+					txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_TEMPERATURE << CAN_SHIFT_SNS_TYPE) | (NODE_ID << CAN_SHIFT_SNS_SID));
+					txMsg.Id |= ( sensor_ds_id[i] << CAN_SHIFT_SNS_ID); /* Set sensor id */
 					BIOS_CanSend(&txMsg);
                 }
             }
@@ -136,8 +130,8 @@ int main(void) {
             txMsg.Data.bytes[1] = (tcTemperature & 0xFF00)>>8;
             txMsg.DataLength = 2;
 
-			txMsg.Id &= ~TYPE_MASK; // rensa type
-			txMsg.Id |= (SNS_TEMP_TC1047 << TYPE_MASK_BITS); // sätt korrekt type
+			txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_TEMPERATURE << CAN_SHIFT_SNS_TYPE) | (NODE_ID << CAN_SHIFT_SNS_SID));
+			txMsg.Id |= (TEMPSENSORID_5 << CAN_SHIFT_SNS_ID); // sätt korrekt sensorid
 
             BIOS_CanSend(&txMsg);
         }
@@ -151,10 +145,7 @@ int main(void) {
             txMsg.Data.bytes[0] = ldr;
             txMsg.DataLength = 1;
 
-			txMsg.Id &= ~TYPE_MASK; /* Clear TYPE */
-			txMsg.Id |= (SNS_LIGHT_LDR << SNS_TYPE_BITS); /* Set SNS_TYPE */
-			txMsg.Id &= ~SNS_ID_MASK; /* Clear sensor id */
-			txMsg.Id |= ( SNS_LDR_TEST1 << SNS_ID_BITS); /* Set sensor id */
+			txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_LIGHT << CAN_SHIFT_SNS_TYPE) | (LIGHTSENSORID_1 << CAN_SHIFT_SNS_ID) | (NODE_ID << CAN_SHIFT_SNS_SID));
 
 			BIOS_CanSend(&txMsg);
         }
