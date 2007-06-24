314c1ddf629b86867b498682fbc5d8a51f689d78
Anders Runeson <runeson@gmail.com>
2007-06-24 16:30:15 +0000

    drivers/IrReceive: IR-receive drivers uppdated: Some changes in calling IrReceive-functions Sony/SIRC protocol added
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@520 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/ir/receiver/irreceiver.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/ir/receiver/irreceiver.h
@@ -20,29 +20,34 @@
 #define IR_NO_PROTOCOL		2
 #define IR_TIME_OVFL		3
 #define IR_NO_DATA 			4
+#define IR_NOT_CORRECT_DATA 5
 
 #define CYCLES_PER_US       (F_CPU/1000000)
 
 #define IR_PROTO_RC5		0
 #define IR_RC5_ST_BIT		864*CYCLES_PER_US		//us
 #define IR_RC5_BIT			864*2*CYCLES_PER_US
-#define IR_RC5_TIMEOUT		105		//ms
+#define IR_RC5_TIMEOUT		105						//ms
 
 #define IR_PROTO_RC6		1
-#define IR_RC6_ST_BIT		1*CYCLES_PER_US		//us
-#define IR_RC6_TIMEOUT		1		//ms
+#define IR_RC6_ST_BIT		1*CYCLES_PER_US			//us
+#define IR_RC6_TIMEOUT		1						//ms
 
-#define IR_PROTO_RCMM		2
-#define IR_RCMM_ST_BIT		256*CYCLES_PER_US		//us //Wont work together with SHARP
-#define IR_RCMM_TIMEOUT		158		//ms
+#define IR_PROTO_RCMM		2								//Wont work together with SHARP
+#define IR_RCMM_ST_BIT		256*CYCLES_PER_US		//us
+#define IR_RCMM_TIMEOUT		158						//ms
 
 #define IR_PROTO_SIRC		3
-#define IR_SIRC_ST_BIT		2400*CYCLES_PER_US	//us
-#define IR_SIRC_TIMEOUT		40		//ms
+#define IR_SIRC_ST_BIT		2400*CYCLES_PER_US		//us
+#define IR_SIRC_TIMEOUT		40						//ms
+#define IR_SIRC_ZERO_LOW	500*CYCLES_PER_US		//us
+#define IR_SIRC_ZERO_HIGH	700*CYCLES_PER_US		//us
+#define IR_SIRC_ONE_LOW		1100*CYCLES_PER_US		//us
+#define IR_SIRC_ONE_HIGH	1300*CYCLES_PER_US		//us
 
-#define IR_PROTO_SHARP		4
-#define IR_SHARP_ST_BIT		275*CYCLES_PER_US		//us //Wont work together with RCMM
-#define IR_SHARP_TIMEOUT	55		//ms
+#define IR_PROTO_SHARP		4								//Wont work together with RCMM
+#define IR_SHARP_ST_BIT		275*CYCLES_PER_US		//us
+#define IR_SHARP_TIMEOUT	55						//ms
 
 #define IR_MIN_REPEATE_PULSE_WIDTH	200
 #define IR_MAX_REPEATE_PULSE_WIDTH	40000
@@ -51,14 +56,19 @@
 /*-----------------------------------------------------------------------------
  * Public Function Prototypes
  *---------------------------------------------------------------------------*/
+void IrReceive_Init(void);
+uint8_t IrReceive_CheckIR(uint8_t *proto, uint8_t *address, uint8_t *command, uint16_t *timeout);
+uint8_t IrReceive_CheckIdle(void);
+
+/*-----------------------------------------------------------------------------
+ * Private Function Prototypes
+ *---------------------------------------------------------------------------*/
+uint8_t receiveRC5(uint8_t *address, uint8_t *command);
+uint8_t receiveSIRC(uint8_t *address, uint8_t *command);
 void initTimer(void);
-int isTimerOvfl(void);
+uint8_t isTimerOvfl(void);
 uint16_t getTimerVal(void);
 void setTimerVal(uint16_t value);
-int receiveRC5(uint8_t *address, uint8_t *command);
-int checkIr(uint8_t *proto, uint8_t *address, uint8_t *command);
-int checkIrIdle(void);
-uint16_t getLastProtoTimeout(void);
 
 
 #endif /*MCU_H_*/
