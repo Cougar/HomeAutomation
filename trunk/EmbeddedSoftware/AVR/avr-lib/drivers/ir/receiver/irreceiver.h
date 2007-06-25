f02b7ed14bc4f5f70c48bc02516adfd0a21fa4d0
Anders Runeson <runeson@gmail.com>
2007-06-25 05:47:22 +0000

    drivers/ir: moved common defines to a ircommon.h
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@521 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/avr-lib/drivers/ir/receiver/irreceiver.h
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/ir/receiver/irreceiver.h
@@ -1,11 +1,12 @@
-#ifndef IR_H_
-#define IR_H_
+#ifndef IRRECEIVE_H_
+#define IRRECEIVE_H_
 
 
 /*-----------------------------------------------------------------------------
  * Includes
  *---------------------------------------------------------------------------*/
 #include <stdio.h>
+#include <drivers/ir/ircommon.h>
 
 /*-----------------------------------------------------------------------------
  * Defines
@@ -14,40 +15,6 @@
 #define IRPIN	PIND
 #define IRDDR	DDRD
 #define IRBIT	PD2
-
-#define IR_OK 				1
-#define IR_FAILED 			-1	//?
-#define IR_NO_PROTOCOL		2
-#define IR_TIME_OVFL		3
-#define IR_NO_DATA 			4
-#define IR_NOT_CORRECT_DATA 5
-
-#define CYCLES_PER_US       (F_CPU/1000000)
-
-#define IR_PROTO_RC5		0
-#define IR_RC5_ST_BIT		864*CYCLES_PER_US		//us
-#define IR_RC5_BIT			864*2*CYCLES_PER_US
-#define IR_RC5_TIMEOUT		105						//ms
-
-#define IR_PROTO_RC6		1
-#define IR_RC6_ST_BIT		1*CYCLES_PER_US			//us
-#define IR_RC6_TIMEOUT		1						//ms
-
-#define IR_PROTO_RCMM		2								//Wont work together with SHARP
-#define IR_RCMM_ST_BIT		256*CYCLES_PER_US		//us
-#define IR_RCMM_TIMEOUT		158						//ms
-
-#define IR_PROTO_SIRC		3
-#define IR_SIRC_ST_BIT		2400*CYCLES_PER_US		//us
-#define IR_SIRC_TIMEOUT		40						//ms
-#define IR_SIRC_ZERO_LOW	500*CYCLES_PER_US		//us
-#define IR_SIRC_ZERO_HIGH	700*CYCLES_PER_US		//us
-#define IR_SIRC_ONE_LOW		1100*CYCLES_PER_US		//us
-#define IR_SIRC_ONE_HIGH	1300*CYCLES_PER_US		//us
-
-#define IR_PROTO_SHARP		4								//Wont work together with RCMM
-#define IR_SHARP_ST_BIT		275*CYCLES_PER_US		//us
-#define IR_SHARP_TIMEOUT	55						//ms
 
 #define IR_MIN_REPEATE_PULSE_WIDTH	200
 #define IR_MAX_REPEATE_PULSE_WIDTH	40000
@@ -71,4 +38,4 @@ uint16_t getTimerVal(void);
 void setTimerVal(uint16_t value);
 
 
-#endif /*MCU_H_*/
+#endif /*IRRECEIVE_H_*/
