5d7720a11abc0ab4775bc7f164809c7897e025d1
Anders Runeson <runeson@gmail.com>
2007-07-02 19:14:00 +0000

    drivers/IR: receiver now receives irdata before parsing it, might be a limitation for some protocol but should not
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@526 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/ir/receiver/irreceiver.c
+++ EmbeddedSoftware/AVR/avr-lib/drivers/ir/receiver/irreceiver.c
@@ -17,7 +17,8 @@
 /*-----------------------------------------------------------------------------
  * Globals
  *---------------------------------------------------------------------------*/
-
+static uint16_t times[MAX_NR_TIMES];			//stores 
+static uint8_t timesCounter=0;					//counts the items in Times, always less then MAX_NR_TIMES
 
 /*-----------------------------------------------------------------------------
  * Prerequisites
@@ -28,17 +29,16 @@
  * Public Functions
  *---------------------------------------------------------------------------*/
 
-//TODO: ändra prescaler till /8
 //TODO: skriv doxygen-header som för de andra funktionerna
 void initTimer(void) {
 	//sätt upp timer
 	#if defined(__AVR_ATmega8__)
-	TCCR1B = (0<<CS12) | (1<<CS11) | (0<<CS10); // prescaler: 1
+	TCCR1B = (0<<CS12) | (1<<CS11) | (0<<CS10); // prescaler: 8
 	TIFR  |= (1<<TOV1);  			// clear overflow flag.
 	TCNT1 = 0;
 	#endif
 	#if defined(__AVR_ATmega88__) || defined(__AVR_ATmega168__)
-	TCCR1B = (0<<CS12) | (1<<CS11) | (0<<CS10); // prescaler: 1
+	TCCR1B = (0<<CS12) | (1<<CS11) | (0<<CS10); // prescaler: 8
 	TIFR1  |= (1<<TOV1);  			// clear overflow flag.
 	TCNT1 = 0;
 	#endif
@@ -75,6 +75,179 @@ void setTimerVal(uint16_t value) {
 	#endif
 }
 
+/**
+ * Get an ir-time from the stored array, this can be used
+ * when a proper protocol is not found.
+ * 
+ * @param index
+ * 		Which time to get
+ * @return
+ * 		The 16bit value at index index
+ */
+uint16_t getRawData(uint8_t index) {
+	return times[index];
+}
+
+/**
+ * Get the number of stored ir-times in the array
+ * 
+ * @return
+ * 		The number of stored ir-times in the array
+ */
+uint8_t getRawDataCnt(void) {
+	return timesCounter;
+}
+
+#if 1
+
+/**
+ * Test data on SIRC protocol, 12-bit version
+ * http://www.sbprojects.com/knowledge/ir/sirc.htm
+ * 
+ * @param address
+ * 		Pointer to store the address of the received data
+ * @param command
+ * 		Pointer to store the command of the received data
+ * @return
+ * 		IR_OK if data parsed successfully, one of several errormessages if not
+ */
+uint8_t testSIRC(uint8_t *address, uint8_t *command) {
+	/* parse times[], max is timesCounter */
+
+	/* check if we have correct amount of data */ 
+	if (timesCounter != 25) {
+		return IR_NOT_CORRECT_DATA;
+	}
+	
+	/* check startbit */
+	if (times[0] > IR_SIRC_ST_BIT + IR_SIRC_ST_BIT/5 || times[0] < IR_SIRC_ST_BIT - IR_SIRC_ST_BIT/5) {
+		return IR_NOT_CORRECT_DATA;
+	}
+	
+	uint16_t rawbits=0;
+	
+	for (uint8_t i = 1; i < timesCounter; i++) {
+		if ((i&1) == 1) {		/* if odd, ir-pause */
+			/* check length of pause between bits */
+			if (times[i] > IR_SIRC_LOW + IR_SIRC_LOW/5 || times[i] < IR_SIRC_LOW - IR_SIRC_LOW/5) {
+				return IR_NOT_CORRECT_DATA;
+			}
+		} else {			/* if even, ir-bit */
+			if (times[i] > IR_SIRC_HIGH_ONE - IR_SIRC_HIGH_ONE/5 && times[i] < IR_SIRC_HIGH_ONE + IR_SIRC_HIGH_ONE/5) {
+				/* write a one */
+				rawbits |= 1<<((i-2)>>1);
+			} else if (times[i] > IR_SIRC_HIGH_ZERO - IR_SIRC_HIGH_ZERO/5 && times[i] < IR_SIRC_HIGH_ZERO + IR_SIRC_HIGH_ZERO/5) {
+				/* do nothing, a zero is already in rawbits */
+			} else {
+				return IR_NOT_CORRECT_DATA;
+			}
+		}
+	}
+	
+	*command = ((uint8_t)rawbits&0x7f);
+	*address = ((uint8_t)(rawbits>>7)&0x1f);
+	
+	return IR_OK;
+}
+
+/**
+ * Test data on RC5 protocol 
+ * http://www.sbprojects.com/knowledge/ir/rc5.htm
+ * 
+ * @param address
+ * 		Pointer to store the address of the received data
+ * @param command
+ * 		Pointer to store the command of the received data
+ * @return
+ * 		IR_OK if data parsed successfully, one of several errormessages if not
+ */
+uint8_t testRC5(uint8_t *address, uint8_t *command) {
+	uint8_t halfbitscnt = 1;
+	uint16_t rawbits = 0;
+	
+	for (uint8_t i = 0; i<timesCounter; i++) {
+		//halfbitscnt&1==1 in the middle of bits
+		//i&1==0 positive flank
+
+		if ((halfbitscnt&1)==1 && (i&1)==0) {		/* in the middle of bit AND a positve flank */
+			rawbits |= (1<<(13-(halfbitscnt>>1)));
+		}
+		
+		if (times[i] > IR_RC5_HALF_BIT - IR_RC5_HALF_BIT/5 && times[i] < IR_RC5_HALF_BIT + IR_RC5_HALF_BIT/5) {
+			halfbitscnt += 1;
+		} else if (times[i] > IR_RC5_BIT - IR_RC5_BIT/5 && times[i] < IR_RC5_BIT + IR_RC5_BIT/5) {
+			halfbitscnt += 2;
+		} else {
+			return IR_NOT_CORRECT_DATA;
+		}
+		
+	}
+
+	*command = ((uint8_t)rawbits&0x3f);
+	*address = ((uint8_t)(rawbits>>6)&0x7f);	
+
+	//support RC5-extended by putting the inversion of startbit 2 as the 7th commandbit
+	*command |= (~(*address) & 0x40);
+	*address &= 0x1f;		//remove togglebit and both startbits from address
+	
+	return IR_OK;
+}
+	
+uint8_t IrReceive_CheckIR(uint8_t *proto, uint8_t *address, uint8_t *command, uint16_t *timeout) {
+	if (IRPIN & (1<<IRBIT)) return IR_NO_DATA;		//om irmodulen ger en etta så återgå
+	
+	initTimer();
+	
+	//this routine should sample the timings for received irdata
+	timesCounter = 0;
+	
+	uint8_t gotTimout = 0;	// a timeout is a very long break in irdata (such as occurs between ir pulsetrains)
+	while (!gotTimout && timesCounter < MAX_NR_TIMES-1) { //loop as long as no timeout occured and not to many pulses received
+		setTimerVal(TIM_OVFL_RELOAD_VAL);
+		//wait for positiv slope, while checking timer overflow
+		while (!(IRPIN & (1<<IRBIT))) {
+			//if timeout (if timer-ovflow-flaggan is set)
+			if (isTimerOvfl() == 1) return IR_TIME_OVFL;	//this timeout is not good (irsignal low which means active)
+		}
+		
+		times[timesCounter] = getTimerVal()-TIM_OVFL_RELOAD_VAL;
+		//TODO: should we check times[timesCounter] so its a realistic time-value? 
+		timesCounter++;
+		
+		setTimerVal(TIM_OVFL_RELOAD_VAL);
+		//wait for negative slope, while checking timer overflow
+		while ((IRPIN & (1<<IRBIT)) && !gotTimout) {
+			//om timeout (om timer-ovflow-flaggan sätt)
+			if (isTimerOvfl() == 1) gotTimout=1;	//return IR_TIME_OVFL;
+		}
+
+		if (!gotTimout) {
+			times[timesCounter] = getTimerVal()-TIM_OVFL_RELOAD_VAL;
+			//TODO: should we check times[timesCounter] so its a realistic time-value? 
+			timesCounter++;
+		}
+	}
+	if (timesCounter >= MAX_NR_TIMES-1) {
+		//to much irdata, not good, abort
+		return IR_TO_MUCH_DATA;
+	}
+	
+	if (testSIRC(&*address, &*command) == IR_OK) {
+		*proto = IR_PROTO_SIRC;
+		*timeout = IR_SIRC_REPETITION;
+		return IR_OK;
+	} else if (testRC5(&*address, &*command) == IR_OK) {
+		*proto = IR_PROTO_RC5;
+		*timeout = IR_RC5_REPETITION;
+		return IR_OK;
+	}
+		
+	return IR_NO_PROTOCOL;
+	
+}
+#endif
+
+#if 0
 /**
  * Receive RC5 data, http://www.sbprojects.com/knowledge/ir/rc5.htm
  * 
@@ -196,18 +369,6 @@ uint8_t receiveSIRC(uint8_t *address, uint8_t *command) {
 	return IR_OK;
 }
 
-//TODO: skriv doxygen-header som för de andra funktionerna
-/**
- * 
- * 
- * 
- * 
- */
-void IrReceive_Init(void) {
-	IRDDR &= ~(1<<IRBIT);
-}
-
-
 /**
  * Checks if ir-receiver is outputting data, if so then receive it 
  * 
@@ -259,8 +420,20 @@ uint8_t IrReceive_CheckIR(uint8_t *proto, uint8_t *address, uint8_t *command, ui
 	
 	return IR_NO_PROTOCOL;
 }
+#endif
 
-//varför är denna funktion så komplex?
+//TODO: skriv doxygen-header som för de andra funktionerna
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+void IrReceive_Init(void) {
+	IRDDR &= ~(1<<IRBIT);
+}
+
+//varför är denna funktion så komplex? jo den filtrerar bort korta ir-pulser
 //TODO: skriv doxygen-header som för de andra funktionerna
 uint8_t IrReceive_CheckIdle(void) {
 	if (IRPIN & (1<<IRBIT)) return IR_NO_DATA;		//om irmodulen ger en etta så återgå
