cc3b8f18136c662a7b86d4ab0476f790e3918102
Anders Runeson <runeson@gmail.com>
2007-07-02 19:14:50 +0000

    app/IrReceive: main.c updated to work with new drivers
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@527 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/application/IrReceive/main.c
+++ trunk/EmbeddedSoftware/AVR/application/IrReceive/main.c
@@ -9,11 +9,14 @@
 #include <drivers/ir/receiver/irreceiver.h>
 
 #define APP_TYPE    CAN_APPTYPES_IRRECEIVER
-#define APP_VERSION 0x0002
+#define APP_VERSION 0x0003
 
 #define STATE_IDLE		0
 #define STATE_IR_REPEAT	1
 
+#define IR_ID_DATA	0
+#define IR_ID_RAW	1
+#define IR_ID_DEBUG	10
 
 // A simple message "queue", with space for one message only.
 // These are declared volatile to tell the compiler not to optimize away accesses.
@@ -51,7 +54,7 @@ int main(void)
 	// Send CAN_NMT_APP_START
 	BIOS_CanSend(&txMsg);
 
-	txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_IR << CAN_SHIFT_SNS_TYPE) | (NODE_ID << CAN_SHIFT_SNS_SID));
+	//txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_IR << CAN_SHIFT_SNS_TYPE) | (IR_ID_DATA<<CAN_SHIFT_SNS_ID) | (NODE_ID << CAN_SHIFT_SNS_SID));
 	
 	unsigned long time = Timebase_CurrentTime();
 	unsigned long irTimeoutTime = 100;
@@ -61,6 +64,7 @@ int main(void)
 	uint8_t state = STATE_IDLE;
 
 	while (1) {
+		#if 0
 		time = Timebase_CurrentTime();
 		if (state == STATE_IDLE) {
 			if (IrReceive_CheckIR(&ir_protocol, &ir_address, &ir_command, &ir_timeout) == IR_OK) {
@@ -69,6 +73,7 @@ int main(void)
 				txMsg.Data.bytes[2] = ir_address;
 				txMsg.Data.bytes[3] = ir_command;
 				txMsg.DataLength = 4;
+				txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_IR << CAN_SHIFT_SNS_TYPE) | (IR_ID_DATA<<CAN_SHIFT_SNS_ID) | (NODE_ID << CAN_SHIFT_SNS_SID));
 				BIOS_CanSend(&txMsg);
 				irTimeoutTime = Timebase_CurrentTime();
 				state = STATE_IR_REPEAT;
@@ -84,10 +89,87 @@ int main(void)
 				irTimeoutTime = Timebase_CurrentTime();
 			}
 		}
+		#endif
+		
+		#if 1
+		time = Timebase_CurrentTime();
+		
+		if (state == STATE_IDLE) {
+			/* test for irdata */
+			uint8_t returnval = IrReceive_CheckIR(&ir_protocol, &ir_address, &ir_command, &ir_timeout);
+			if (returnval == IR_OK) {
+				/* a protocol was found for irdata, send on can */
+				txMsg.Data.bytes[0] = 0x00;
+				txMsg.Data.bytes[1] = ir_protocol;
+				txMsg.Data.bytes[2] = ir_address;
+				txMsg.Data.bytes[3] = ir_command;
+				txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_IR << CAN_SHIFT_SNS_TYPE) | (IR_ID_DATA<<CAN_SHIFT_SNS_ID) | (NODE_ID << CAN_SHIFT_SNS_SID));
+				txMsg.DataLength = 4;
+				BIOS_CanSend(&txMsg);
+				
+				/* set up timeout */
+				irTimeoutTime = Timebase_CurrentTime();
+				state = STATE_IR_REPEAT;
+				
+			} else if (returnval == IR_NO_PROTOCOL) {
+				/* the protocol is unknown so the raw ir-data is sent, makes it easier to develop a new protocol */
+				ir_protocol = IR_PROTO_UNKNOWN; 	//used in STATE_IR_REPEATE, to know if to send a release
+				txMsg.DataLength = 8;
+				txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_IR << CAN_SHIFT_SNS_TYPE) | (IR_ID_RAW<<CAN_SHIFT_SNS_ID) | (NODE_ID << CAN_SHIFT_SNS_SID));
+				uint8_t datacnt = getRawDataCnt();
+				for (uint8_t i = 0; i < datacnt>>2; i++) {
+					uint8_t index = i<<2;
+					txMsg.Data.words[0] = getRawData(index);
+					txMsg.Data.words[1] = getRawData(index+1);
+					txMsg.Data.words[2] = getRawData(index+2);
+					txMsg.Data.words[3] = getRawData(index+3);
+					
+					/* buffers will be filled when sending more than 2-3 messages, so retry until sent */
+					while (BIOS_CanSend(&txMsg) != CAN_OK) {}
+				}
+				
+				uint8_t lastpacketcnt = datacnt&0x03;
+				if (lastpacketcnt > 0) {
+					txMsg.DataLength = lastpacketcnt<<1;
+					for (uint8_t i = 0; i < lastpacketcnt; i++) {
+						txMsg.Data.words[i] = getRawData((datacnt&0xfc)|i);
+					}
+					BIOS_CanSend(&txMsg);
+				}
+
+				irTimeoutTime = Timebase_CurrentTime();
+				state = STATE_IR_REPEAT;
+				ir_timeout = 200;			/* Set a default timeout */
+			} else if (returnval != IR_NO_DATA) {
+				/* debug, send the returned message on errors (except for IR_NO_DATA-errors) */
+				txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_IR << CAN_SHIFT_SNS_TYPE) | (IR_ID_DEBUG<<CAN_SHIFT_SNS_ID) | (NODE_ID << CAN_SHIFT_SNS_SID));
+				txMsg.DataLength = 1;
+				txMsg.Data.bytes[0] = returnval;
+				BIOS_CanSend(&txMsg);
+			}
+
+		} else if (state == STATE_IR_REPEAT) {
+			/* check for new ir-data, if no new data during the timout-time then set state to IDLE */
+			if (time - irTimeoutTime >= ir_timeout) {		
+				state = STATE_IDLE;
+				/* if the protocol was known then send a release-message */
+				if (ir_protocol != IR_PROTO_UNKNOWN) {
+					txMsg.Data.bytes[0] = 0x0f;
+					BIOS_CanSend(&txMsg);
+				}
+			}
+			if (IrReceive_CheckIdle() == IR_OK) {
+				irTimeoutTime = Timebase_CurrentTime();
+			}
+		}
+			
+		#endif
 
 		
 		if (rxMsgFull) {
-			// Flush the received message
+			/* No message reception functionality implemented */
+			
+			/* Flush the received message */
     		rxMsgFull = 0; //  
 		}
 	}
