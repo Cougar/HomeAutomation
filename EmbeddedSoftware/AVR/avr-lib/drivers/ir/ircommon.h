be8b3f9551400e81c2e9548609fcb058e5e6eb40
Anders Runeson <runeson@gmail.com>
2007-07-06 14:54:29 +0000

    drivers/irreceive: Samsung protocol implemented (very much like NEC)
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@531 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/ir/ircommon.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/ir/ircommon.h
@@ -29,18 +29,21 @@
 #define IR_RC5_BIT			889*2*CYCLES_PER_US/TIMER_PRESC		//us
 #define IR_RC5_REPETITION	105									//ms	(time between pulsetrains)
 #define IR_RC5_F_MOD		36									//kHz	(modulation frequency)
+#define IR_RC5_TOL_DIV		4
 
 /* not implemented */
 #define IR_PROTO_RC6		1
 #define IR_RC6_ST_BIT		1*CYCLES_PER_US/TIMER_PRESC			//us
 #define IR_RC6_REPETITION	1									//ms	(time between pulsetrains)
 #define IR_RC6_F_MOD		36									//kHz	(modulation frequency)
+#define IR_RC6_TOL_DIV		4
 
 /* not implemented */
 #define IR_PROTO_RCMM		2								//Wont work together with SHARP
 #define IR_RCMM_ST_BIT		256*CYCLES_PER_US/TIMER_PRESC		//us
 #define IR_RCMM_REPETITION 158									//ms	(time between pulsetrains)
 #define IR_RCMM_F_MOD		36									//kHz	(modulation frequency)
+#define IR_RCMM_TOL_DIV		4
 
 #define IR_PROTO_SIRC		3
 #define IR_SIRC_ST_BIT		2400*CYCLES_PER_US/TIMER_PRESC		//us
@@ -49,12 +52,7 @@
 #define IR_SIRC_LOW			600*CYCLES_PER_US/TIMER_PRESC		//us
 #define IR_SIRC_HIGH_ONE	1200*CYCLES_PER_US/TIMER_PRESC		//us
 #define IR_SIRC_HIGH_ZERO	600*CYCLES_PER_US/TIMER_PRESC		//us
-#if 0
-#define IR_SIRC_ZERO_LOW	450*CYCLES_PER_US/TIMER_PRESC		//us
-#define IR_SIRC_ZERO_HIGH	750*CYCLES_PER_US/TIMER_PRESC		//us
-#define IR_SIRC_ONE_LOW		1050*CYCLES_PER_US/TIMER_PRESC		//us
-#define IR_SIRC_ONE_HIGH	1350*CYCLES_PER_US/TIMER_PRESC		//us
-#endif
+#define IR_SIRC_TOL_DIV		4
 
 #define IR_PROTO_SHARP		4								//Wont work together with RCMM
 #define IR_SHARP_HIGH		280*CYCLES_PER_US/TIMER_PRESC		//us
@@ -62,16 +60,27 @@
 #define IR_SHARP_LOW_ZERO	780*CYCLES_PER_US/TIMER_PRESC		//us
 #define IR_SHARP_REPETITION	55									//ms	(time between pulsetrains)
 #define IR_SHARP_F_MOD		38									//kHz	(modulation frequency)
+#define IR_SHARP_TOL_DIV	4
 
 #define IR_PROTO_NEC		5
 #define IR_NEC_ST_BIT		9000*CYCLES_PER_US/TIMER_PRESC		//us
-#define IR_NEC_ST_PAUSE_BIT	4500*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_NEC_ST_PAUSE		4500*CYCLES_PER_US/TIMER_PRESC		//us
 #define IR_NEC_REPETITION	110									//ms	(time between pulsetrains)
 #define IR_NEC_F_MOD		38									//kHz	(modulation frequency)
 #define IR_NEC_LOW_ONE		1690*CYCLES_PER_US/TIMER_PRESC		//us
 #define IR_NEC_LOW_ZERO		560*CYCLES_PER_US/TIMER_PRESC		//us
-#define IR_NEC_HIGH		560*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_NEC_HIGH			560*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_NEC_TOL_DIV		4
 
+#define IR_PROTO_SAMS		6
+#define IR_SAMS_ST_BIT		4500*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_SAMS_ST_PAUSE	4500*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_SAMS_REPETITION	55									//ms	(time between pulsetrains)
+#define IR_SAMS_F_MOD		38									//kHz	(modulation frequency)
+#define IR_SAMS_LOW_ONE		1690*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_SAMS_LOW_ZERO	560*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_SAMS_HIGH		560*CYCLES_PER_US/TIMER_PRESC		//us
+#define IR_SAMS_TOL_DIV		3
 
 #define IR_PROTO_UNKNOWN	0xff								//
 
