9eb5e04fbe1cca164536025fd943560d5dd8fd25
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2007-07-20 09:54:53 +0000

    * avr-lib: Changed drivers/ir/transceiver to handle basic transmit functions, but it does not yet modulate the signal.
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@552 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/avr-lib/drivers/ir/transceiver/irtransceiver.c
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/ir/transceiver/irtransceiver.c
@@ -20,7 +20,9 @@
  *---------------------------------------------------------------------------*/
 static uint16_t times[MAX_NR_TIMES];			//stores 
 static uint8_t timesCounter=0;					//counts the items in Times, always less then MAX_NR_TIMES
+static uint8_t timesIndex;						//selects the pulse width in Times to send
 volatile uint8_t data_received;
+volatile uint8_t data_transmitted;
 uint8_t detect_edge;
 uint8_t store;
 
@@ -33,24 +35,25 @@ uint8_t store;
 #define ICIE1	TICIE1
 #endif
 
-#define IR_COMP_VECTOR	TIMER1_COMPB_vect
-#define IR_OVF_VECTOR	TIMER1_COMPA_vect
-#define IR_CAPT_VECTOR	TIMER1_CAPT_vect
-#define IR_TIMEOUT_REG	OCR1A
-#define IR_PW_REG		OCR1B
-#define IR_CAPT_REG		ICR1
-#define IR_TIMER_INIT()	TCCR1A = 0; \
-						TCCR1B = (1<<ICNC1)|(1<<WGM12)|(2<<CS10);
-#define IR_MASK_COMP()	TIMSK1 &= ~(1<<OCIE1B);
-#define IR_UNMASK_COMP()	TIFR1 = (1<<OCF1B); TIMSK1 |= (1<<OCIE1B);
-#define IR_MASK_OVF()	TIMSK1 &= ~(1<<OCIE1A);
-#define IR_UNMASK_OVF()	TIFR1 = (1<<OCF1A); TIMSK1 |= (1<<OCIE1A);
-#define IR_MASK_CAPT()	TIMSK1 &= ~(1<<ICIE1);
-#define IR_UNMASK_CAPT()	TIFR1 = (1<<ICF1); TIMSK1 |= (1<<ICIE1);
-#define IR_CAPT_FALLING()	TCCR1B &= ~(1<<ICES1); \
-							TIFR1 = (1<<ICF1);
-#define IR_CAPT_RISING()	TCCR1B |= (1<<ICES1); \
-							TIFR1 = (1<<ICF1);
+#define IR_COMPARE_VECTOR		TIMER1_COMPB_vect
+#define IR_TIMEOUT_VECTOR		TIMER1_COMPA_vect
+#define IR_CAPTURE_VECTOR		TIMER1_CAPT_vect
+#define IR_TIMEOUT_REG			OCR1A
+#define IR_COMPARE_REG			OCR1B
+#define IR_CAPTURE_REG			ICR1
+#define IR_COUNT_REG			TCNT1
+#define IR_TIMER_INIT()			TCCR1A = 0; \
+								TCCR1B = (1<<ICNC1)|(2<<CS10);
+#define IR_MASK_COMPARE()		TIMSK1 &= ~(1<<OCIE1B);
+#define IR_UNMASK_COMPARE()		TIFR1 = (1<<OCF1B); TIMSK1 |= (1<<OCIE1B);
+#define IR_MASK_TIMEOUT()		TIMSK1 &= ~(1<<OCIE1A);
+#define IR_UNMASK_TIMEOUT()		TIFR1 = (1<<OCF1A); TIMSK1 |= (1<<OCIE1A);
+#define IR_MASK_CAPTURE()		TIMSK1 &= ~(1<<ICIE1);
+#define IR_UNMASK_CAPTURE()		TIFR1 = (1<<ICF1); TIMSK1 |= (1<<ICIE1);
+#define IR_CAPTURE_FALLING()	TCCR1B &= ~(1<<ICES1); \
+								TIFR1 = (1<<ICF1);
+#define IR_CAPTURE_RISING()		TCCR1B |= (1<<ICES1); \
+								TIFR1 = (1<<ICF1);
 
 #define IR_RX_ACTIVE_LOW	1
 #define IR_TX_ACTIVE_LOW	1
@@ -59,49 +62,61 @@ uint8_t store;
  * Interrupt Handlers
  *---------------------------------------------------------------------------*/
 
-ISR(IR_COMP_VECTOR)
+ISR(IR_COMPARE_VECTOR)
 {
-	
+	PORTC ^= (1<<PC5);
+	if (timesIndex < timesCounter)
+	{
+		IR_COMPARE_REG += times[timesIndex++];		
+	}
+	else
+	{
+		IR_MASK_COMPARE();
+		data_transmitted = 1;
+	}
 }
 
-ISR(IR_OVF_VECTOR)
+ISR(IR_TIMEOUT_VECTOR)
 {
 	if (timesCounter)
 	{
 		/* Disable interrupts until the application has taken action on the
 		 * current data and reenabled reception. */
-		IR_MASK_CAPT();
-		IR_MASK_OVF();
+		IR_MASK_CAPTURE();
+		IR_MASK_TIMEOUT();
 		/* Notify the application that a pulse train has been received. */
 		data_received = 1;
 	}
 	store = 0;
 }
 
-ISR(IR_CAPT_VECTOR)
+ISR(IR_CAPTURE_VECTOR)
 {
-	/* Read the measured pulse time from the capture register. */
-	uint16_t pulsewidth = IR_CAPT_REG;
-	
-	/* Subtract the current measurement from the counter instead of setting it
-	 * to zero so that the following measurement is not affected by variations
-	 * in the interrupt response time. */ 
-	TCNT1 -= pulsewidth;
-	//TCNT1 = 0;
+	static uint16_t prev_time;
+	uint16_t pulsewidth;
+
+	/* Read the measured transition time from the capture register. */
+	uint16_t time = IR_CAPTURE_REG;
 	
 	/* Toggle the edge detection. */
 	if (detect_edge == 0)
 	{
-		IR_CAPT_RISING();
+		IR_CAPTURE_RISING();
 		detect_edge = 1;
-		PORTC &= ~(1<<PC5);
 	}
 	else
 	{
-		IR_CAPT_FALLING();
+		IR_CAPTURE_FALLING();
 		detect_edge = 0;
-		PORTC |= (1<<PC5);
 	}
+	
+	/* Subtract the current measurement from the previous to get the pulse
+	 * width. */
+	pulsewidth = time - prev_time;
+	prev_time = time;
+	
+	/* Set the timeout. */
+	IR_TIMEOUT_REG = time + IR_MAX_PULSE_WIDTH;
 	
 	if (store)
 	{
@@ -111,7 +126,7 @@ ISR(IR_CAPT_VECTOR)
 		//TODO: Report overflow to application
 		if (timesCounter == MAX_NR_TIMES)
 		{
-			IR_MASK_CAPT();
+			IR_MASK_CAPTURE();
 		}
 	}
 	else
@@ -121,7 +136,7 @@ ISR(IR_CAPT_VECTOR)
 		store = 1;
 		/* Enable timeout interrupt for detection of the end of the pulse
 		 * train. */
-		IR_UNMASK_OVF();
+		IR_UNMASK_TIMEOUT();
 	}
 }
 
@@ -135,6 +150,7 @@ void IrTransceiver_Init(void)
 	IR_TIMER_INIT();
 	IR_TIMEOUT_REG = IR_MAX_PULSE_WIDTH;
 	IRDDR &= ~(1<<IRBIT);
+	DDRC |= (1<<PC5);
 }
 
 void IrTransceiver_Start(void)
@@ -144,13 +160,13 @@ void IrTransceiver_Start(void)
 	data_received = 0;
 	store = 0;
 #if (IR_RX_ACTIVE_LOW == 1)
-	IR_CAPT_FALLING();
+	IR_CAPTURE_FALLING();
 	detect_edge = 0;
 #else
-	IR_CAPT_RISING();
+	IR_CAPTURE_RISING();
 	detect_edge = 1;
 #endif
-	IR_UNMASK_CAPT();
+	IR_UNMASK_CAPTURE();
 }
 
 uint8_t IrTransceiver_Poll(uint16_t **buffer, uint8_t *length)
@@ -165,6 +181,24 @@ uint8_t IrTransceiver_Poll(uint16_t **buffer, uint8_t *length)
 	{
 		return IR_NO_DATA;
 	}
+}
+
+uint8_t IrTransceiver_Transmit(uint16_t **buffer, uint8_t *length)
+{
+	data_transmitted = 0;
+	if (timesCounter == 0) return IR_NO_DATA;
+	timesIndex = 1;
+	IR_COMPARE_REG = IR_COUNT_REG + times[0];
+#if (IR_RX_ACTIVE_LOW == 1)
+	PORTC &= ~(1<<PC5);
+#else
+	PORTC |= (1<<PC5);
+#endif
+	IR_UNMASK_COMPARE();
+	
+	while (!data_transmitted);
+	
+	return IR_OK;
 }
 
 /**
