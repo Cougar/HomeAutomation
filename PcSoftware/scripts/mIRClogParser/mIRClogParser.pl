0987f132fa5f298cd7ec82d6cb44bc41d4038c63
Anders Runeson <runeson@gmail.com>
2007-08-08 17:18:25 +0000

    mirclogparser: fixed bug introduced with last feature
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@593 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/scripts/mIRClogParser/mIRClogParser.pl
+++ PcSoftware/scripts/mIRClogParser/mIRClogParser.pl
@@ -204,7 +204,7 @@ while (<LOGFILE>) {
 			$parsedLine =~ s/\[wiki\]/**[wiki]**/g;
 			$parsedLine =~ s/\[svn\]/**[svn]**/g;
 
-			$parsedLine =~ s/http:\/\/projekt\.auml\.se\/(.+) /[[$1]] /g;
+			$parsedLine =~ s/http:\/\/projekt\.auml\.se\/(.+)\s/[[$1]] /g;
 			
 			$parsedLine .= "\\\\"."\n";
 			if ($debugmode == 0) {
