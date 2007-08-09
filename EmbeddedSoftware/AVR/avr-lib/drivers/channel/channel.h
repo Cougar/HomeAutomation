ec17df88c66c5d5379bf7f63387892443293bcfa
pengi <pengi@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2007-08-09 10:59:27 +0000

    started commenting channel library
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@595 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/channel/channel.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/channel/channel.h
@@ -1,8 +1,23 @@
+/**********************************************************
+ * Channel based subsystem for HomeAutomation
+ *
+ * Author: Max Sikstrom
+ *
+ **********************************************************/
+
 #ifndef CHANNEL_H_
 #define CHANNEL_H_
 
+/**********************************************************
+ * Includes
+ **********************************************************/
 #include <inttypes.h>
 #include <bios.h>
+
+/**********************************************************
+ * Constants
+ **********************************************************/
+#define CAN_CHANNEL     0x0aUL
 
 #define CHANNEL_FAIL    0
 #define CHANNEL_OK      1
@@ -20,8 +35,9 @@
 #define CHANNEL_SUBCLASS_CHN_NAME   0x4UL
 #define CHANNEL_SUBCLASS_CHN_TYPE   0x5UL
 
-#define CAN_CHANNEL     0x0aUL
-
+/**********************************************************
+ * Internal storage structure
+ **********************************************************/
 typedef struct {
     uint16_t    channel;
     uint16_t    value;
@@ -41,14 +57,46 @@ typedef struct {
 
 } Channel_Info_t;
 
+/**********************************************************
+ * Functions
+ **********************************************************/
+
+/* Initialize channel subsystem
+ *
+ * Must be done after can initializes.
+ */
 uint8_t Channel_Init( void );
 
+/* Handle a channel CAN-message.
+ *
+ * Returns CHANNEL_OK if ok, CHANNEL_FAIL otherwise
+ */
 uint8_t Channel_HandleMsg( Can_Message_t *msg );
+
+/* Bind a channel to function id
+ *
+ * This can, and should, be done from a macronode through the bus
+ */
 void Channel_Bind( uint8_t id, uint8_t channel );
 
+/* Get a value unscaled from a channel referenced by a function id.
+ */
 uint16_t Channel_GetValue( uint8_t id );
+
+/* Set a value unscaled to a channeld referenced by a function id.
+ */
 uint8_t Channel_SetValue( uint8_t id, uint16_t value );
 
+/* Get internal storage for function id
+ */
+Channel_Info_t *Channel_GetStorage( uint8_t id );
+
+/**********************************************************
+ * Callbacks
+ **********************************************************/
+
+/* A callback routine for handling a valuechange in a channel
+ */
 void Channel_Callback_Value( uint8_t id, uint16_t value );
 
 #endif
