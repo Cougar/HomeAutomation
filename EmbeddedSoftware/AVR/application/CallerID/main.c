1ce95d5aec0183ff72a44ba252da74e43623bdd7
Anders Runeson <runeson@gmail.com>
2007-08-26 06:30:41 +0000

    app/CallerID: Updated to work with mt8870-drivers
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@624 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/application/CallerID/main.c
+++ EmbeddedSoftware/AVR/application/CallerID/main.c
@@ -5,41 +5,12 @@
 #include <config.h> // All configuration parameters
 #include <bios.h>   // BIOS interface declarations, including CAN structure and ID defines.
 #include <drivers/uart/serial.h>
-#include <drivers/timer/timer.h>
-
-/*
- * De få signaler som man behöver koppla in från MT8870 är
- * StD, Q1, Q2, Q3 och Q4
- */
-
-#define MT_StD_PIN      PIND
-#define MT_StD_DDR      DDRD
-#define MT_StD_BIT      PD2
-
-#define MT_Q1_PIN       PINC
-#define MT_Q1_DDR       DDRC
-#define MT_Q1_BIT       PC0
-
-#define MT_Q2_PIN       PINC
-#define MT_Q2_DDR       DDRC
-#define MT_Q2_BIT       PC1
-
-#define MT_Q3_PIN       PINC
-#define MT_Q3_DDR       DDRC
-#define MT_Q3_BIT       PC2
-
-#define MT_Q4_PIN       PINC
-#define MT_Q4_DDR       DDRC
-#define MT_Q4_BIT       PC3
-
-#define DIAL_TIMEOUT	1800
-
-
+#include <drivers/callerid/mt8870/mt8870.h>
 
 #define STATE_IDLE			0
 #define STATE_GET_DATA		1
-#define STATE_WAIT1			2
-#define STATE_WAIT2			3
+#define STATE_START_WAIT	2
+#define STATE_WAIT			3
 #define STATE_STOP			4
 
 #define APP_TYPE    0xf001
@@ -59,25 +30,11 @@ void can_receive(Can_Message_t *msg) {
 	}
 }
 
-// Timer callback function used for some timer tests
-void timer_callback(uint8_t timer) {
-	Can_Message_t msg;
-	
-	msg.ExtendedFlag = 1;
-	msg.Id = (CAN_TST << CAN_SHIFT_CLASS) | NODE_ID;
-	msg.RemoteFlag = 0;
-	msg.DataLength = 1;
-	msg.Data.bytes[0] = timer;
-	
-	BIOS_CanSend(&msg);
-}
-
 int main(void)
 {
 	// Enable interrupts as early as possible
 	sei();
 	
-	Timer_Init();
 	Serial_Init();
 	
 	Can_Message_t txMsg;
@@ -93,89 +50,41 @@ int main(void)
 	// Send CAN_NMT_APP_START
 	BIOS_CanSend(&txMsg);
 	
-	//set ports to input
-	MT_StD_DDR &= ~(1<<MT_StD_BIT);
-	MT_Q1_DDR &= ~(1<<MT_Q1_BIT);
-	MT_Q2_DDR &= ~(1<<MT_Q2_BIT);
-	MT_Q3_DDR &= ~(1<<MT_Q3_BIT);
-	MT_Q4_DDR &= ~(1<<MT_Q4_BIT);
+	Callerid_Init();
 
-	
-	printf("AVR Test Application\n");
-	
-	txMsg.Id = (CAN_TST << CAN_SHIFT_CLASS) | NODE_ID;
-	txMsg.Data.dwords[0] = 0x01020304;
-	txMsg.DataLength = 8;
-
-	uint8_t receviercounter = 0;
-    uint8_t datain = 0;
+	uint8_t rxbuffer[MAX_TONES];
     uint8_t state=0;
+    uint8_t rxlen=0;
 
 	while (1) {
 		
 		if (state == STATE_IDLE) {
-			if ((MT_StD_PIN & (1<<MT_StD_BIT)) != 0) {
-				state = STATE_GET_DATA;
-			}
-		} else if (state == STATE_GET_DATA) {
-			//nu finns data tillgänglig på Qx
-			datain = ((MT_Q1_PIN >> MT_Q1_BIT) & 0x1);
-			datain |= (((MT_Q2_PIN >> MT_Q2_BIT) & 0x1)<<1);
-			datain |= (((MT_Q3_PIN >> MT_Q3_BIT) & 0x1)<<2);
-			datain |= (((MT_Q4_PIN >> MT_Q4_BIT) & 0x1)<<3);
-			if (datain == 0) { datain = 0x1d; }		//DTMF0  = D    //this is temporary
-			if (datain == 11) { datain = 0x1e; }	//DTMF12 = *    //this is temporary
-			if (datain == 12) { datain = 0x1f; }	//DTMF11 = #    //this is temporary
-			if (datain == 15) { datain = 0xc; }		//DTMF11 = C
-			if (datain == 10) { datain = 0; }		//DTMF10 = number 0
-			if (datain == 13) { datain = 0xa; }		//DTMF13 = A
-			if (datain == 14) { datain = 0xb; }		//DTMF14 = B
-			if (datain == 0x1d) { datain = 0xd; }	//DTMF0  = D    //now permanent
-			if (datain == 0x1e) { datain = 0xe; }	//DTMF12 = *    //now permanent
-			if (datain == 0x1f) { datain = 0xf; }	//DTMF11 = #    //now permanent
-		
-			if ((receviercounter & 0x1) == 0) {
-				txMsg.Data.bytes[(receviercounter>>1)] = (datain<<4);
-			} else {
-				txMsg.Data.bytes[(receviercounter>>1)] |= datain;
-			}
-			receviercounter++;
-
-			state = STATE_WAIT1;
-		} else if (state == STATE_WAIT1) {
-			if ((MT_StD_PIN & (1<<MT_StD_BIT)) == 0) {
-				Timer_SetTimeout(0, DIAL_TIMEOUT, TimerTypeOneShot, 0);
-				state = STATE_WAIT2;
-			}	
-			
-		} else if (state == STATE_WAIT2) {
-			if ((MT_StD_PIN & (1<<MT_StD_BIT)) != 0) {
-				if (receviercounter != 16) {
-					state = STATE_GET_DATA;
-				} else {
-					state = STATE_WAIT1;
-				}
-			}
-			
-			if (Timer_Expired(0)) {
+			Callerid_Start(rxbuffer);
+			state = STATE_START_WAIT;
+		} else if (state == STATE_START_WAIT) {
+			state = STATE_WAIT;
+		} else if (state == STATE_WAIT) {
+			uint8_t retval = Callerid_Poll(&rxlen);
+			if (retval == RET_FINISHED) {
 				state = STATE_STOP;
+			} else if (retval == RET_OVERFLOW) {
+				state = STATE_IDLE;
 			}
 		} else if (state == STATE_STOP) {
-			if (datain == 0xc) {    //om sista tonen var c
-				if ((receviercounter & 0x1) == 0) {
-					txMsg.DataLength = (receviercounter>>1);
-				} else {
-					txMsg.DataLength = (receviercounter>>1)+1;
-					//txMsg.Data.bytes[(receviercounter>>1)] |= 0xc; //spara ett extra c om udda antal
+			//if ((rxbuffer[0] == 0xa || rxbuffer[0] == 0xb) && rxbuffer[rxlen-1] == 0xc) {
+				for (uint8_t i = 0; i < 16; i=i+2) {
+					txMsg.Data.bytes[i>>1] = rxbuffer[i]<<4;
+					if (i+1 < rxlen) {
+						txMsg.Data.bytes[i>>1] |= rxbuffer[i+1];
+					}
 				}
+				txMsg.DataLength = (rxlen>>1)+(rxlen&1);
 				txMsg.Id = ((CAN_SNS << CAN_SHIFT_CLASS) | (SNS_TYPE_PHONENR << CAN_SHIFT_SNS_TYPE) | (0<<CAN_SHIFT_SNS_ID) | (NODE_ID << CAN_SHIFT_SNS_SID));
 				BIOS_CanSend(&txMsg);
-			}				
-			
-			receviercounter = 0;
+	
+			//}
 			state = STATE_IDLE;
 		}
-		
 	}
 	
 	return 0;
