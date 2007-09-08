19b89531401c31f3b35377f6fb5adf337aa58574
Anders Runeson <runeson@gmail.com>
2007-09-08 09:13:20 +0000

    avr-lib/mcp2515: Added functionality for changing clockout prescaler from node config
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@629 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/can/mcp2515/mcp2515_cfg.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/can/mcp2515/mcp2515_cfg.h
@@ -14,14 +14,26 @@
  * MCP2515 Chip Select port on avr. 
  */
 
-/* Already defined in node configuration bios.inc */
+/* Should already be defined in node configuration bios.inc */
 #ifndef MCP_CS_PORT
 #define MCP_CS_PORT	PORTB
 #endif
 #ifndef MCP_CS_DDR
 #define MCP_CS_DDR	DDRB
+#endif
 #ifndef MCP_CS_BIT
 #define MCP_CS_BIT	PB2
+#endif
+
+/* Choose prescaler for clock output */
+#if MCP_CLOCK_PRESC == 8
+#define MCP_PRESCALER CLKOUT_PS8
+#elif MCP_CLOCK_PRESC == 4
+#define MCP_PRESCALER CLKOUT_PS4
+#elif MCP_CLOCK_PRESC == 2
+#define MCP_PRESCALER CLKOUT_PS2
+#else
+#define MCP_PRESCALER CLKOUT_PS1
 #endif
 
 #if defined(__AVR_ATmega8__)
