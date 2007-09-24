ab9ebba14357a55b66c004efa573985365faf8e1
Anders Runeson <runeson@gmail.com>
2007-09-24 19:21:28 +0000

    drivers/ldr: Changed adchannel selection implementation, also removed decrease of precision, I will add this to application/sensors instead.
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@639 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/sensor/ldr/ldr.c
+++ EmbeddedSoftware/AVR/avr-lib/drivers/sensor/ldr/ldr.c
@@ -16,42 +16,42 @@
 #include <stdlib.h>
 
 #include "ldr.h"
-
+#include <config.h>
 /*----------------------------------------------
  * Functions
  * --------------------------------------------*/
- 
+
 uint8_t LDR_SensorInit(void)
 {
-#if defined(ADC0)
+#if ADCHAN == 0
     /* Enable ADC0 */
     ADMUX &= ~((1<<MUX0)|(1<<MUX1)|(1<<MUX2)|(1<<MUX3));
-#elif defined(ADC1)
+#elif ADCHAN == 1
     /* Enable ADC1 */
     ADMUX |= (1<<MUX0);
     ADMUX &= ~((1<<MUX1)|(1<<MUX2)|(1<<MUX3));
-#elif defined(ADC2)
+#elif ADCHAN == 2
     /* Enable ADC2 */
     ADMUX |= (1<<MUX1)|;
     ADMUX &= ~((1<<MUX0)|(1<<MUX2)|(1<<MUX3));
-#elif defined(ADC3)
+#elif ADCHAN == 3
     /* Enable ADC3 */
     ADMUX |= (1<<MUX0)|(1<<MUX1);
     ADMUX &= ~((1<<MUX2)|(1<<MUX3));
-#elif defined(ADC4)
+#elif ADCHAN == 4
     /* Enable ADC4 */
     ADMUX |= (1<<MUX2);
     ADMUX &= ~((1<<MUX0)|(1<<MUX1)|(1<<MUX3));
-#elif defined(ADC5)
+#elif ADCHAN == 5
     /* Enable ADC5 */
     ADMUX |= (1<<MUX0)|(1<<MUX2);
     ADMUX &= ~((1<<MUX1)|(1<<MUX3));
 //#error yo helt fel
-#elif defined(ADC6)
+#elif ADCHAN == 6
     /* Enable ADC6 */
     ADMUX |= (1<<MUX1)|(1<<MUX2);
     ADMUX &= ~((1<<MUX0)|(1<<MUX3));
-#elif defined(ADC7)
+#elif ADCHAN == 7
     /* Enable ADC7 (only for TQFP package) */
     ADMUX |= (1<<MUX0)|(1<<MUX1)|(1<<MUX2);
     ADMUX &= ~(1<<MUX3);
@@ -72,6 +72,8 @@ uint8_t LDR_SensorInit(void)
 
     /* Make the first conversion (takes 25 ADC clock cycles) and throw away */
     LDR_GetData(0);
+    
+    return 0;
 }
 
 /*
@@ -87,9 +89,8 @@ uint32_t LDR_GetData(uint8_t sensor)
 	while( ADCSRA & (1<<ADSC) ){ /* Wait for conversion to complete */ }
 
 	/* Get the result and return */
-	lightData = ADCW>>2;
+	lightData = ADCW; //>>2;
 
     return lightData;
 	
 }
- 
