6617d22bb8f3f6c3eb94b39b55a2e231c12cf211
Anders Runeson <runeson@gmail.com>
2007-10-11 18:49:51 +0000

    mcu.c updated with prescaler settings, not used in application template or by bios
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@657 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/avr-lib/drivers/mcu/mcu.c
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/mcu/mcu.c
@@ -24,13 +24,35 @@
  * set to 8MHz. Interrupts must not be enabled when calling this function!
  */
 void Mcu_Init() {
-	#if defined(__AVR_ATmega88__) || defined(__AVR_ATmega168__)
+#if defined(__AVR_ATmega88__) || defined(__AVR_ATmega168__)
 	// set the clock speed to 8MHz
 	// set the clock prescaler. First write CLKPCE to enable setting of clock the
 	// next four instructions.
 	CLKPR=(1<<CLKPCE);
-	CLKPR=0; // 8 MHZ
+	#ifndef AVR_CLOCK_PRESC
+		CLKPR=0; // 8 MHZ
 	#endif
+	
+	#if AVR_CLOCK_PRESC == 1
+		CLKPR=0; // 8 MHZ
+	#elif AVR_CLOCK_PRESC == 2
+		CLKPR=1; // 4 MHZ
+	#elif AVR_CLOCK_PRESC == 4
+		CLKPR=2; // 2 MHZ
+	#elif AVR_CLOCK_PRESC == 8
+		CLKPR=3; // 1 MHZ
+	#elif AVR_CLOCK_PRESC == 16
+		CLKPR=4; // 500 kHZ
+	#elif AVR_CLOCK_PRESC == 32
+		CLKPR=5; // 250 kHZ
+	#elif AVR_CLOCK_PRESC == 64
+		CLKPR=6; // 125 kHZ
+	#elif AVR_CLOCK_PRESC == 128
+		CLKPR=7; // 62.5 kHZ
+	#elif AVR_CLOCK_PRESC == 256
+		CLKPR=8; // 31.25 kHZ
+	#endif
+#endif
 }
 
 /**
