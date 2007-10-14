4dda4af3f404d09d25425d9ddbc1495947ec1432
Anders Runeson <runeson@gmail.com>
2007-10-14 11:35:09 +0000

    drivers/enc28j60: using spi with USART in spi-mode is now implemented, this is to be used on ethernet expansion to corecard
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@659 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/eth/enc28j60/enc28j60_cfg.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/eth/enc28j60/enc28j60_cfg.h
@@ -10,11 +10,30 @@
  *---------------------------------------------------------------------------*/
  
  /**
+ * ENC28J60 SPI select. For USART in SPI-mode, use the following defined 
+ */ 
+
+//#define ENC28J60_USART_SPI_MODE
+
+ /**
  * ENC28J60 Chip Select port on avr. 
  */ 
  
-#define ENC28J60_CONTROL_PORT   PORTC
-#define ENC28J60_CONTROL_DDR    DDRC
-#define ENC28J60_CONTROL_CS     0
+#define ENC28J60_CS_PORT	PORTC
+#define ENC28J60_CS_DDR		DDRC
+#define ENC28J60_CS			0
+
+ /**
+ * ENC28J60 Reset port on avr. 
+ */ 
+
+#define ENC28J60_RESET_PORT	PORTC
+#define ENC28J60_RESET_DDR	DDRC
+#define ENC28J60_RESET_CS	1
+
+// only for USART in SPImode
+#define XCK_DDR				DDRD
+#define XCK					PD4
+
 
 #endif /*ENC28J60_CFG_H_*/
