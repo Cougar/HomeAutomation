94680cac6b020905195a9630468e7f03a0d7c5d0
Anders Runeson <runeson@gmail.com>
2007-10-14 14:59:10 +0000

    standalone/simpleethernet: now using avr-libdir, not dependent on old libdir anymore
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@661 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ EmbeddedSoftware/AVR/standalone/CanSimpleEthernet/config.h
@@ -0,0 +1,41 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// Kolla makefile för att sätta MCU!!
+
+#define F_CPU 					8000000UL
+
+//#define MCU atmega88
+#define CAN_CONTROLLER 			MCP2515
+#define MCP_CS_PORT 			PORTB
+#define MCP_CS_DDR 				DDRB
+#define MCP_CS_BIT 				PB2
+#define MCP_CAN_BITRATE_KBPS 	250
+#define MCP_CLOCK_FREQ_MHZ 		20
+#define MCP_CLOCK_PRESC 		1
+
+
+#define ENC28J60_USART_SPI_MODE
+
+ /**
+ * ENC28J60 Chip Select port on avr. 
+ */ 
+ 
+#define ENC28J60_CS_PORT	PORTC
+#define ENC28J60_CS_DDR		DDRC
+#define ENC28J60_CS			0
+
+ /**
+ * ENC28J60 Reset port on avr. 
+ */ 
+
+#define ENC28J60_RESET_PORT	PORTC
+#define ENC28J60_RESET_DDR	DDRC
+#define ENC28J60_RESET	1
+
+// only for USART in SPImode
+#define XCK_DDR				DDRD
+#define XCK					PD4
+
+
+#endif /*CONFIG_H_*/
