760c03944028499991f621e794fd09855f6969f8
Anders Runeson <runeson@gmail.com>
2007-10-14 17:59:21 +0000

    standalone/Can2Serial: now using avr-libdir, not dependent on old libdir anymore
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@665 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/EmbeddedSoftware/AVR/standalone/Can2Serial/config.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// Kolla makefile för att sätta MCU!!
+
+#define F_CPU 					20000000UL
+
+//#define MCU atmega88
+#define CAN_CONTROLLER 			MCP2515
+#define MCP_CS_PORT 			PORTB
+#define MCP_CS_DDR 				DDRB
+#define MCP_CS_BIT 				PB2
+#define MCP_CAN_BITRATE_KBPS 	250
+#define MCP_CLOCK_FREQ_MHZ 		20
+#define MCP_CLOCK_PRESC 		1
+
+#define SERIAL_BAUDRATE 		57600
+#define SERIAL_RX_BUFFER_SIZE	32
+#define SERIAL_TX_BUFFER_SIZE	64
+
+/* set to 0 for not sending timestamps on bus */
+#define SENDTIMESTAMP	0
+
+#endif /*CONFIG_H_*/
