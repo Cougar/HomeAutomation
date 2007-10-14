a873069cd1530e68ce211772b7c8435551910317
Anders Runeson <runeson@gmail.com>
2007-10-14 19:05:58 +0000

    standalone/CanTest: now using avr-libdir, not dependent on old libdir anymore
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@667 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/EmbeddedSoftware/AVR/standalone/CanTest/config.h
@@ -0,0 +1,29 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// Kolla makefile för att sätta MCU!!
+
+#define F_CPU 					20000000UL
+
+#define UART_OUTPUT /* If you got the node connected to a computer enable this */
+//#define LED_OUTPUT /* If you havent got any connection to a computer but still got some IOs free */
+
+#define SENDING_ID	0xffffffffUL // FIXME vilka idn kan vara lämpliga?
+#define ECHO_RECEIVE_ID		0x1700000UL
+#define ECHO_SENDING_ID		0x17000FFUL
+
+
+//#define MCU atmega88
+#define CAN_CONTROLLER 			MCP2515
+#define MCP_CS_PORT 			PORTB
+#define MCP_CS_DDR 				DDRB
+#define MCP_CS_BIT 				PB2
+#define MCP_CAN_BITRATE_KBPS 	250
+#define MCP_CLOCK_FREQ_MHZ 		20
+#define MCP_CLOCK_PRESC 		1
+
+#define SERIAL_BAUDRATE 		57600
+#define SERIAL_RX_BUFFER_SIZE	32
+#define SERIAL_TX_BUFFER_SIZE	64
+
+#endif /*CONFIG_H_*/
