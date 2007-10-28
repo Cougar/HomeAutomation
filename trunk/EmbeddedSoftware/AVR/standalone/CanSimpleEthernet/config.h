809392dd230b89532b1a190245cb408e24c65665
Anders Runeson <runeson@gmail.com>
2007-10-28 12:44:14 +0000

    simpleethernet: moved mac and ip defines to config.h
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@680 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/standalone/CanSimpleEthernet/config.h
+++ trunk/EmbeddedSoftware/AVR/standalone/CanSimpleEthernet/config.h
@@ -15,23 +15,39 @@
 #define MCP_CLOCK_PRESC 		1
 
 
-//#define ENC28J60_USART_SPI_MODE
+#define ENC28J60_USART_SPI_MODE
+
+ /* 
+ * mac and ip have to be unique in your local area network. 
+ * You can not have the same numbers in two devices: 
+ */
+
+#define ENC28J60_MAC1			0x54
+#define ENC28J60_MAC2			0x55
+#define ENC28J60_MAC3			0x58
+#define ENC28J60_MAC4			0x10
+#define ENC28J60_MAC5			0x00
+#define ENC28J60_MAC6			0x27
+
+#define ENC28J60_IP1			192
+#define ENC28J60_IP2			168
+#define ENC28J60_IP3			0
+#define ENC28J60_IP4			50
 
  /**
  * ENC28J60 Chip Select port on avr. 
  */ 
- 
+
 #define ENC28J60_CS_PORT	PORTC
 #define ENC28J60_CS_DDR		DDRC
-#define ENC28J60_CS			0
+#define ENC28J60_CS			PC0
 
  /**
  * ENC28J60 Reset port on avr. 
  */ 
-
 #define ENC28J60_RESET_PORT	PORTC
 #define ENC28J60_RESET_DDR	DDRC
-#define ENC28J60_RESET	1
+#define ENC28J60_RESET		PC1
 
 // only for USART in SPImode
 #define XCK_DDR				DDRD
