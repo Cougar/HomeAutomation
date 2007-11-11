309039ff5051c597f74c4e0ac2882b3f7b7e4b2a
Anders Runeson <runeson@gmail.com>
2007-11-11 17:02:36 +0000

    drivers: can.h and mcp2515.h updated with doxygen-doc
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@691 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/can/can.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/can/can.h
@@ -12,54 +12,37 @@
  *---------------------------------------------------------------------------*/
 
 /**
- * CAN Return Type. Most CAN functions return one of these. 
+ * @brief Return values.
+ * 
+ * CAN Return Type. Most CAN functions return one of these.
  */
 typedef enum {
-	CAN_FAIL = -1,					/* The function failed */
-	CAN_OK = 1,						/* The function succeeded */
+	CAN_FAIL = -1,				/**< The function failed. */
+	CAN_OK = 1,					/**< The function succeeded. */
 	
-	CAN_MSG_AVAILABLE = 2,			/* Messsages are available */
-	CAN_NO_MSG_AVAILABLE = -2,		/* Messages are NOT available */
+	CAN_MSG_AVAILABLE = 2,		/**< Messsages are available. */
+	CAN_NO_MSG_AVAILABLE = -2,	/**< Messages are NOT available. */
 	
-	//CAN_CTRLERROR = -4
 } Can_Return_t;
 
 /**
+ * @brief Message structure.
+ * 
  * CAN Message Type. Stores a complete CAN frame.
  */
 typedef struct {
-    /*
-     * ID section. Contains either 29bit ID or 11bit ID (depending on ExtendedFlag).
-     */
-    uint32_t Id;
-    
-    /*
-     * DLC section. Contains the data length expressed in number of bytes. Range [0,8].
-     */
-    uint8_t DataLength;
-    
-    /**
-     * Extended frame flag. Set to 1 if the ID is 29bit, or 0 if it is 11bit.
-     */
-    uint8_t ExtendedFlag;
-    
-    /**
-     * Remote frame flag. Set to 1 if the frame is a remote frame. 0 otherwise.
-     */
-    uint8_t RemoteFlag;
+    uint32_t Id;				/**< ID section. Contains either 29bit ID or 11bit ID (depending on ExtendedFlag). */
+    uint8_t DataLength;			/**< DLC section. Contains the data length expressed in number of bytes. Range [0,8]. */
+    uint8_t ExtendedFlag;		/**< Extended frame flag. Set to 1 if the ID is 29bit, or 0 if it is 11bit. */
+    uint8_t RemoteFlag;			/**< Remote frame flag. Set to 1 if the frame is a remote frame. 0 otherwise. */
     
     /*
      * DATA section. Contains the 8 data bytes.
      */
     union {
-        /* read as 8bit unsigned data */
-        uint8_t bytes[8];
-        
-        /* read as 16bit unsigned data */
-        uint16_t words[4];
-        
-        /* read as 32bit unsigned data */
-        uint32_t dwords[2];
+        uint8_t bytes[8];		/**< read as 8bit unsigned data */
+        uint16_t words[4];		/**< read as 16bit unsigned data */
+        uint32_t dwords[2];		/**< read as 32bit unsigned data */
     } Data;
 } Can_Message_t;
 
--- EmbeddedSoftware/AVR/avr-lib/drivers/can/mcp2515/mcp2515.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/can/mcp2515/mcp2515.h
@@ -1,3 +1,21 @@
+/** 
+ * @defgroup mcp2515 mcp2515 Library
+ * @code #include <drivers/can/mcp2515/mcp2515.h> @endcode
+ * 
+ * @brief Functions for can controller communication. 
+ * Exposes functions for initialization of controller and sending a message.
+ * For receiving messages, an interrupt callback must be set up at the user of 
+ * this driver.
+ * 
+ * Based on Martin Thomas' code which was based on Kvaser's 
+ * MCP2510 sample-application.
+ * 
+ * @author	Jimmy Myhrman, Andreas Fritiofson
+ * @date	2005-01-01
+ */
+
+/**@{*/
+
 #ifndef MCP2515_H_
 #define MCP2515_H_
 
@@ -9,13 +27,9 @@
 
 #define MCP_RXBUF_0 (MCP_RXB0SIDH)
 #define MCP_RXBUF_1 (MCP_RXB1SIDH)
-
 #define MCP_TXBUF_0 (MCP_TXB0SIDH)
 #define MCP_TXBUF_1 (MCP_TXB1SIDH)
 #define MCP_TXBUF_2 (MCP_TXB2SIDH)
-
-// #define MCP2515_SELECT() (SPI_SS_LOW())
-// #define MCP2515_UNSELECT() (SPI_SS_HIGH())
 
 #define MCP2515_SELECT()   ( MCP_CS_PORT &= ~(1<<MCP_CS_BIT) )
 #define MCP2515_UNSELECT() ( MCP_CS_PORT |=  (1<<MCP_CS_BIT) )
@@ -24,42 +38,39 @@
 #define MCP2515_FAIL       (1)
 #define MCP_ALLTXBUSY      (255)
 
-#if 0
-void MCP2515_Reset(void);
 
-uint8_t MCP2515_ReadRegister(const uint8_t address);
-void MCP2515_SetRegisterS(const uint8_t address, const uint8_t values[], const uint8_t n);
-
-void MCP2515_SetRegister(const uint8_t address, const uint8_t value);
-void MCP2515_SetRegisterS(const uint8_t address, const uint8_t values[], const uint8_t n);
-void MCP2515_ModifyRegister(const uint8_t address, const uint8_t mask, const uint8_t data);
-
-uint8_t MCP2515_ReadStatus(void);
-uint8_t MCP2515_RxStatus(void);
-
-uint8_t MCP2515_SetCanCtrlMode(const uint8_t newmode);
-uint8_t MCP2515_SetClkout(const uint8_t newmode);
-
-inline void MCP2515_ConfigRate(void);
-uint8_t MCP2515_Init(void);
-
-void MCP2515_WriteCanId(const uint8_t mcp_addr, const uint8_t ext, const uint32_t can_id);
-void MCP2515_ReadCanId(const uint8_t mcp_addr, uint8_t* ext, uint32_t* can_id );
-void MCP2515_WriteCanMsg(const uint8_t buffer_sidh_addr, const Can_Message_t* msg);
-void MCP2515_ReadCanMsg(const uint8_t buffer_sidh_addr, Can_Message_t* msg);
-
-void MCP2515_StartTransmit(const uint8_t mcp_addr);
-
-uint8_t MCP2515_GetNextFreeTXBuf(uint8_t *txbuf_n);
-
-#ifdef MCP_DEBUGMODE
-void MCP2515_DumpExtendedStatus(void);
-#endif
-#endif
-
+/**
+ * @brief Initialize CAN.
+ * Initializes CAN interface and the mcp2515 can controller 
+ * 
+ * @return
+ *		CAN_OK if initialization was successful.
+ * 		CAN_INIT_FAIL in case of general error.
+ */ 
 Can_Return_t Can_Init(void);
+
+/**
+ * @brief Send a CAN message.
+ * Sends a CAN message immediately with the controller hardware. If the CAN
+ * controller is busy, the function will return CAN_FAIL.
+ *
+ * @param msg
+ *		Pointer to the CAN message storage buffer.
+ * 
+ * @return
+ *		CAN_OK if the message was successfully sent to the controller.
+ *		CAN_FAIL if the controller is busy.
+ */
 Can_Return_t Can_Send(Can_Message_t* msg);
-void Can_Process(Can_Message_t* msg); // Callback from ISR to application code 
-//Can_Return_t Can_Receive(Can_Message_t *msg);
+
+/**
+ * @brief Callback from driver interrupt service routine.
+ * Users of mcp2515 drivers must implement this function to receive 
+ * CAN messages.
+ * 
+ * @param msg
+ *		Pointer to the CAN message storage buffer.
+ */
+void Can_Process(Can_Message_t* msg);  
 
 #endif
