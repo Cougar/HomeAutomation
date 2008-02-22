519f41afb13597aeadc247f1dae31253f330a9db
Olof <olosve@dtek.chalmers.se>
2008-02-22 17:11:02 +0000

    + Ramverk för konsollgränssnitt
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@740 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/pyCanDaemon/Main.py
+++ PcSoftware/pyCanDaemon/Main.py
@@ -20,13 +20,58 @@ daemonApp = None
 
 class CanDaemon():
     
+    cfg = None
     nodeIf = None
     ifNotifier = None
     terminated = False
     command_list = []
+    command_map = {}
+    help_command_map = {}
 
     def __init__ (self):
-        pass
+        self.command_list = [['help', self.helpCmd, 'display help'],
+                       ['log', self.logCmd, 'log control: \"help log\"'],
+                       ['exec', self.execCmd,  'run command listing <filename>\n'],
+                       ['filter', self.filterCmd, 'filter commands: \"help filter\"'],
+                       ['state', self.stateCmd, 'state commands: \"help state\"\n'],
+                       ['addif', self.ifAddCmd, 'add interface: \"help addif\"'],
+                       ['remif', self.ifRemCmd, 'remove interface <name>'],
+                       ['ifup', self.ifUpCmd, 'bring up interface <name>'],
+                       ['ifdown', self.ifDownCmd, 'bring down interface <name>\n'],
+                       ['addbrigde', self.addBridgeCmd, 'add bridge: \"help addbridge\"'],
+                       ['rembridge', self.remBridgeCmd, 'remove bridge <name>\n'],
+                       ['tcpd', self.tcpdCmd, 'tcpd server control: \"help tcpd\"'],
+                       ['tlsd', self.tlsdCmd, 'tlsd server control: \"help tlsd\"'],
+                       ['canctld', self.canCtldCmd, 'canctld server control: \"help canctld\"\n'],
+                       ['status', self.statusCmd, 'show daemon status'],
+                       ['stats', self.statsCmd, 'show statistics\n'],
+                       ['nodelist', self.nodesCmd, 'list can nodes'],
+                       ['filterlist', self.filterlistCmd, 'list active filters\n'],
+                       ['sim', self.simCmd, 'simulator commands: \"help sim\"']]
+        
+        index = 0
+        for cmd in self.command_list:
+            self.command_map[cmd[0]] = index
+            index += 1
+
+        logHelp = 'not implemented'
+        stateHelp = 'not implemented'
+        addifHelp = 'not implemented'
+        addbridgeHelp = 'not implemented'
+        tcpdHelp = 'not implemented'
+        tlsdHelp = 'not implemented'
+        canctldHelp = 'not implemented'
+        simHelp = 'not implemented'
+        self.help_command_map = {'log' : logHelp,
+                                'state' : stateHelp,
+                                'addif' : addifHelp,
+                                'addbridge' : addbridgeHelp,
+                                'tcpd' : tcpdHelp,
+                                'tlsd' : tlsdHelp,
+                                'canctld' : canctldHelp,
+                                'sim' : simHelp}
+
+        self.cfg = DaemonConfig()
     
     class IfNotifier():
         """ Used by if threads to send messages to the main program thread """
@@ -52,62 +97,88 @@ class CanDaemon():
         return (spcstr+str)
     
     def helpCmd(self, *args):
-        quitStr = 'quit, exit'
-#        print quitStr, self.__rightAdjust(quitStr, 'shutdown pyCanDaemon')
-#        for cmd in cmdNames:
-#            print cmd, self.__rightAdjust(cmd, command_map[cmd][1])
-        
+        args = args[0]
+        if len(args) < 1:
+            quitStr = 'quit, exit'
+            print quitStr, self.__rightAdjust(quitStr, 'shutdown pyCanDaemon')
+            for cmd in self.command_list:
+                print cmd[0], self.__rightAdjust(cmd[0], cmd[2])
+        elif len(args) < 2:
+            subCmd = args[0]
+            if self.help_command_map.has_key(subCmd):
+                print self.help_command_map[subCmd]
+            else:
+                print 'no help for ' + subCmd
+        else:
+            print 'usage: help [topic]'
+
     def filterCmd(self, *args):
-        pass
+        cmd = args[0]
+        filterName = args[1]
+        # FIXME: verify 
+        if cmd == 'add':
+            print 'Importing filter: ' + filterName
+            self.cfg.filterCfg.importFilter(filterName)
+        elif cmd == 'rem' or cmd == 'remove':
+            print 'Deleting filter: ' + filterName
+            # FIXME: actually delete
     
     def stateCmd(self, *args):
-        pass
+        cmd = args[0]
+        spaceName = args[1]
+        if cmd == 'add':
+            print 'Importing state space: ' + spaceName
+            self.cfg.stateSpaceCfg.importSpace(spaceName)
+        elif cmd == 'rem' or cmd == 'remove':
+            print 'Deleting state space: ' + spaceName
+            # FIXME: actually delete statespace
     
     def ifAddCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def ifRemCmd(self, *args):
-        pass
-    
+        print 'not implemented'
+
     def ifUpCmd(self, *args):
-        pass
-    
+        print 'not implemented'
+ 
     def ifDownCmd(self, *args):
+        print 'not implemented'
         pass
     
     def addBridgeCmd(self, *args):
-        pass
-    
+        print 'not implemented'
+
     def remBridgeCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def tcpdCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def tlsdCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def canCtldCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def statusCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def statsCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def nodesCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def filterlistCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def simCmd(self, *args):
-        pass
+        print 'not implemented'
     
     def logCmd(self, *args):
-        pass
-    
+        print 'not implemented'
+
     def execCmd(self, *args):
         pass
     
@@ -134,8 +205,6 @@ class CanDaemon():
         log.basicConfig(level=log.DEBUG)
         
         print 'INIT: Get config options from command line / config file'
-        cfg = DaemonConfig()
-        
         try:
             opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
         except getopt.GetoptError:
@@ -154,19 +223,19 @@ class CanDaemon():
             if o in ("-o", "--output"):
                 pass
     #            output = a
-    
+
         print 'INIT: Loading filters and spaces'
-  #      cfg.filterCfg.importFilter('DefaultFilter')
-  #      cfg.stateSpaceCfg.importSpace('DefaultStateSpace')
-        cfg.stateSpaceCfg.loadSpaces()
-        cfg.filterCfg.loadFilters()
-        cfg.setupFilterBindings()
-        cfg.setupFilterChain()
-        
+#       self.cfg.filterCfg.importFilter('DefaultFilter')
+#       self.cfg.stateSpaceCfg.importSpace('DefaultStateSpace')
+        self.cfg.stateSpaceCfg.loadSpaces()
+        self.cfg.filterCfg.loadFilters()
+        self.cfg.setupFilterBindings()
+        self.cfg.setupFilterChain()
+
         print 'INIT: Starting selected can interface'
         ''' nodeif thread launch '''
         self.ifNotifier = self.IfNotifier()
-        pktHandler = CanPktHandler1(cfg)
+        pktHandler = CanPktHandler1(self.cfg)
         
         ifConfig = NodeIfCanStim.DEFAULT_CONFIG
         self.nodeIf = NodeIfCanStim(ifConfig, pktHandler, self.ifNotifier)
@@ -188,37 +257,17 @@ class CanDaemon():
         
         print 'CanDaemon v1 ready'
         print 'Enter command or type \"help\" for a list of commands'
-        
-        global command_map
-        command_map = {'help' : (self.helpCmd, 'display help'),
-                       'filter' : (self.filterCmd, 'filter commands: \"help filter\"'),
-                       'state' : (self.stateCmd, 'state commands: \"help state\"'),
-                       'addif' : (self.ifAddCmd, 'add interface: \"help addif\"'),
-                       'remif' : (self.ifRemCmd, 'remove interface <name>'),
-                       'ifup' : (self.ifUpCmd, 'bring up interface <name>'),
-                       'ifdown' : (self.ifDownCmd, 'bring down interface <name>'),
-#                       'addbrigde' : (self.addBridgeCmd, 'add bridge: \"help addbridge\"'),
-#                       'rembridge' : (self.remBridgeCmd, 'remove bridge <name>'),
-                       'tcpd' : (self.tcpdCmd, 'tcpd server control: \"help tcpd\"'),
-#                       'tlsd' : (self.tlsdCmd, 'tlsd server control: \"help tlsd\"'),
-#                       'canctld' : (self.canCtldCmd, 'canctld server control: \"help canctld\"'),
-#                       'status' : (self.statusCmd, 'show daemon status'),
-#                       'stats' : (self.statsCmd, 'show statistics'),
-#                       'nodelist' : (self.nodesCmd, 'list can nodes'),
-#                       'filterlist' : (self.filterlistCmd, 'list active filters'),
-#                       'sim' : (self.simCmd, 'simulator commands: \"help sim\"'),
-#                       'log' : (self.logCmd, 'log control: \"help log\"'),
-#                       'exec' : (self.execCmd,  'run command listing <filename>')}
-
         while not self.terminated:
             input = raw_input('> ').strip().lower()
             cmd = input.split(' ')
+            if len(cmd[0]) < 2:
+                continue
             if cmd[0] == 'quit' or cmd[0] == 'exit':
                 self.terminated = True
-            elif command_map.has_key(cmd[0]):
-                command_map[cmd[0]][0](cmd[1:])
+            elif self.command_map.has_key(cmd[0]):
+                self.command_list[self.command_map[cmd[0]]][1](cmd[1:])
             else:
-                print 'Unknown command: ' + cmd[0]
+                print 'unknown command: ' + cmd[0]
 
         """ -- suggested commands -- p n = priority to implement (1 is highest)
             base: (p 1)
@@ -277,8 +326,8 @@ class CanDaemon():
             print 'Stopping If thread'
             self.nodeIf.stop()
 
-        cfg.stateSpaceCfg.saveSpaces()
-        cfg.filterCfg.saveFilters()
+        self.cfg.stateSpaceCfg.saveSpaces()
+        self.cfg.filterCfg.saveFilters()
         
         print 'Waiting 0.1..'
         time.sleep(0.1) # wait for threads
