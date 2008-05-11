b3406388b77f2727682a9c33bb42e9dfa17a5fb6
Noddan <martin.norden@gmail.com>
2008-05-11 08:35:18 +0000

    /avrlib/adc: Now supports channels!
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@792 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/avr-lib/drivers/adc/adc.c
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/adc/adc.c
@@ -11,44 +11,11 @@
 /*----------------------------------------------
  * Functions
  * --------------------------------------------*/
+ 
+ static uint8_t currrentChannel; //Used to keep the channel of the last measurement. This is used to determine if the channel needs to be changed.
 
 uint8_t ADC_Init(void)
 {
-#if ADCHAN == 0
-    /* Enable ADC0 */
-    ADMUX &= ~((1<<MUX0)|(1<<MUX1)|(1<<MUX2)|(1<<MUX3));
-#elif ADCHAN == 1
-    /* Enable ADC1 */
-    ADMUX |= (1<<MUX0);
-    ADMUX &= ~((1<<MUX1)|(1<<MUX2)|(1<<MUX3));
-#elif ADCHAN == 2
-    /* Enable ADC2 */
-    ADMUX |= (1<<MUX1)|;
-    ADMUX &= ~((1<<MUX0)|(1<<MUX2)|(1<<MUX3));
-#elif ADCHAN == 3
-    /* Enable ADC3 */
-    ADMUX |= (1<<MUX0)|(1<<MUX1);
-    ADMUX &= ~((1<<MUX2)|(1<<MUX3));
-#elif ADCHAN == 4
-    /* Enable ADC4 */
-    ADMUX |= (1<<MUX2);
-    ADMUX &= ~((1<<MUX0)|(1<<MUX1)|(1<<MUX3));
-#elif ADCHAN == 5
-    /* Enable ADC5 */
-    ADMUX |= (1<<MUX0)|(1<<MUX2);
-    ADMUX &= ~((1<<MUX1)|(1<<MUX3));
-#elif ADCHAN == 6
-    /* Enable ADC6 */
-    ADMUX |= (1<<MUX1)|(1<<MUX2);
-    ADMUX &= ~((1<<MUX0)|(1<<MUX3));
-#elif ADCHAN == 7
-    /* Enable ADC7 (only for TQFP package) */
-    ADMUX |= (1<<MUX0)|(1<<MUX1)|(1<<MUX2);
-    ADMUX &= ~(1<<MUX3);
-#else
-    #error No adc defined
-#endif
-
     /* Enable AVcc as Voltage Reference */
     ADMUX |= (1<<REFS0);
     ADMUX &= ~(1<<REFS1);
@@ -60,8 +27,8 @@ uint8_t ADC_Init(void)
     PRR &= ~(1<<PRADC);
     ADCSRA |= (1<<ADEN);
 
-    /* Make the first conversion (takes 25 ADC clock cycles) and throw away */
-    ADC_Get(0);
+    /* Make sure that the next ADC_Get will do an empty measurement first by setting the channel to an impossible value */
+    currentChannel = 10; 
     
     return 0;
 }
@@ -72,6 +39,49 @@ uint8_t ADC_Init(void)
 uint16_t ADC_Get(uint8_t channel)
 {
 	uint16_t adcdata;
+	
+	if (currentChannel != channel) //Did we use this channel the last time?
+	{
+		switch(channel)//If not, let's switch the admux
+		{
+		case 0:
+		    /* Enable ADC0 */
+		    ADMUX &= ~((1<<MUX0)|(1<<MUX1)|(1<<MUX2)|(1<<MUX3));
+		case 1:
+		    /* Enable ADC1 */
+		    ADMUX |= (1<<MUX0);
+		    ADMUX &= ~((1<<MUX1)|(1<<MUX2)|(1<<MUX3));
+		case 2:
+		    /* Enable ADC2 */
+		    ADMUX |= (1<<MUX1)|;
+		    ADMUX &= ~((1<<MUX0)|(1<<MUX2)|(1<<MUX3));
+		case 3:
+		    /* Enable ADC3 */
+		    ADMUX |= (1<<MUX0)|(1<<MUX1);
+		    ADMUX &= ~((1<<MUX2)|(1<<MUX3));
+		case 4:
+		    /* Enable ADC4 */
+		    ADMUX |= (1<<MUX2);
+		    ADMUX &= ~((1<<MUX0)|(1<<MUX1)|(1<<MUX3));
+		case 5:
+		    /* Enable ADC5 */
+		    ADMUX |= (1<<MUX0)|(1<<MUX2);
+		    ADMUX &= ~((1<<MUX1)|(1<<MUX3));
+		case 6:
+		    /* Enable ADC6 */
+		    ADMUX |= (1<<MUX1)|(1<<MUX2);
+		    ADMUX &= ~((1<<MUX0)|(1<<MUX3));
+		case 7:
+		    /* Enable ADC7 (only for TQFP package) */
+		    ADMUX |= (1<<MUX0)|(1<<MUX1)|(1<<MUX2);
+		    ADMUX &= ~(1<<MUX3);
+		default:
+		    //Some kind of error message?
+		}
+		
+		currentChannel = channel;	//Update the channel for the last measurement to the one we want this time
+		ADC_Get(channel);	//And do a dummy-read
+	}
 
 	/* Start measurement (takes 13 ADC clock cycles) */
 	ADCSRA |= (1<<ADSC);
