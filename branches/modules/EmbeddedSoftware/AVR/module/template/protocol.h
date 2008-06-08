be840d1f7b4093adba3302da9832135a471885b9
Linus Lundin <linus.lundin@gmail.com>
2008-06-08 13:28:42 +0000

    Changed template to use command files
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@864 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- branches/modules/EmbeddedSoftware/AVR/module/template/protocol.h
+++ branches/modules/EmbeddedSoftware/AVR/module/template/protocol.h
@@ -1,6 +1,13 @@
 #ifndef <TEMPLATE>_PROTOCOL
 #define <TEMPLATE>_PROTOCOL
+#include <drivers/can/modules/PhysicalQuantityCMD.h>
+#include <drivers/can/modules/GlobalCMD.h>
+/*
+ *      This file contains commands used for only for this module
+ *      How the Data-part of a frame with a sertain command is formed shall be documented on the project wiki
+ *      Only Id:s with numbers between 128 and 191 is allowed
+ */
 
-#define CAN_CMD_MODULE_DUMMY		0x01
+//#define CAN_CMD_MODULE_SPECIFIC_PWM	0x128
 
 #endif // <TEMPLATE>_PROTOCOL
