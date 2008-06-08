f6c7d9f7af669c25c505ff5a311fc7cd9aa12655
Linus Lundin <linus.lundin@gmail.com>
2008-06-08 13:22:20 +0000

    Added files for commands used in modules branch, Pysical Quantity and global commands
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@861 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ branches/modules/EmbeddedSoftware/AVR/avr-lib/drivers/can/modules/GlobalCMD.h
@@ -0,0 +1,10 @@
+#ifndef GLOBAL_CMD_H_
+#define GLOBAL_CMD_H_
+/*
+ *	This file contains commands that are not module specific, "global commands"
+ *	How the Data-part of a frame with a sertain unit is formed shall be documented on the project wiki
+ * 	Only Id:s with numbers between 0 and 63 is allowed
+ */
+//#define CAN_CMD_MODULE_GLOB_???			0
+
+#endif /*GLOBAL_CMD_H_*/
---
+++ branches/modules/EmbeddedSoftware/AVR/avr-lib/drivers/can/modules/PhysicalQuantityCMD.h
@@ -0,0 +1,12 @@
+#ifndef PHYS_QUAN_CMD_H_
+#define PHYS_QUAN_CMD_H_
+/*
+ *	This file contains commands used for standard units used by the modules build system
+ *	How the Data-part of a frame with a sertain unit is formed shall be documented on the project wiki
+ * 	Only Id:s with numbers between 64 and 127 is allowed
+ */
+#define CAN_CMD_MODULE_PHYS_PWM			64
+#define CAN_CMD_MODULE_PHYS_TEMPERATURE_CELSIUS	65
+#define CAN_CMD_MODULE_PHYS_HUMIDITY_PERCENT	66
+
+#endif /*PHYS_QUAN_CMD_H_*/
