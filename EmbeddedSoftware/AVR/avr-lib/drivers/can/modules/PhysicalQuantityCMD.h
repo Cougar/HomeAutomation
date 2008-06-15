75bd9c09c6ff5d8def08a810775b91d8386b3039
Linus Lundin <linus.lundin@gmail.com>
2008-06-15 18:07:21 +0000

    Changed command-files to define the commands in hex, makes the files easier to parse
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@874 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/can/modules/PhysicalQuantityCMD.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/can/modules/PhysicalQuantityCMD.h
@@ -3,12 +3,12 @@
 /*
  *	This file contains commands used for standard units used by the modules build system
  *	How the Data-part of a frame with a sertain unit is formed shall be documented on the project wiki
- * 	Only Id:s with numbers between 64 and 127 is allowed
+ * 	Only Id:s with numbers between 0x40 and 0x7f is allowed
  */
-#define CAN_CMD_MODULE_PHYS_PWM			64
-#define CAN_CMD_MODULE_PHYS_TEMPERATURE_CELSIUS	65
-#define CAN_CMD_MODULE_PHYS_HUMIDITY_PERCENT	66
-#define CAN_CMD_MODULE_PHYS_VOLTAGE		67
-#define CAN_CMD_MODULE_PHYS_PHONENUMBER		68
+#define CAN_CMD_MODULE_PHYS_PWM			0x40UL
+#define CAN_CMD_MODULE_PHYS_TEMPERATURE_CELSIUS	0x41UL
+#define CAN_CMD_MODULE_PHYS_HUMIDITY_PERCENT	0x42UL
+#define CAN_CMD_MODULE_PHYS_VOLTAGE			0x43UL
+#define CAN_CMD_MODULE_PHYS_PHONENUMBER		0x44UL
 
 #endif /*PHYS_QUAN_CMD_H_*/
--- EmbeddedSoftware/AVR/module/act_hd44780/protocol.h
+++ EmbeddedSoftware/AVR/module/act_hd44780/protocol.h
@@ -5,13 +5,13 @@
 /*
  *      This file contains commands used for only for this module
  *      How the Data-part of a frame with a sertain command is formed shall be documented on the project wiki
- *      Only Id:s with numbers between 128 and 191 is allowed
+ *      Only Id:s with numbers between 0x80 and 0xbf is allowed
  */
 
-#define CAN_CMD_MODULE_LCD_CLEAR		128
-#define CAN_CMD_MODULE_LCD_CURSOR		129
-#define CAN_CMD_MODULE_LCD_PRINT		130
-#define CAN_CMD_MODULE_LCD_SIZE		131
-#define CAN_CMD_MODULE_LCD_BACKLIGHT	132
+#define CAN_CMD_MODULE_LCD_CLEAR		0x80
+#define CAN_CMD_MODULE_LCD_CURSOR		0x81
+#define CAN_CMD_MODULE_LCD_PRINT		0x82
+#define CAN_CMD_MODULE_LCD_SIZE		0x83
+#define CAN_CMD_MODULE_LCD_BACKLIGHT	0x84
 
 #endif // ACT_HD44780_PROTOCOL
