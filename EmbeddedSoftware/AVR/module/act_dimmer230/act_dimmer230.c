6ab642df7df7820fb4596294ab157312b88b3136
Anders Runeson <runeson@gmail.com>
2008-07-28 19:53:10 +0000

    dimmer230: more work
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@894 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/act_dimmer230/act_dimmer230.c
+++ EmbeddedSoftware/AVR/module/act_dimmer230/act_dimmer230.c
@@ -1,50 +1,121 @@
 
 #include "act_dimmer230.h"
+
+uint8_t dimmerValue = 0;	//store in eeprom later
+							//value of 0 is zero output, value of 255 is max output
 uint8_t state = ACT_DIMMMER230_STATE_IDLE;
+uint16_t timerStore = 0;	//used if triac should be trigged after zerocross detected but before real zerocross event
+const uint16_t powerStepTable[] PROGMEM = {845, 1068, 1225, 1352, 1459, 1553, 1638, 1716, 1788, 1855, 1917, 1976, 2032, 2086, 2138, 2188, 2236, 2282, 2327, 2370, 2412, 2453, 2493, 2532, 2570, 2607, 2643, 2679, 2714, 2748, 2782, 2815, 2848, 2880, 2911, 2942, 2972, 3002, 3032, 3061, 3090, 3119, 3147, 3175, 3203, 3230, 3257, 3284, 3311, 3337, 3363, 3389, 3415, 3440, 3465, 3490, 3515, 3539, 3563, 3587, 3611, 3635, 3659, 3682, 3705, 3728, 3751, 3774, 3797, 3820, 3843, 3865, 3887, 3909, 3931, 3953, 3975, 3997, 4019, 4041, 4062, 4083, 4104, 4125, 4146, 4167, 4188, 4209, 4230, 4251, 4272, 4292, 4313, 4333, 4354, 4374, 4395, 4415, 4435, 4455, 4476, 4496, 4516, 4536, 4556, 4576, 4596, 4615, 4635, 4655, 4675, 4694, 4714, 4734, 4754, 4773, 4793, 4813, 4832, 4852, 4872, 4891, 4911, 4931, 4951, 4970, 4990};
+
+#if act_dimmer230_SYNC>0
+uint8_t zeroCrossCnt = 0;
+#endif
 
 ISR (TIMER1_COMPA_vect) 
 {
-
+	//TODO: testa hur lång denna loop behöver vara
+	PORTC |= _BV(PC4);
+   	uint8_t dummycnt = 1;
+	while (dummycnt > 0) 
+	{
+		dummycnt++;
+		asm("nop");
+		asm("nop");
+	}
+	PORTC &= ~_BV(PC4);			
+	
+	if (timerStore == 0)
+	{
+		TIMSK1=0;							//disable timer interrupt
+		state = ACT_DIMMMER230_STATE_IDLE;
+	} 
+	else 
+	{
+		TCNT1 = 0;							//reset timer1 count register
+		OCR1A = timerStore;
+		timerStore = 0;
+	}
 }
+
 ISR (act_dimmer230_ZC_PCINT_vect) 
 {
-	if (state==ACT_DIMMMER230_STATE_IDLE && (act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT))) 
+	if (act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT)) 
 	{
-		
-		//enable timer and timer interrupt
-		TCNT1 = 0;				//reset timer1 count register
-		// here timer oc1a register should be set to the correct time (zero-cross time + dimmer value)
-		TIFR1=(1<<OCF1A);		//clear interrupt flag before enabling interrupt
-		TIMSK1|=(1<<OCIE1A);
+		//OBS glöm inte timeDiff
+		uint16_t newTimerVal = 0; //timeDiff
 
-		state = ACT_DIMMMER230_STATE_TIMER_ON;
+		//here new timer value is calculated 
+
+		if (dimmerValue > 0 && dimmerValue < 127)
+		{
+			newTimerVal += pgm_read_word(&powerStepTable[dimmerValue-1]);
+		} 
+		else if (dimmerValue < 255)
+		{
+			newTimerVal += 10000 - pgm_read_word(&powerStepTable[254-dimmerValue]);
+		} 
+		else if (dimmerValue == 255)
+		{
+			newTimerVal += 9900;	//almost max, to make sure we don't fire after next zerocross
+		}
+
+		if (state==ACT_DIMMMER230_STATE_TIMER_ON)
+		{
+			//TODO: this will probably end up out of sync, not a good solution?
+			timerStore = newTimerVal + TCNT1 - OCR1A;
+		}
+		else 
+		{
+			TCNT1 = 0;				//reset timer1 count register
+			OCR1A = newTimerVal;
+
+			//enable timer and timer interrupt
+			TIFR1=(1<<OCF1A);		//clear interrupt flag before enabling interrupt
+			TIMSK1|=(1<<OCIE1A);
+
+			state = ACT_DIMMMER230_STATE_TIMER_ON;
+		}
+
+#if act_dimmer230_SYNC>0
+		zeroCrossCnt++;
+		if (zeroCrossCnt >= act_dimmer230_SYNC)
+		{
+			zeroCrossCnt = 0;
+			//TODO: send on can
+		}
+#endif		
 	}
 }
 
 void act_dimmer230_Init(void)
 {
-	///TODO: Initialize hardware etc here
-
-	// set dimmer channel1 to output 0
+	// set dimmer channel1 port to output 0
 	act_dimmer230_CHAN1_PORT &= ~_BV(act_dimmer230_CHAN1_BIT);
 	act_dimmer230_CHAN1_DDR |= _BV(act_dimmer230_CHAN1_BIT);
-	// set zero cross detection to input, no pullup
+	// set zero cross detection port to input, no pullup
 	act_dimmer230_ZC_PORT &= ~_BV(act_dimmer230_ZC_BIT);
 	act_dimmer230_ZC_DDR &= ~_BV(act_dimmer230_ZC_BIT);
 	
-	
 	TIMSK1=0;							//disable timer interrupt
 	TCCR1A=0;
-	TCCR1B=(1<<CS11);					//enable timer, set to prescaler 8, must be changed if clk is changed
 	
 	//wait for high (ac leaving zero cross)
 	while (!(act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT))) { }
+	TCCR1B=(1<<CS11);					//enable timer, set to prescaler 8, must be changed if cpu freq is changed
 	//wait for low (ac approaching zero cross)
 	while (act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT)) { }
 	uint16_t timeAtFall = TCNT1;	//Timer_GetTicks();
 	//wait for high (ac leaving zero cross)
 	while (!(act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT))) { }
 	uint16_t timeAtRise = TCNT1;	//Timer_GetTicks();
+	
+	uint16_t timeDiff=0;
+	// warning, watch out for wrap around on timestamps (something is broken if wrapped)
+	if (timeAtFall < timeAtRise) {
+		//calculate time from falling edge to ac zero cross value
+		timeDiff = timeAtRise-timeAtFall;
+		timeDiff = timeDiff >> 1; //divide by two
+	}
 
 	//setup interrupt on zerocross, pcint
 	PCMSK0=(1<<act_dimmer230_ZC_PCINT_BIT);
