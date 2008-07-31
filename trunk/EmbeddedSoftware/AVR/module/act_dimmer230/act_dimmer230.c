fd82e950e9ea563766b6e6ad2288d8e5e59ad0aa
Anders Runeson <runeson@gmail.com>
2008-07-31 05:40:45 +0000

    modules/dimmer230: fixes after implementation tests
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@896 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/module/act_dimmer230/act_dimmer230.c
+++ trunk/EmbeddedSoftware/AVR/module/act_dimmer230/act_dimmer230.c
@@ -1,10 +1,18 @@
 
 #include "act_dimmer230.h"
 
-uint8_t dimmerValue = 0;	//store in eeprom later
-							//value of 0 is zero output, value of 255 is max output
+uint8_t demo = 0;
+//uint8_t demo = 2;	//disable demo
+
+//uint16_t periodTime = 0;
+			//measured during init (by taking the time difference between two zero cross)
+			//not used, bad idea, the powerStepTable below asumes 10000
+uint16_t xcTimeDiff=0;
+			//zero cross sync (difference between pos and neg flank of zero cross, divided by 2)
+uint8_t dimmerValueCh1 = 0;
+			//store in eeprom later
+			//value of 0 is zero output, value of 255 is max output
 uint8_t state = ACT_DIMMMER230_STATE_IDLE;
-uint16_t timerStore = 0;	//used if triac should be trigged after zerocross detected but before real zerocross event
 const uint16_t powerStepTable[] PROGMEM = {845, 1068, 1225, 1352, 1459, 1553, 1638, 1716, 1788, 1855, 1917, 1976, 2032, 2086, 2138, 2188, 2236, 2282, 2327, 2370, 2412, 2453, 2493, 2532, 2570, 2607, 2643, 2679, 2714, 2748, 2782, 2815, 2848, 2880, 2911, 2942, 2972, 3002, 3032, 3061, 3090, 3119, 3147, 3175, 3203, 3230, 3257, 3284, 3311, 3337, 3363, 3389, 3415, 3440, 3465, 3490, 3515, 3539, 3563, 3587, 3611, 3635, 3659, 3682, 3705, 3728, 3751, 3774, 3797, 3820, 3843, 3865, 3887, 3909, 3931, 3953, 3975, 3997, 4019, 4041, 4062, 4083, 4104, 4125, 4146, 4167, 4188, 4209, 4230, 4251, 4272, 4292, 4313, 4333, 4354, 4374, 4395, 4415, 4435, 4455, 4476, 4496, 4516, 4536, 4556, 4576, 4596, 4615, 4635, 4655, 4675, 4694, 4714, 4734, 4754, 4773, 4793, 4813, 4832, 4852, 4872, 4891, 4911, 4931, 4951, 4970, 4990};
 
 #if act_dimmer230_SYNC>0
@@ -13,58 +21,49 @@ uint8_t zeroCrossCnt = 0;
 
 ISR (TIMER1_COMPA_vect) 
 {
-	//TODO: testa hur lång denna loop behöver vara
+	//testa hur lång denna loop behöver vara
+	//verkar fungera upp till 249-250 sen blir det för korta pulser
 	PORTC |= _BV(PC4);
-   	uint8_t dummycnt = 1;
+   	uint8_t dummycnt = 240;
 	while (dummycnt > 0) 
 	{
 		dummycnt++;
 		asm("nop");
-		asm("nop");
 	}
 	PORTC &= ~_BV(PC4);			
 	
-	if (timerStore == 0)
-	{
-		TIMSK1=0;							//disable timer interrupt
-		state = ACT_DIMMMER230_STATE_IDLE;
-	} 
-	else 
-	{
-		TCNT1 = 0;							//reset timer1 count register
-		OCR1A = timerStore;
-		timerStore = 0;
-	}
+	TIMSK1=0;							//disable timer interrupt
+	state = ACT_DIMMMER230_STATE_IDLE;
 }
 
 ISR (act_dimmer230_ZC_PCINT_vect) 
 {
-	if (act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT)) 
+	//only execute if zerocross pin is low
+	if (!(act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT))) 
 	{
-		//OBS glöm inte timeDiff
-		uint16_t newTimerVal = 0; //timeDiff
+		uint16_t newTimerVal = xcTimeDiff;
 
 		//here new timer value is calculated 
 
-		if (dimmerValue > 0 && dimmerValue < 127)
+		if (dimmerValueCh1 > 0 && dimmerValueCh1 <= 127)
 		{
-			newTimerVal += ACT_DIMMMER230_PERIOD_TIME - pgm_read_word(&powerStepTable[dimmerValue-1]);
+			newTimerVal += ACT_DIMMMER230_PERIOD_TIME - pgm_read_word(&powerStepTable[dimmerValueCh1-1]);
 		} 
-		else if (dimmerValue < 255)
+		else if (dimmerValueCh1 < 255)
 		{
-			newTimerVal += pgm_read_word(&powerStepTable[254-dimmerValue]);
+			newTimerVal += pgm_read_word(&powerStepTable[254-dimmerValueCh1]);
 		} 
-		else if (dimmerValue == 255)
+		else if (dimmerValueCh1 == 255)
 		{
 			newTimerVal += 100;	//almost max, to make sure we don't fire before zerocross
 		}
 		
-		if (newTimerVal > ACT_DIMMMER230_PERIOD_TIME) 
+		if (newTimerVal > ACT_DIMMMER230_PERIOD_TIME)
 		{
 			newTimerVal -= ACT_DIMMMER230_PERIOD_TIME;
 		}
 
-		if (dimmerValue > 0) 
+		if (dimmerValueCh1 > 0) 
 		{
 			TCNT1 = 0;				//reset timer1 count register
 			OCR1A = newTimerVal;
@@ -82,6 +81,28 @@ ISR (act_dimmer230_ZC_PCINT_vect)
 		{
 			zeroCrossCnt = 0;
 			//TODO: send on can
+
+			StdCan_Msg_t txMsg;
+			txMsg.Length = 6;
+			txMsg.Data[0] = (newTimerVal>>8)&0xff;
+			txMsg.Data[1] = newTimerVal&0xff;
+			//txMsg.Data[2] = (periodTime>>8)&0xff;
+			//txMsg.Data[3] = periodTime&0xff;
+			txMsg.Data[4] = (xcTimeDiff>>8)&0xff;
+			txMsg.Data[5] = xcTimeDiff&0xff;
+			//StdCan_Put(&txMsg);
+
+			if (demo == 0) {
+				dimmerValueCh1++;
+				if (dimmerValueCh1 == 255) {
+					demo = 1;
+				}
+			} else if (demo == 1) {
+				dimmerValueCh1--;
+				if (dimmerValueCh1 == 0) {
+					demo = 0;
+				}
+			}
 		}
 #endif		
 	}
@@ -108,13 +129,21 @@ void act_dimmer230_Init(void)
 	//wait for high (ac leaving zero cross)
 	while (!(act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT))) { }
 	uint16_t timeAtRise = TCNT1;	//Timer_GetTicks();
-	
-	uint16_t timeDiff=0;
+
+	//wait for low (ac approaching zero cross)
+	//while (act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT)) { }
+	//wait for high (ac leaving zero cross)
+	//while (!(act_dimmer230_ZC_PIN&(1<<act_dimmer230_ZC_BIT))) { }
+	//uint16_t timeAtRise2 = TCNT1;	//Timer_GetTicks();
+	//if (timeAtRise < timeAtRise2) {
+	//	periodTime = timeAtRise2 - timeAtRise;
+	//}
+
 	// warning, watch out for wrap around on timestamps (something is broken if wrapped)
 	if (timeAtFall < timeAtRise) {
 		//calculate time from falling edge to ac zero cross value
-		timeDiff = timeAtRise-timeAtFall;
-		timeDiff = timeDiff >> 1; //divide by two
+		xcTimeDiff = timeAtRise-timeAtFall;
+		xcTimeDiff = xcTimeDiff >> 1; //divide by two
 	}
 
 	//setup interrupt on zerocross, pcint
