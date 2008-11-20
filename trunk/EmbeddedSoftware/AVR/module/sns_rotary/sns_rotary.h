9d2d2bc5a03fd906ceb17a060d0e4f94d300194d
Linus Lundin <linus.lundin@gmail.com>
2008-11-20 19:24:01 +0000

    Added rotary encoder module
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@926 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/EmbeddedSoftware/AVR/module/sns_rotary/sns_rotary.h
@@ -0,0 +1,24 @@
+#ifndef SNS_ROTARY
+#define SNS_ROTARY
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+/* system files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+/* lib files */
+#include <config.h>
+#include <bios.h>
+#include <drivers/can/stdcan.h>
+#include <drivers/timer/timer.h>
+
+#include "protocol.h"
+
+void sns_rotary_Init(void);
+void sns_rotary_Process(void);
+void sns_rotary_HandleMessage(StdCan_Msg_t *rxMsg);
+void sns_rotary_List(uint8_t ModuleSequenceNumber);
+
+#endif // SNS_ROTARY
