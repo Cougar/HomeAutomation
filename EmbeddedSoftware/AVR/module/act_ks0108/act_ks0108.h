5d8ba0deda7406d6ba1142e878d597030979e68a
Linus Lundin <linus.lundin@gmail.com>
2008-11-24 20:59:33 +0000

    Added module for gLcd, only display test, no protocol implemented
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@928 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ EmbeddedSoftware/AVR/module/act_ks0108/act_ks0108.h
@@ -0,0 +1,27 @@
+#ifndef ACT_KS0108
+#define ACT_KS0108
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+/* system files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+/* lib files */
+#include <config.h>
+#include <bios.h>
+#include <drivers/can/stdcan.h>
+#include <drivers/timer/timer.h>
+
+#include <drivers/lcd/glcd/ks0108.h>
+#include <avr/pgmspace.h>
+
+#include "protocol.h"
+
+void act_ks0108_Init(void);
+void act_ks0108_Process(void);
+void act_ks0108_HandleMessage(StdCan_Msg_t *rxMsg);
+void act_ks0108_List(uint8_t ModuleSequenceNumber);
+
+#endif // ACT_KS0108
