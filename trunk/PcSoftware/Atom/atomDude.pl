debbbc69826df4eb1082d00ab33ff844e88aac2e
Mattias Runge <mattias@runge.se>
2009-01-12 08:50:59 +0000

    Atom: Made reset node work with a timer and implemented support in atomDude
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1040 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/Atom/atomDude.pl
+++ trunk/PcSoftware/Atom/atomDude.pl
@@ -13,37 +13,45 @@ my $port = 1202;
 my $filename = "";
 my $hwid = "";
 my $bios = "false";
+my $reset = "false";
 
 foreach $argnum (0 .. $#ARGV)
 {
 	if ($ARGV[$argnum] eq "-h")
 	{
 		$hostname = $ARGV[$argnum+1];
+		next;
+
 	}
 	elsif ($ARGV[$argnum] eq "-p")
 	{
 		$port = $ARGV[$argnum+1];
+		next;
+
 	}
 	elsif ($ARGV[$argnum] eq "-i")
 	{
 		$hwid = $ARGV[$argnum+1];
+		next;
+
 	}
 	elsif ($ARGV[$argnum] eq "-f")
 	{
 		$filename = $ARGV[$argnum+1];
+		next;
+
 	}
 	elsif ($ARGV[$argnum] eq "-b")
 	{
 		$bios = "true";
 	}
+	elsif ($ARGV[$argnum] eq "-r")
+	{
+		$reset = "true";
+	}
 }
 
-if ($filename eq "")
-{
-	print "You must specify a file!\n";
-	exit 1;
-}
-elsif ($hwid eq "")
+if ($hwid eq "")
 {
 	die "You must specify a hardware id!\n";
 	exit 1;
@@ -53,9 +61,11 @@ elsif ($hwid eq "")
 @hwidChars = split(//, $hwid);
 $hwid = @hwidChars[0]. @hwidChars[1]. @hwidChars[8]. @hwidChars[9]. @hwidChars[6]. @hwidChars[7]. @hwidChars[4]. @hwidChars[5]. @hwidChars[2]. @hwidChars[3];
 
-open(FILE, $filename) || die("Could not open file!");
-@fileData=<FILE>; 
-close(FILE);
+print "atomDude settings:\n";
+print "Hardware id: " . $hwid . "\n";
+print "Atom address: " . $hostname . ":" . $port . "\n";
+print "Hexfile: " . $filename . "\n";
+print "Parameters: " . "\n";
 
 my $sock = new IO::Socket::INET(
 				PeerAddr => $hostname, 
@@ -63,36 +73,76 @@ my $sock = new IO::Socket::INET(
 				Proto => 'tcp'
 				);
 die "Could not create socket: $!\n" unless $sock;
+print "Connected to Atom\n";
 
-sleep(1);
-
-print $sock "programNode('", $hwid, "','";
-
-foreach $line (@fileData)
+if ($filename ne "")
 {
-	print $sock $line;
-} 
+	open(FILE, $filename) || die("Could not open file!");
+	@fileData=<FILE>; 
+	close(FILE);
+	print "Hexfile loaded\n";
 
-print $sock "', ", $bios, ");\n";
+	print $sock "programNode('", $hwid, "','";
 
-while (1)
+	foreach $line (@fileData)
+	{
+		print $sock $line;
+	} 
+
+	print $sock "', ", $bios, ");\n";
+
+	my $pLoop = 1;
+
+	while ($pLoop eq 1)
+	{
+		$sock->recv($text, 128);
+		
+		if (rindex($text, "STOP") ne -1)
+		{
+			print $text;
+			$pLoop = 0;
+		}
+		elsif ($text ne '')
+		{
+			print $text;
+		}
+		else
+		{
+			print "Connection closed by other side!";
+			close($sock);
+			exit 1;
+		}
+	}
+}
+
+if ($reset eq "true")
 {
-	$sock->recv($text, 128);
+	print $sock "resetNode('", $hwid, "');\n";
+
+	my $rLoop = 1;
 	
-	if (rindex($text, "STOP") ne -1)
+	while ($rLoop eq 1)
 	{
-		print "\n";
-		exit 0;
-	}
-	elsif ($text ne '')
-	{
+		$sock->recv($text, 128);
 		print $text;
-	}
-	else
-	{
-		print "Connection closed by other side!";
-		exit 0;
+		
+		if (rindex($text, "Success") ne -1)
+		{
+			$rLoop = 0;
+		}
+		elsif (rindex($text, "Failed") ne -1)
+		{
+			close($sock);
+			exit 1;
+		}
+		elsif ($text eq '')
+		{
+			print "Connection closed by other side!";
+			close($sock);
+			exit 1;
+		}
 	}
 }
 
 close($sock);
+exit 0;
