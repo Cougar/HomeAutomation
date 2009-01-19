b8b8fe68321a48fcb8c0509359d783026b655a26
Anders Runeson <runeson@gmail.com>
2009-01-19 19:23:59 +0000

    atom: some documentation in IntelHex.js
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1065 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/Atom/Services/System/IntelHex.js
+++ PcSoftware/Atom/Services/System/IntelHex.js
@@ -1,9 +1,11 @@
 
 const MAX_SIZE = 524288;
 
+/* Call constructor with the contents of an Intel Hex file (rows as arrays) 
+   http://en.wikipedia.org/wiki/.hex
+*/
 function IntelHex(hexLines)
 {
-//print("constructor of IntelHex\n");
 	this.myValid = false;
 	this.myAddrLower = 0x7FFFFFFF;
 	this.myAddrUpper = 0;
@@ -15,32 +17,30 @@ function IntelHex(hexLines)
 	var lineAddr = 0;
 	var lineCode = 0;
 	var fullAddr = 0;
-
+	
+	/* Parse hexfile to binary (array of bytes) */
 	for (var n = 0; n < hexLines.length; n++)
 	{
 		if (hexLines[n].length>=11 && hexLines[n].substr(0,1)==":") {
-			lineDataCount = hex2uint(hexLines[n].substr(1,2));
-			lineAddr =  hex2uint(hexLines[n].substr(3,4)) & 65535;	 
-			lineCode = hex2uint(hexLines[n].substr(7,2));   
+			lineDataCount = hex2uint(hexLines[n].substr(1,2));		/* Byte count */
+			lineAddr =  hex2uint(hexLines[n].substr(3,4)) & 65535;	/* Address */
+			lineCode = hex2uint(hexLines[n].substr(7,2));			/* Record type */
 			fullAddr = (lineAddrHigh * 65536) + lineAddr;
 
-			switch(lineCode) 
+			switch(lineCode)
 			{
-				case 0:
+				case 0:	/* If Data record */
 					if ((fullAddr + lineDataCount - 1) > this.myAddrUpper) this.myAddrUpper = fullAddr + lineDataCount - 1;
 					if (lineCode == 0 && fullAddr < this.myAddrLower) this.myAddrLower = fullAddr;
 					for (var j = 0; j < lineDataCount; j++) {
 						this.myHexData[fullAddr] = hex2uint(hexLines[n].substr(j * 2 + 9, 2));
-						//this.myHexData[fullAddr] &= 255;
 						fullAddr++;
 					}
-//print("found a record of type 0, fulladdr: "+fullAddr+"\nhexLines.length: "+hexLines.length+"\n"+hexLines[n]+"\n");
 					break;
-				case 4:
+				case 4:	/* If Extended Linear Address Record */
 					lineAddrHigh = hex2uint(hexLines[n].substr(9, 4)) & 65535;
 					break;
-				case 1: 
-//print("found a record of type 1");
+				case 1:	/* If End Of File record */
 					this.myValid = true;
 					break; 
 				
@@ -54,6 +54,7 @@ IntelHex.prototype.myAddrLower = null;
 IntelHex.prototype.myAddrUpper = null;
 IntelHex.prototype.myValid = null;
 
+/* Get the CRC of data, includes "holes" in the middle */
 IntelHex.prototype.getCRC16 = function()
 {
 	var crc=0;
@@ -81,6 +82,7 @@ IntelHex.prototype.crc16_update = function(crc, byte)
 	return crc;
 }
 
+/* Get the length of the data */
 IntelHex.prototype.getLength = function()
 {
 	if (this.myAddrUpper+1>this.myAddrLower)
@@ -93,21 +95,25 @@ IntelHex.prototype.getLength = function()
 	}
 }
 
+/* Get the validity of the data (check if end of file record was found) */
 IntelHex.prototype.isValid = function()
 {
 	return this.myValid;
 }
 
+/* Get the lowest address of the data */
 IntelHex.prototype.getAddrLower = function()
 {
 	return this.myAddrLower;
 }
 
+/* Get the highest address of the data */
 IntelHex.prototype.getAddrUpper = function()
 {
 	return this.myAddrUpper;
 }
 
+/* Get a byte at a certain address */
 IntelHex.prototype.getByte = function(address)
 {
 	var byte = 0xff;
