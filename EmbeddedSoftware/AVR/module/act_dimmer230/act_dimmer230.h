cc5de0df61c08107cbbf1f3cc6a00f1b877d712a
Anders Runeson <runeson@gmail.com>
2009-02-08 07:48:50 +0000

    modules/dimmer: uses eeprom to store dimmer value. Added a lot of documentation to the code
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1081 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/act_dimmer230/act_dimmer230.h
+++ EmbeddedSoftware/AVR/module/act_dimmer230/act_dimmer230.h
@@ -62,4 +62,52 @@ void act_dimmer230_Process(void);
 void act_dimmer230_HandleMessage(StdCan_Msg_t *rxMsg);
 void act_dimmer230_List(uint8_t ModuleSequenceNumber);
 
+#ifdef act_dimmer230_USEEEPROM
+	struct act_dimmer230_Data{
+		// the dimmer will store its current value in eeprom, to use when starting
+		uint8_t eeDimmerValue;
+	};	
+
+// Do not change anything below this line.
+// ----------------------------------------------------------------
+
+	#include <drivers/misc/eeprom_crc8.h>
+	#include <avr/eeprom.h>
+	#define EEDATA 			(uint8_t*)&eeprom_act_dimmer230.Data
+	#define EEDATA16		(uint16_t*)&eeprom_act_dimmer230.Data
+	#define EEDATA_CALC_CRC		eeprom_crc8((uint8_t*)&eeprom_act_dimmer230.Data,sizeof(struct act_dimmer230_Data))
+	#define EEDATA_UPDATE_CRC	eeprom_write_byte((uint8_t*)&eeprom_act_dimmer230.crc,eeprom_crc8((uint8_t*)&eeprom_act_dimmer230.Data,sizeof(struct act_dimmer230_Data)))
+	#define EEDATA_OK		EEDATA_CALC_CRC == EEDATA_STORED_CRC
+	#define EEDATA_STORED_CRC 	eeprom_read_byte((uint8_t*)&eeprom_act_dimmer230.crc)
+	
+	struct eeprom_act_dimmer230{
+		struct act_dimmer230_Data Data;
+		uint8_t crc;
+	};
+	extern struct eeprom_act_dimmer230 EEMEM eeprom_act_dimmer230;
+	
+	#define WITH_CRC	1
+	#define WITHOUT_CRC	0
+	static __inline__ void eeprom_write_byte_crc(uint8_t *__p, uint8_t __value, uint8_t crc) {
+	  if (__value != eeprom_read_byte(__p))
+	  {
+	    eeprom_write_byte(__p, __value);
+	    if (crc == WITH_CRC)
+	    {
+	      EEDATA_UPDATE_CRC;
+	    }
+	  }
+	}
+	static __inline__ void eeprom_write_word_crc(uint16_t *__p, uint16_t __value, uint8_t crc) {
+	  if (__value != eeprom_read_word(__p))
+	  {
+	    eeprom_write_word(__p, __value);
+	    if (crc == WITH_CRC)
+	    {
+	      EEDATA_UPDATE_CRC;
+	    }
+	  }
+	}
+#endif
+
 #endif // ACT_DIMMER230
