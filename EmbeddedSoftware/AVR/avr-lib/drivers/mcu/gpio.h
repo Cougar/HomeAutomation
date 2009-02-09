7b19bb17b8e8d7759e7880cf107d95ca141e95bd
Linus Lundin <linus.lundin@gmail.com>
2009-02-09 19:56:14 +0000

    Added functionality to make easier defines for corecard pins
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1082 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/mcu/gpio.h
+++ EmbeddedSoftware/AVR/avr-lib/drivers/mcu/gpio.h
@@ -118,6 +118,111 @@ HAL_ASSIGN_PIN(GPIO_C5, C, 5);
 //HAL_ASSIGN_PIN(GPIO_C6, C, 6);
 
 
+// for corecard:
+#define EXP_A	&PORTB,&PINB,&DDRB,PB7
+#define PORT_EXP_A	PORTB
+#define PIN_EXP_A	PINB
+#define DDR_EXP_A	DDRB
+#define NR_EXP_A	PB7
+
+#define EXP_B	&PORTB,&PINB,&DDRB,PB2
+#define PORT_EXP_B	PORTB
+#define PIN_EXP_B	PINB
+#define DDR_EXP_B	DDRB
+#define NR_EXP_B	PB2
+
+#define EXP_C	&PORTB,&PINB,&DDRB,PB1
+#define PORT_EXP_C	PORTB
+#define PIN_EXP_C	PINB
+#define DDR_EXP_C	DDRB
+#define NR_EXP_C	PB1
+
+#define EXP_D	&PORTB,&PINB,&DDRB,PB0
+#define PORT_EXP_D	PORTB
+#define PIN_EXP_D	PINB
+#define DDR_EXP_D	DDRB
+#define NR_EXP_D	PB0
+
+#define EXP_E	&PORTD,&PIND,&DDRD,PD7
+#define PORT_EXP_E	PORTD
+#define PIN_EXP_E	PIND
+#define DDR_EXP_E	DDRD
+#define NR_EXP_E	PD7
+
+#define EXP_F	&PORTD,&PIND,&DDRD,PD6
+#define PORT_EXP_F	PORTD
+#define PIN_EXP_F	PIND
+#define DDR_EXP_F	DDRD
+#define NR_EXP_F	PD6
+
+#define EXP_G	&PORTD,&PIND,&DDRD,PD5
+#define PORT_EXP_G	PORTD
+#define PIN_EXP_G	PIND
+#define DDR_EXP_G	DDRD
+#define NR_EXP_G	PD5
+
+#define EXP_H	&PORTD,&PIND,&DDRD,PD4
+#define PORT_EXP_H	PORTD
+#define PIN_EXP_H	PIND
+#define DDR_EXP_H	DDRD
+#define NR_EXP_H	PD4
+
+#define EXP_I	&PORTD,&PIND,&DDRD,PD2
+#define PORT_EXP_I	PORTD
+#define PIN_EXP_I	PIND
+#define DDR_EXP_I	DDRD
+#define NR_EXP_I	PD2
+
+#define EXP_J	&PORTD,&PIND,&DDRD,PD1
+#define PORT_EXP_J	PORTD
+#define PIN_EXP_J	PIND
+#define DDR_EXP_J	DDRD
+#define NR_EXP_J	PD1
+
+#define EXP_K	&PORTD,&PIND,&DDRD,PD0
+#define PORT_EXP_K	PORTD
+#define PIN_EXP_K	PIND
+#define DDR_EXP_K	DDRD
+#define NR_EXP_K	PC0
+
+#define EXP_L	&PORTC,&PINC,&DDRC,PC5
+#define PORT_EXP_L	PORTC
+#define PIN_EXP_L	PINC
+#define DDR_EXP_L	DDRC
+#define NR_EXP_L	PC5
+
+#define EXP_M	&PORTC,&PINC,&DDRC,PC4
+#define PORT_EXP_M	PORTC
+#define PIN_EXP_M	PINC
+#define DDR_EXP_M	DDRC
+#define NR_EXP_M	PC4
+
+#define EXP_N	&PORTC,&PINC,&DDRC,PC2
+#define PORT_EXP_N	PORTC
+#define PIN_EXP_N	PINC
+#define DDR_EXP_N	DDRC
+#define NR_EXP_N	PC2
+
+#define EXP_O	&PORTC,&PINC,&DDRC,PC1
+#define PORT_EXP_O	PORTC
+#define PIN_EXP_O	PINC
+#define DDR_EXP_O	DDRC
+#define NR_EXP_O	PC1
+
+#define EXP_P	&PORTC,&PINC,&DDRC,PC0
+#define PORT_EXP_P	PORTC
+#define PIN_EXP_P	PINC
+#define DDR_EXP_P	DDRC
+#define NR_EXP_P	PC0
+
+static __inline__ void gpio_set_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port |= (1 << nr);}
+static __inline__ void gpio_clr_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port &= ~(1 << nr);}
+static __inline__ void gpio_set_out(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*ddr |= (1 << nr);}
+static __inline__ void gpio_set_in(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*ddr &= ~(1 << nr);}
+static __inline__ void gpio_set_pullup(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port |= (1 << nr);}
+static __inline__ void gpio_clr_pullup(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port &= ~(1 << nr);}
+static __inline__ void gpio_toggle_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port ^= (1 << nr);}
+static __inline__ uint8_t gpio_get_state(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {return (*pin & (1 << nr)) != 0;}
 
 #endif
 //eof gpio.h
