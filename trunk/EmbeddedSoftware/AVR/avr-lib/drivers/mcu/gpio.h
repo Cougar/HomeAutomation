0bd8eb3429c1ba0fa8521ef3557906125c5d1134
Anders Runeson <runeson@gmail.com>
2009-02-10 16:53:18 +0000

    drivers/gpio: added defines for all gpio, cleanup and comments for new defines
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1084 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/avr-lib/drivers/mcu/gpio.h
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/mcu/gpio.h
@@ -12,111 +12,131 @@
 #include <avr/io.h>
 
 /******************************************************************************
-* void GPIOX_set() sets GPIOX pin to logical 1 level.
-* void GPIOX_clr() clears GPIOX pin to logical 0 level.
-* void GPIOX_make_in makes GPIOX pin as input.
-* void GPIOX_make_out makes GPIOX pin as output.
-* uint8_t GPIOX_read() returns logical level GPIOX pin.
-* uint8_t GPIOX_state() returns configuration of GPIOX port.
+* Port output functions
+* void gpio_set_out() makes GPIOX pin as output
+* void gpio_set_pin() sets GPIOX pin to logical 1 level (when output)
+* void gpio_clr_pin() clears GPIOX pin to logical 0 level (when output)
+* void gpio_toggle_pin() toggles level of output (when output)
+*
+* Port input functions
+* void gpio_set_in() makes GPIOX pin as input
+* void gpio_set_pullup() makes GPIOX pulled high (when input)
+* void gpio_clr_pullup() removes pullup on GPIOX (when input)
+* uint8_t gpio_get_state() returns logical level GPIOX pin
 *******************************************************************************/
-#define INLINE static inline __attribute__ ((always_inline))
+static __inline__ void gpio_set_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port |= (1 << nr);}
+static __inline__ void gpio_clr_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port &= ~(1 << nr);}
+static __inline__ void gpio_set_out(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*ddr |= (1 << nr);}
+static __inline__ void gpio_set_in(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*ddr &= ~(1 << nr);}
+static __inline__ void gpio_set_pullup(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port |= (1 << nr);}
+static __inline__ void gpio_clr_pullup(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port &= ~(1 << nr);}
+static __inline__ void gpio_toggle_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port ^= (1 << nr);}
+static __inline__ uint8_t gpio_get_state(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {return (*pin & (1 << nr)) != 0;}
 
-#define HAL_ASSIGN_PIN(name, port, bit) \
-INLINE void  GPIO_##name##_set(void)         {PORT##port |= (1 << bit);} \
-INLINE void  GPIO_##name##_clr(void)         {PORT##port &= ~(1 << bit);} \
-INLINE uint8_t  GPIO_##name##_read(void)     {return (PIN##port & (1 << bit)) != 0;} \
-INLINE uint8_t  GPIO_##name##_state(void)    {return (DDR##port & (1 << bit)) != 0;} \
-INLINE void  GPIO_##name##_make_out(void)    {DDR##port |= (1 << bit);} \
-INLINE void  GPIO_##name##_make_in(void)     {DDR##port &= ~(1 << bit); PORT##port &= ~(1 << bit);} \
-INLINE void  GPIO_##name##_make_pullup(void) {PORT##port |= (1 << bit);}\
-INLINE void  GPIO_##name##_toggle(void)      {PORT##port ^= (1 << bit);} 
+// generic ports:
+#define GPIO_D0			&PORTD,&PIND,&DDRD,PD0
+#define PORT_GPIO_D0	PORTD
+#define PIN_GPIO_D0		PIND
+#define DDR_GPIO_D0		DDRD
+#define NR_GPIO_D0		PD0
 
-// the macros for the manipulation by EXP_A
-HAL_ASSIGN_PIN(EXP_A, B, 7);
-// the macros for the manipulation by EXP_B
-HAL_ASSIGN_PIN(EXP_B, B, 2);
-// the macros for the manipulation by EXP_C
-HAL_ASSIGN_PIN(EXP_C, B, 1);
-// the macros for the manipulation by EXP_D
-HAL_ASSIGN_PIN(EXP_D, B, 0);
-// the macros for the manipulation by EXP_E
-HAL_ASSIGN_PIN(EXP_E, D, 7);
-// the macros for the manipulation by EXP_F
-HAL_ASSIGN_PIN(EXP_F, D, 6);
-// the macros for the manipulation by EXP_G
-HAL_ASSIGN_PIN(EXP_G, D, 5);
-// the macros for the manipulation by EXP_H
-HAL_ASSIGN_PIN(EXP_H, D, 4);
-// the macros for the manipulation by EXP_I
-HAL_ASSIGN_PIN(EXP_I, D, 2);
-// the macros for the manipulation by EXP_J
-HAL_ASSIGN_PIN(EXP_J,  D, 1);
-// the macros for the manipulation by EXP_K
-HAL_ASSIGN_PIN(EXP_K, D, 0);
-// the macros for the manipulation by EXP_L
-HAL_ASSIGN_PIN(EXP_L, C, 5);
-// the macros for the manipulation by EXP_M
-HAL_ASSIGN_PIN(EXP_M, C, 4);
-// the macros for the manipulation by EXP_N
-HAL_ASSIGN_PIN(EXP_N, C, 2);
-// the macros for the manipulation by EXP_O
-HAL_ASSIGN_PIN(EXP_O, C, 5);
-// the macros for the manipulation by EXP_P
-HAL_ASSIGN_PIN(EXP_P, C, 1);
-// the macros for the manipulation by EXP_Q
-HAL_ASSIGN_PIN(EXP_Q, C, 7);
-// the macros for the manipulation by EXP_R
-HAL_ASSIGN_PIN(EXP_R, C, 7);
+#define GPIO_D1			&PORTD,&PIND,&DDRD,PD1
+#define PORT_GPIO_D1	PORTD
+#define PIN_GPIO_D1		PIND
+#define DDR_GPIO_D1		DDRD
+#define NR_GPIO_D1		PD1
 
-// the macros for the manipulation by PORTB BIT0
-HAL_ASSIGN_PIN(GPIO_B0, B, 0);
-// the macros for the manipulation by PORTB BIT1
-HAL_ASSIGN_PIN(GPIO_B1, B, 1);
-// the macros for the manipulation by PORTB BIT2
-HAL_ASSIGN_PIN(GPIO_B2, B, 2);
-// the macros for the manipulation by PORTB BIT3	bit 3-5 are spi communication with mcp2515
-//HAL_ASSIGN_PIN(GPIO_B3, B, 3);
-// the macros for the manipulation by PORTB BIT4
-//HAL_ASSIGN_PIN(GPIO_B4, B, 4);
-// the macros for the manipulation by PORTB BIT5
-//HAL_ASSIGN_PIN(GPIO_B5, B, 5);
-// the macros for the manipulation by PORTB BIT6	bit 6 is external clock from mcp2515
-//HAL_ASSIGN_PIN(GPIO_B6, B, 6);
-// the macros for the manipulation by PORTB BIT7
-HAL_ASSIGN_PIN(GPIO_B7, B, 7);
+#define GPIO_D2			&PORTD,&PIND,&DDRD,PD2
+#define PORT_GPIO_D2	PORTD
+#define PIN_GPIO_D2		PIND
+#define DDR_GPIO_D2		DDRD
+#define NR_GPIO_D2		PD2
 
-// the macros for the manipulation by PORTD BIT0
-HAL_ASSIGN_PIN(GPIO_D0, D, 0);
-// the macros for the manipulation by PORTD BIT1
-HAL_ASSIGN_PIN(GPIO_D1, D, 1);
-// the macros for the manipulation by PORTD BIT2
-HAL_ASSIGN_PIN(GPIO_D2, D, 2);
-// the macros for the manipulation by PORTD BIT3	bit 3 is interrup from mcp2515
-//HAL_ASSIGN_PIN(GPIO_D3, D, 3);
-// the macros for the manipulation by PORTD BIT4
-HAL_ASSIGN_PIN(GPIO_D4, D, 4);
-// the macros for the manipulation by PORTD BIT5
-HAL_ASSIGN_PIN(GPIO_D5, D, 5);
-// the macros for the manipulation by PORTD BIT6
-HAL_ASSIGN_PIN(GPIO_D6, D, 6);
-// the macros for the manipulation by PORTD BIT7
-HAL_ASSIGN_PIN(GPIO_D7, D, 7);
+#define GPIO_D4			&PORTD,&PIND,&DDRD,PD4
+#define PORT_GPIO_D4	PORTD
+#define PIN_GPIO_D4		PIND
+#define DDR_GPIO_D4		DDRD
+#define NR_GPIO_D4		PD4
 
-// the macros for the manipulation by PORTC BIT0
-HAL_ASSIGN_PIN(GPIO_C0, C, 0);
-// the macros for the manipulation by PORTC BIT1
-HAL_ASSIGN_PIN(GPIO_C1, C, 1);
-// the macros for the manipulation by PORTC BIT2
-HAL_ASSIGN_PIN(GPIO_C2, C, 2);
-// the macros for the manipulation by PORTC BIT3	bit 3 is cs on corecard but could be used on nodes
-HAL_ASSIGN_PIN(GPIO_C3, C, 3);
-// the macros for the manipulation by PORTC BIT4
-HAL_ASSIGN_PIN(GPIO_C4, C, 4);
-// the macros for the manipulation by PORTC BIT5
-HAL_ASSIGN_PIN(GPIO_C5, C, 5);
-// the macros for the manipulation by PORTC BIT6	bit 6 is reset
-//HAL_ASSIGN_PIN(GPIO_C6, C, 6);
+#define GPIO_D5			&PORTD,&PIND,&DDRD,PD5
+#define PORT_GPIO_D5	PORTD
+#define PIN_GPIO_D5		PIND
+#define DDR_GPIO_D5		DDRD
+#define NR_GPIO_D5		PD5
 
+#define GPIO_D6			&PORTD,&PIND,&DDRD,PD6
+#define PORT_GPIO_D6	PORTD
+#define PIN_GPIO_D6		PIND
+#define DDR_GPIO_D6		DDRD
+#define NR_GPIO_D6		PD6
+
+#define GPIO_D7			&PORTD,&PIND,&DDRD,PD7
+#define PORT_GPIO_D7	PORTD
+#define PIN_GPIO_D7		PIND
+#define DDR_GPIO_D7		DDRD
+#define NR_GPIO_D7		PD7
+
+
+#define GPIO_B0			&PORTB,&PINB,&DDRB,PB0
+#define PORT_GPIO_B0	PORTB
+#define PIN_GPIO_B0		PINB
+#define DDR_GPIO_B0		DDRB
+#define NR_GPIO_B0		PB0
+
+#define GPIO_B1			&PORTB,&PINB,&DDRB,PB1
+#define PORT_GPIO_B1	PORTB
+#define PIN_GPIO_B1		PINB
+#define DDR_GPIO_B1		DDRB
+#define NR_GPIO_B1		PB1
+
+#define GPIO_B2			&PORTB,&PINB,&DDRB,PB2
+#define PORT_GPIO_B2	PORTB
+#define PIN_GPIO_B2		PINB
+#define DDR_GPIO_B2		DDRB
+#define NR_GPIO_B2		PB2
+
+#define GPIO_B7			&PORTB,&PINB,&DDRB,PB7
+#define PORT_GPIO_B7	PORTB
+#define PIN_GPIO_B7		PINB
+#define DDR_GPIO_B7		DDRB
+#define NR_GPIO_B7		PB7
+
+
+#define GPIO_C0			&PORTC,&PINC,&DDRC,PC0
+#define PORT_GPIO_C0	PORTC
+#define PIN_GPIO_C0		PINC
+#define DDR_GPIO_C0		DDRC
+#define NR_GPIO_C0		PC0
+
+#define GPIO_C1			&PORTC,&PINC,&DDRC,PC1
+#define PORT_GPIO_C1	PORTC
+#define PIN_GPIO_C1		PINC
+#define DDR_GPIO_C1		DDRC
+#define NR_GPIO_C1		PC1
+
+#define GPIO_C2			&PORTC,&PINC,&DDRC,PC2
+#define PORT_GPIO_C2	PORTC
+#define PIN_GPIO_C2		PINC
+#define DDR_GPIO_C2		DDRC
+#define NR_GPIO_C2		PC2
+
+#define GPIO_C3			&PORTC,&PINC,&DDRC,PC3
+#define PORT_GPIO_C3	PORTC
+#define PIN_GPIO_C3		PINC
+#define DDR_GPIO_C3		DDRC
+#define NR_GPIO_C3		PC3
+
+#define GPIO_C4			&PORTC,&PINC,&DDRC,PC4
+#define PORT_GPIO_C4	PORTC
+#define PIN_GPIO_C4		PINC
+#define DDR_GPIO_C4		DDRC
+#define NR_GPIO_C4		PC4
+
+#define GPIO_C5			&PORTC,&PINC,&DDRC,PC5
+#define PORT_GPIO_C5	PORTC
+#define PIN_GPIO_C5		PINC
+#define DDR_GPIO_C5		DDRC
+#define NR_GPIO_C5		PC5
 
 // for corecard:
 #define EXP_A	&PORTB,&PINB,&DDRB,PB7
@@ -214,15 +234,6 @@ HAL_ASSIGN_PIN(GPIO_C5, C, 5);
 #define PIN_EXP_P	PINC
 #define DDR_EXP_P	DDRC
 #define NR_EXP_P	PC0
-
-static __inline__ void gpio_set_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port |= (1 << nr);}
-static __inline__ void gpio_clr_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port &= ~(1 << nr);}
-static __inline__ void gpio_set_out(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*ddr |= (1 << nr);}
-static __inline__ void gpio_set_in(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*ddr &= ~(1 << nr);}
-static __inline__ void gpio_set_pullup(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port |= (1 << nr);}
-static __inline__ void gpio_clr_pullup(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port &= ~(1 << nr);}
-static __inline__ void gpio_toggle_pin(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {*port ^= (1 << nr);}
-static __inline__ uint8_t gpio_get_state(volatile uint8_t* port, volatile uint8_t* pin, volatile uint8_t* ddr,uint8_t nr) {return (*pin & (1 << nr)) != 0;}
 
 #endif
 //eof gpio.h
