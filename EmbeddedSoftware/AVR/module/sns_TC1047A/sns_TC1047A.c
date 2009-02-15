189c39eeb9cc346d8e36a14b6460227071ff6fd2
Linus Lundin <linus.lundin@gmail.com>
2009-02-15 12:28:53 +0000

    Fixes
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1099 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/sns_TC1047A/sns_TC1047A.c
+++ EmbeddedSoftware/AVR/module/sns_TC1047A/sns_TC1047A.c
@@ -14,7 +14,7 @@ void sns_TC1047A_Process(void)
 {
 	///TODO: Stuff that needs doing is done here
 	if (Timer_Expired(sns_TC1047A_TIMER)) {
-		uint16_t temperture = ADC_Get(TC1047AAD);
+		uint16_t temperature = ADC_Get(TC1047AAD);
 		/* Use 5 volt as reference */
 		temperature = temperature * 5;
 		temperature = ((uint16_t)(((uint32_t)temperature * 100) / 1024) - 50);
@@ -23,7 +23,7 @@ void sns_TC1047A_Process(void)
 		StdCan_Set_direction(txMsg.Header, DIRECTIONFLAG_FROM_OWNER);
 		txMsg.Header.ModuleType = CAN_MODULE_TYPE_SNS_TC1047A;
 		txMsg.Header.ModuleId = sns_TC1047A_ID;
-		txMsg.Header.Command = CAN_MODULE_CMD_PHYSICAL_TEMPERATURE;
+		txMsg.Header.Command = CAN_MODULE_CMD_PHYSICAL_TEMPERATURE_CELSIUS;
 		txMsg.Length = 3;
 		txMsg.Data[0] = 0;
 		txMsg.Data[1] = (temperture>>(2))&0xff;
