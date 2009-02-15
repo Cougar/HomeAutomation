abc68f80583a0288189130c39e934cb1ecf6e967
Linus Lundin <linus.lundin@gmail.com>
2009-02-15 12:48:05 +0000

    Update
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1102 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/sns_TC1047A/sns_TC1047A.c
+++ EmbeddedSoftware/AVR/module/sns_TC1047A/sns_TC1047A.c
@@ -27,7 +27,7 @@ void sns_TC1047A_Process(void)
 		txMsg.Length = 3;
 		txMsg.Data[0] = 0;
 		txMsg.Data[1] = (temperature>>(2))&0xff;
-		txMsg.Data[2] = (temperature>>(10))&0xff;
+		txMsg.Data[2] = (temperature<<(2))&0xff;
 
 		StdCan_Put(&txMsg);
 	}
