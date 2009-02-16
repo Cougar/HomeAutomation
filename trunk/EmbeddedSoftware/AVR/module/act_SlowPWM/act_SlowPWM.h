a88e34a24e2437022793b85bc8e55a756f6ed559
Linus Lundin <linus.lundin@gmail.com>
2009-02-16 17:39:07 +0000

    Added act_SlowPWM
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1113 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/EmbeddedSoftware/AVR/module/act_SlowPWM/act_SlowPWM.h
@@ -0,0 +1,34 @@
+#ifndef ACT_SLOWPWM
+#define ACT_SLOWPWM
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+/* system files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+/* lib files */
+#include <config.h>
+#include <bios.h>
+#include <drivers/can/stdcan.h>
+#include <drivers/timer/timer.h>
+#include <drivers/mcu/gpio.h>
+
+void act_SlowPWM_Init(void);
+void act_SlowPWM_Process(void);
+void act_SlowPWM_HandleMessage(StdCan_Msg_t *rxMsg);
+void act_SlowPWM_List(uint8_t ModuleSequenceNumber);
+
+#define PWMSENSORID	0
+
+#ifdef act_SlowPWM_USEEEPROM
+	struct act_SlowPWM_Data{
+		///TODO: Define EEPROM variables needed by the module
+		uint16_t PwmPeriod;
+		uint16_t defaultPwmValue;
+		uint8_t defaultStates;
+	};	
+#endif
+
+#endif // ACT_SLOWPWM
