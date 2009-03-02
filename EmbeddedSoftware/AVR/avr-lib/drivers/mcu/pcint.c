83411cce82d21a33ee9a08e7d5e85c84e2a43d84
Anders Runeson <runeson@gmail.com>
2009-03-02 18:41:57 +0000

    drivers/pcint: optimisation, controversial
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1134 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/avr-lib/drivers/mcu/pcint.c
+++ EmbeddedSoftware/AVR/avr-lib/drivers/mcu/pcint.c
@@ -21,9 +21,11 @@
 
 struct {
 	uint8_t pcintBit;
-	uint8_t lastStatus;
+	//uint8_t lastStatus;
 	pcintCallback_t callback;
 } pcints[PCINT_NUM_PCINTS];
+
+uint32_t pinLastStatus;
 
 /*-----------------------------------------------------------------------------
  * Interrupt Service Routines
@@ -37,27 +39,35 @@ Returns:  -
 **************************************************************************/
 ISR(PCINT0_vect) {
 	uint8_t p;
+
+	uint32_t pin = ((uint32_t)PIND<<16)|((uint32_t)PINC<<8)|(PINB);
 	/* Check which pin that have had a change and call the callback.*/
 	//printf("I\n");
 	for (p = 0; p < PCINT_NUM_PCINTS; p++)
 	{
-		if (pcints[p].pcintBit < 8) {
-			if (pcints[p].lastStatus != ((PINB & (1 << pcints[p].pcintBit)) != 0)) {
+		if (pinLastStatus != ((pin & (1 << pcints[p].pcintBit)) != 0)) {
 				pcints[p].callback(p);
+		}
+		
+		/*if (pcints[p].pcintBit < 8) {
+			if (pcints[p].lastStatus != ((PINB & (1 << pcints[p].pcintBit)) != 0)) {
 				pcints[p].lastStatus = ((PINB & (1 << pcints[p].pcintBit)) != 0);
+				pcints[p].callback(p);
 			}
 		} else if (pcints[p].pcintBit < 16) {
 			if (pcints[p].lastStatus != ((PINC & (1 << pcints[p].pcintBit%8)) != 0)) {
-				pcints[p].callback(p);
 				pcints[p].lastStatus = ((PINC & (1 << pcints[p].pcintBit%8)) != 0);
+				pcints[p].callback(p);
 			}
 		} else if (pcints[p].pcintBit < 24) {
 			if (pcints[p].lastStatus != ((PIND & (1 << pcints[p].pcintBit%8)) != 0)) {
-				pcints[p].callback(p);
 				pcints[p].lastStatus = ((PIND & (1 << pcints[p].pcintBit%8)) != 0);
+				pcints[p].callback(p);
 			}
-		}
+		}*/
 	}
+	
+	pinLastStatus = pin;
 } /* ISR(PCINT0_vect) */
 
 
@@ -95,25 +105,27 @@ void Pcint_SetCallback(uint8_t pcint_id, uint8_t pcintBit, pcintCallback_t callb
 		uint8_t sreg = SREG;
 		cli();
 
-		PCIFR=(1<<PCIF0);	/* clear any pending interrupt before enabling interrupts */
-		PCICR|=(1<<PCIE0);	/* enable interrupt for PCINT0 */
+		PCIFR=(1<<PCIF0|1<<PCIF1|1<<PCIF2);	/* clear any pending interrupt before enabling interrupts */
+		PCICR|=(1<<PCIE0|1<<PCIE1|1<<PCIE2);	/* enable interrupt for PCINT0-2 */
+#if 0
 		PCIFR=(1<<PCIF1);	/* clear any pending interrupt before enabling interrupts */
 		PCICR|=(1<<PCIE1);	/* enable interrupt for PCINT1 */
 		PCIFR=(1<<PCIF2);	/* clear any pending interrupt before enabling interrupts */
 		PCICR|=(1<<PCIE2);	/* enable interrupt for PCINT2 */
+#endif
 		pcints[pcint_id].pcintBit = pcintBit;
-		pcints[pcint_id].lastStatus = 0;
+		//pcints[pcint_id].lastStatus = 0;
 		if (callback != 0) {
 			if (pcints[pcint_id].pcintBit < 8) {
-				pcints[pcint_id].lastStatus = ((PINB & (1 << pcints[pcint_id].pcintBit%8)) != 0);
+				//pcints[pcint_id].lastStatus = ((PINB & (1 << pcints[pcint_id].pcintBit%8)) != 0);
 				PCMSK0 |= (1<<pcints[pcint_id].pcintBit%8);
 //printf("PCMSK %u\n", PCMSK0);
 			} else if (pcints[pcint_id].pcintBit < 16) {
-				pcints[pcint_id].lastStatus = ((PINC & (1 << pcints[pcint_id].pcintBit%8)) != 0);
+				//pcints[pcint_id].lastStatus = ((PINC & (1 << pcints[pcint_id].pcintBit%8)) != 0);
 				PCMSK1 |= (1<<pcints[pcint_id].pcintBit%8);
 //printf("PCMSK1 %u\n", PCMSK1);
 			} else if (pcints[pcint_id].pcintBit < 24) {
-				pcints[pcint_id].lastStatus = ((PIND & (1 << pcints[pcint_id].pcintBit%8)) != 0);
+				//pcints[pcint_id].lastStatus = ((PIND & (1 << pcints[pcint_id].pcintBit%8)) != 0);
 				PCMSK2 |= (1<<pcints[pcint_id].pcintBit%8);
 //printf("PCMSK2 %u\n", PCMSK2);
 			}
@@ -126,6 +138,9 @@ void Pcint_SetCallback(uint8_t pcint_id, uint8_t pcintBit, pcintCallback_t callb
 				PCMSK2 &= ~(1<<(pcints[pcint_id].pcintBit%8));
 			}
 		}
+
+		uint32_t pinLastStatus = ((uint32_t)PIND<<16)|((uint32_t)PINC<<8)|(PINB);
+
 		pcints[pcint_id].callback = callback;
 		SREG = sreg;
 	}
