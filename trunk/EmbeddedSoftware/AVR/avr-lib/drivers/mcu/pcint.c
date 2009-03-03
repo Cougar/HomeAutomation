9478e46454a40bdfcad2f98ae44d44f7592796af
Linus Lundin <linus.lundin@gmail.com>
2009-03-03 15:56:11 +0000

    Small changes to sns_power and pcint
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1138 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/avr-lib/drivers/mcu/pcint.c
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/mcu/pcint.c
@@ -50,18 +50,18 @@ ISR(PCINT0_vect) {
 	for (p = 0; p < PCINT_NUM_PCINTS; p++)
 	{
 		if (pcints[p].pcintBit < 8) {
-			if (pcints[p].lastStatus != ((rPinB & (1 << pcints[p].pcintBit)) != 0)) {
-				pcints[p].lastStatus = ((rPinB & (1 << pcints[p].pcintBit)) != 0);
+			if (pcints[p].lastStatus != (rPinB & (1 << pcints[p].pcintBit))) {
+				pcints[p].lastStatus = (rPinB & (1 << pcints[p].pcintBit));
 				pcints[p].callback(p, pcints[p].lastStatus);
 			}
 		} else if (pcints[p].pcintBit < 16) {
-			if (pcints[p].lastStatus != ((rPinC & (1 << pcints[p].pcintBit%8)) != 0)) {
-				pcints[p].lastStatus = ((rPinC & (1 << pcints[p].pcintBit%8)) != 0);
+			if (pcints[p].lastStatus != (rPinC & (1 << pcints[p].pcintBit%8))) {
+				pcints[p].lastStatus = (rPinC & (1 << pcints[p].pcintBit%8));
 				pcints[p].callback(p, pcints[p].lastStatus);
 			}
 		} else if (pcints[p].pcintBit < 24) {
-			if (pcints[p].lastStatus != ((rPinD & (1 << pcints[p].pcintBit%8)) != 0)) {
-				pcints[p].lastStatus = ((rPinD & (1 << pcints[p].pcintBit%8)) != 0);
+			if (pcints[p].lastStatus != (rPinD & (1 << pcints[p].pcintBit%8))) {
+				pcints[p].lastStatus = (rPinD & (1 << pcints[p].pcintBit%8));
 				pcints[p].callback(p, pcints[p].lastStatus);
 			}
 		}
@@ -116,15 +116,15 @@ void Pcint_SetCallback(uint8_t pcint_id, uint8_t pcintBit, pcintCallback_t callb
 		pcints[pcint_id].lastStatus = 0;
 		if (callback != 0) {
 			if (pcints[pcint_id].pcintBit < 8) {
-				pcints[pcint_id].lastStatus = ((PINB & (1 << pcints[pcint_id].pcintBit%8)) != 0);
+				pcints[pcint_id].lastStatus = (PINB & (1 << pcints[pcint_id].pcintBit%8));
 				PCMSK0 |= (1<<pcints[pcint_id].pcintBit%8);
 //printf("PCMSK %u\n", PCMSK0);
 			} else if (pcints[pcint_id].pcintBit < 16) {
-				pcints[pcint_id].lastStatus = ((PINC & (1 << pcints[pcint_id].pcintBit%8)) != 0);
+				pcints[pcint_id].lastStatus = (PINC & (1 << pcints[pcint_id].pcintBit%8));
 				PCMSK1 |= (1<<pcints[pcint_id].pcintBit%8);
 //printf("PCMSK1 %u\n", PCMSK1);
 			} else if (pcints[pcint_id].pcintBit < 24) {
-				pcints[pcint_id].lastStatus = ((PIND & (1 << pcints[pcint_id].pcintBit%8)) != 0);
+				pcints[pcint_id].lastStatus = (PIND & (1 << pcints[pcint_id].pcintBit%8));
 				PCMSK2 |= (1<<pcints[pcint_id].pcintBit%8);
 //printf("PCMSK2 %u\n", PCMSK2);
 			}
