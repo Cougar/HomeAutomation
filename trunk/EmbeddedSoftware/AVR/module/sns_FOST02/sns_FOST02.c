a324009ea272bf637958bb7f60981ce640aefc02
Linus Lundin <linus.lundin@gmail.com>
2009-03-20 21:48:15 +0000

    New test
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1143 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/module/sns_FOST02/sns_FOST02.c
+++ trunk/EmbeddedSoftware/AVR/module/sns_FOST02/sns_FOST02.c
@@ -42,8 +42,8 @@ void GetTemperature(void)
 		txMsg.Data[4]= byte2;
 		
 		TmpV = TmpV << 4;
-		TmpV = TmpV*0.04;
-		TmpV= TmpV <<4;
+		TmpV = TmpV/100;
+		TmpV= TmpV <<6;
 		TmpV = TmpV-0x2800;
 		txMsg.Data[2]= (uint8_t) (TmpV & 0x00FF);
 		txMsg.Data[1]= (uint8_t) ((TmpV >> 8) & 0x00FF);
