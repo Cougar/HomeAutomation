9d88964231012f244af53e51f962cb8a2bc0920a
Anders Runeson <runeson@gmail.com>
2009-04-13 16:11:33 +0000

    added exchangecalendar.js and conferenceroomdisplay.js. the first should be ready, the latter is not started. startup.js now starts exchangecalendar.js
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1170 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/Atom/Services/System/Startup.js
+++ trunk/PcSoftware/Atom/Services/System/Startup.js
@@ -16,6 +16,7 @@ loadScript("System/TextTable.js");
 loadScript("System/IntelHex.js");
 
 loadScript("Other/OnlinePhonebook.js");
+loadScript("Other/ExchangeCalendar.js");
 
 /* This function is called when the virtual machine is up and running.
    Put things that needs to start here */
