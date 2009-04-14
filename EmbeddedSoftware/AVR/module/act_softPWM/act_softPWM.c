d3c8c65737a9303cfe08ca22536e390c8c0dcb6f
Linus Lundin <linus.lundin@gmail.com>
2009-04-14 20:38:46 +0000

    updated softPWM, now supports 8 channels
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1177 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/act_softPWM/act_softPWM.c
+++ EmbeddedSoftware/AVR/module/act_softPWM/act_softPWM.c
@@ -1,5 +1,8 @@
 
 #include "act_softPWM.h"
+uint16_t currentTimer = 0;
+uint16_t maxTimer = 10;
+uint8_t	resolution = 1;
 
 #ifdef act_softPWM_USEEEPROM
 #include "act_softPWM_eeprom.h"
@@ -12,6 +15,30 @@ struct eeprom_act_softPWM EEMEM eeprom_act_softPWM =
 	},
 	0	// crc, must be a correct value, but this will also be handled by the EEPROM module or make scripts
 }; 
+#endif
+#ifdef PIN_0
+	uint16_t PIN_0_timer=0;
+#endif
+#ifdef PIN_1
+	uint16_t PIN_1_timer=0;
+#endif
+#ifdef PIN_2
+	uint16_t PIN_2_timer=0;
+#endif
+#ifdef PIN_3
+	uint16_t PIN_3_timer=0;
+#endif
+#ifdef PIN_4
+	uint16_t PIN_4_timer=0;
+#endif
+#ifdef PIN_5
+	uint16_t PIN_5_timer=0;
+#endif
+#ifdef PIN_6
+	uint16_t PIN_6_timer=0;
+#endif
+#ifdef PIN_7
+	uint16_t PIN_7_timer=0;
 #endif
 
 void act_softPWM_Init(void)
@@ -30,22 +57,44 @@ void act_softPWM_Init(void)
 	}
 #endif  
 	///TODO: Initialize hardware etc here
-	gpio_set_out(PIN_RED);
-	gpio_set_out(PIN_GREEN);
-	gpio_set_out(PIN_BLUE);
-	gpio_clr_pin(PIN_RED);
-	gpio_clr_pin(PIN_GREEN);
-	gpio_clr_pin(PIN_BLUE);
+#ifdef PIN_0
+	gpio_set_out(PIN_0);
+	gpio_clr_pin(PIN_0);
+#endif
+#ifdef PIN_1
+	gpio_set_out(PIN_1);
+	gpio_clr_pin(PIN_1);
+#endif
+#ifdef PIN_2
+	gpio_set_out(PIN_2);
+	gpio_clr_pin(PIN_2);
+#endif
+#ifdef PIN_3
+	gpio_set_out(PIN_3);
+	gpio_clr_pin(PIN_3);
+#endif
+#ifdef PIN_4
+	gpio_set_out(PIN_4);
+	gpio_clr_pin(PIN_4);
+#endif
+#ifdef PIN_5
+	gpio_set_out(PIN_5);
+	gpio_clr_pin(PIN_5);
+#endif
+#ifdef PIN_6
+	gpio_set_out(PIN_6);
+	gpio_clr_pin(PIN_6);
+#endif
+#ifdef PIN_7
+	gpio_set_out(PIN_7);
+	gpio_clr_pin(PIN_7);
+#endif
 	
-	Timer_SetTimeout(act_softPWM_TIMER, 1, TimerTypeFreeRunning, NULL);
+	Timer_SetTimeout(act_softPWM_TIMER, resolution, TimerTypeFreeRunning, NULL);
 	// to use PCINt lib, call this function: (the callback function look as a timer callback function)
 	// Pcint_SetCallbackPin(act_softPWM_PCINT, EXP_C , &act_softPWM_pcint_callback);
 
 }
-uint8_t redTimer = 0;
-uint8_t greenTimer = 0;
-uint8_t blueTimer = 0;
-uint8_t currentTimer = 0;
 
 void act_softPWM_Process(void)
 {
@@ -53,20 +102,71 @@ void act_softPWM_Process(void)
 		currentTimer++;
 		if (currentTimer == 10) {
 			currentTimer=0;
-			gpio_set_pin(PIN_RED);
-			gpio_set_pin(PIN_GREEN);
-			gpio_set_pin(PIN_BLUE);
+			#ifdef PIN_0 
+				gpio_set_pin(PIN_0);
+			#endif
+			#ifdef PIN_1 
+				gpio_set_pin(PIN_1);
+			#endif
+			#ifdef PIN_2 
+				gpio_set_pin(PIN_2);
+			#endif
+			#ifdef PIN_3 
+				gpio_set_pin(PIN_3);
+			#endif
+			#ifdef PIN_4 
+				gpio_set_pin(PIN_4);
+			#endif
+			#ifdef PIN_5 
+				gpio_set_pin(PIN_5);
+			#endif
+			#ifdef PIN_6 
+				gpio_set_pin(PIN_6);
+			#endif
+			#ifdef PIN_7 
+				gpio_set_pin(PIN_7);
+			#endif
 		}
-		
-		if (currentTimer == redTimer) {
-			gpio_clr_pin(PIN_RED);
+		#ifdef PIN_0
+		if (currentTimer >= PIN_0_timer) {
+			gpio_clr_pin(PIN_0);
 		}
-		if (currentTimer == greenTimer) {
-			gpio_clr_pin(PIN_GREEN);
+		#endif
+		#ifdef PIN_1
+		if (currentTimer >= PIN_1_timer) {
+			gpio_clr_pin(PIN_1);
 		}
-		if (currentTimer == blueTimer) {
-			gpio_clr_pin(PIN_BLUE);
+		#endif
+		#ifdef PIN_2
+		if (currentTimer >= PIN_2_timer) {
+			gpio_clr_pin(PIN_2);
 		}
+		#endif
+		#ifdef PIN_3
+		if (currentTimer >= PIN_3_timer) {
+			gpio_clr_pin(PIN_3);
+		}
+		#endif
+		#ifdef PIN_4
+		if (currentTimer >= PIN_4_timer) {
+			gpio_clr_pin(PIN_4);
+		}
+		#endif
+		#ifdef PIN_5
+		if (currentTimer >= PIN_5_timer) {
+			gpio_clr_pin(PIN_5);
+		}
+		#endif
+		#ifdef PIN_6
+		if (currentTimer >= PIN_6_timer) {
+			gpio_clr_pin(PIN_6);
+		}
+		#endif
+		#ifdef PIN_7
+		if (currentTimer >= PIN_7_timer) {
+			gpio_clr_pin(PIN_7);
+		}
+		#endif
 	}
 }
 
@@ -79,11 +179,62 @@ void act_softPWM_HandleMessage(StdCan_Msg_t *rxMsg)
 	{
 		switch (rxMsg->Header.Command)
 		{
-		case CAN_MODULE_CMD_SOFTPWM_RGB_VALUE:
-			redTimer = rxMsg->Data[0];
-			greenTimer = rxMsg->Data[1];
-			blueTimer = rxMsg->Data[2];
-		break;
+		case CAN_MODULE_CMD_PHYSICAL_PWM:
+			if ((((uint16_t)rxMsg->Data[1])<8) + rxMsg->Data[2] > maxTimer)
+			{
+				rxMsg->Data[2] = maxTimer & 0x00ff;
+				rxMsg->Data[1] = (maxTimer>>8) & 0x00ff;
+			}
+			switch (rxMsg->Data[0])
+			{
+			#ifdef PIN_0
+			case 0:
+				PIN_0_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			#ifdef PIN_1
+			case 1:
+				PIN_1_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			#ifdef PIN_2
+			case 2:
+				PIN_2_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			#ifdef PIN_3
+			case 3:
+				PIN_3_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			#ifdef PIN_4
+			case 4:
+				PIN_4_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			#ifdef PIN_5
+			case 5:
+				PIN_5_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			#ifdef PIN_6
+			case 6:
+				PIN_6_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			#ifdef PIN_7
+			case 7:
+				PIN_7_timer = (((uint16_t)rxMsg->Data[1])<<8) + rxMsg->Data[2];
+				break;
+			#endif
+			}
+			break;
+		case CAN_MODULE_CMD_SOFTPWM_CONFIG:
+			maxTimer = (((uint16_t)rxMsg->Data[0])<<8) + rxMsg->Data[1];
+			resolution = rxMsg->Data[2];
+			Timer_SetTimeout(act_softPWM_TIMER, resolution, TimerTypeFreeRunning, NULL);
+			break;
+
 		}
 	}
 }
