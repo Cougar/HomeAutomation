dee62f359757801c1e487c6789740a1aba67d9f0
Anders Runeson <runeson@gmail.com>
2009-07-17 15:33:28 +0000

    modules: added start of touch module
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1274 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ EmbeddedSoftware/AVR/module/sns_Touch/sns_Touch.h
@@ -0,0 +1,36 @@
+#ifndef SNS_TOUCH
+#define SNS_TOUCH
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+/* system files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+/* lib files */
+#include <config.h>
+#include <bios.h>
+#include <drivers/can/stdcan.h>
+#include <drivers/timer/timer.h>
+#include <drivers/mcu/gpio.h>
+#include <drivers/adc/adc.h>
+
+//to use PCINT lib. uncomment the line below
+//#include <drivers/mcu/pcint.h>
+
+void sns_Touch_Init(void);
+void sns_Touch_Process(void);
+void sns_Touch_HandleMessage(StdCan_Msg_t *rxMsg);
+void sns_Touch_List(uint8_t ModuleSequenceNumber);
+
+
+#ifdef sns_Touch_USEEEPROM
+	struct sns_Touch_Data{
+		///TODO: Define EEPROM variables needed by the module
+		uint8_t x;
+		uint16_t y;
+	};	
+#endif
+
+#endif // SNS_TOUCH
