6d86170dfed3052912af5183c30359e243e2f658
Anders Runeson <runeson@gmail.com>
2009-07-19 15:45:44 +0000

    modules/touch: missed to commit a couple of updated files
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1297 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/sns_Touch/sns_Touch.h
+++ EmbeddedSoftware/AVR/module/sns_Touch/sns_Touch.h
@@ -15,6 +15,7 @@
 #include <drivers/timer/timer.h>
 #include <drivers/mcu/gpio.h>
 #include <drivers/adc/adc.h>
+#include <drivers/touch/gesture/gesture.h>
 
 //to use PCINT lib. uncomment the line below
 //#include <drivers/mcu/pcint.h>
@@ -23,41 +24,6 @@ void sns_Touch_Init(void);
 void sns_Touch_Process(void);
 void sns_Touch_HandleMessage(StdCan_Msg_t *rxMsg);
 void sns_Touch_List(uint8_t ModuleSequenceNumber);
-
-typedef struct
-{
-	uint8_t x;
-	uint8_t y;
-} point;
-
-typedef struct
-{
-	uint8_t f1;
-	uint8_t f2;
-	uint8_t f3;
-	uint8_t f4;
-	uint8_t f5;
-	uint8_t f6;
-	uint8_t f7;
-	uint8_t f8;
-	uint8_t f9;
-} gesture;
-
-static __inline__ uint8_t min(uint8_t val1, uint8_t val2) {
-  if (val1 > val2)
-  {
-  	return val2;
-  }
-  return val1;
-}
-
-static __inline__ uint8_t max(uint8_t val1, uint8_t val2) {
-  if (val1 > val2)
-  {
-  	return val1;
-  }
-  return val2;
-}
 
 #ifdef sns_Touch_USEEEPROM
 	struct sns_Touch_Data{
