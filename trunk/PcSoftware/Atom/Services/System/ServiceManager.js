f9e613e80a100215ccce225a280bc16e842d64b6
Mattias Runge <mattias@runge.se>
2009-07-19 18:08:27 +0000

    Added the ability to register a callback in the ServiceManager to get events when a service is created Usage: ServiceManager.addCallback(function(service) { alert('service started: ' + service.getName() + ' of type ' + service.getType()); });
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1299 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/Atom/Services/System/ServiceManager.js
+++ trunk/PcSoftware/Atom/Services/System/ServiceManager.js
@@ -2,6 +2,12 @@
 function ServiceManager() {}
 
 ServiceManager.Services = new Array();
+ServiceManager.Callbacks = new Array();
+
+ServiceManager.addCallback = function(callback)
+{
+	ServiceManager.Callbacks[ServiceManager.Callbacks.length] = callback;
+}
 
 ServiceManager.getService = function(type, name, id)
 {
@@ -19,6 +25,11 @@ ServiceManager.getService = function(type, name, id)
 		}
 		
 		ServiceManager.Services[fullId] = eval("(new " + name + "('" + type + "', '" + name + "', " + id + "))");///FIXME: Can we do this without eval??
+		
+		for (var n in ServiceManager.Callbacks)
+		{
+			ServiceManager.Callbacks[n](ServiceManager.Services[fullId]);
+		}
 	}
 	
 	return ServiceManager.Services[fullId];
