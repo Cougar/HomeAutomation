b16cec9cbb84b6a6517218d56b3ea6d554af5926
Linus Lundin <linus.lundin@gmail.com>
2010-03-05 13:07:26 +0000

    Fixed error in EEPROM example in template.c for the modules
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1446 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/template/template.c
+++ EmbeddedSoftware/AVR/module/template/template.c
@@ -3,15 +3,15 @@
 
 #ifdef <template>_USEEEPROM
 #include "<template>_eeprom.h"
-struct eeprom_<template> EEMEM eeprom_<template> = 
+struct eeprom_<template> EEMEM eeprom_<template> =
 {
 	{
-		///TODO: Define initialization values on the EEPROM variables here, this will generate a *.eep file that can be used to store this values to the node, can in future be done with a EEPROM module and the make-scrips. Write the values in the exact same order as the struct is defined in the *.h file. 
+		///TODO: Define initialization values on the EEPROM variables here, this will generate a *.eep file that can be used to store this values to the node, can in future be done with a EEPROM module and the make-scrips. Write the values in the exact same order as the struct is defined in the *.h file.
 		0xAB,	// x
 		0x1234	// y
 	},
 	0	// crc, must be a correct value, but this will also be handled by the EEPROM module or make scripts
-}; 
+};
 #endif
 
 void <template>_Init(void)
@@ -21,14 +21,14 @@ void <template>_Init(void)
 	{
 	  ///TODO: Use stored data to set initial values for the module
 	  blablaX = eeprom_read_byte(EEDATA.x);
-	  blablaY = eeprom_read_word(EEDATA.y);
+	  blablaY = eeprom_read_word(EEDATA16.y);
 	} else
 	{	//The CRC of the EEPROM is not correct, store default values and update CRC
 	  eeprom_write_byte_crc(EEDATA.x, 0xAB, WITHOUT_CRC);
-	  eeprom_write_word_crc(EEDATA.y, 0x1234, WITHOUT_CRC);
+	  eeprom_write_word_crc(EEDATA16.y, 0x1234, WITHOUT_CRC);
 	  EEDATA_UPDATE_CRC;
 	}
-#endif  
+#endif
 	///TODO: Initialize hardware etc here
 
 	// to use PCINt lib, call this function: (the callback function look as a timer callback function)
@@ -60,7 +60,7 @@ void <template>_HandleMessage(StdCan_Msg_t *rxMsg)
 void <template>_List(uint8_t ModuleSequenceNumber)
 {
 	StdCan_Msg_t txMsg;
-	
+
 	StdCan_Set_class(txMsg.Header, CAN_MODULE_CLASS_xyz); ///TODO: Change this to the actual class type
 	StdCan_Set_direction(txMsg.Header, DIRECTIONFLAG_FROM_OWNER);
 	txMsg.Header.ModuleType = CAN_MODULE_TYPE_xyz; ///TODO: Change this to the actual module type
@@ -72,9 +72,9 @@ void <template>_List(uint8_t ModuleSequenceNumber)
 	txMsg.Data[1] = NODE_HW_ID_BYTE1;
 	txMsg.Data[2] = NODE_HW_ID_BYTE2;
 	txMsg.Data[3] = NODE_HW_ID_BYTE3;
-	
+
 	txMsg.Data[4] = NUMBER_OF_MODULES;
 	txMsg.Data[5] = ModuleSequenceNumber;
-	
+
 	while (StdCan_Put(&txMsg) != StdCan_Ret_OK);
 }
