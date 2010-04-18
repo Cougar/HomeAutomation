2458b13b1ce36b785f26ccc1f70f65e95278a39a
Anders Runeson <runeson@gmail.com>
2010-04-18 14:44:42 +0000

    drivers/ir: added multi transceiver, work in progress, current difference from old driver is use of pcint instead of input capture
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1465 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/ir/transceiver/irtransceiverMulti.h
@@ -0,0 +1,61 @@
+/** 
+ * @defgroup infrared IR Driver Library
+ * @code #include <drivers/ir/transceiver/transceiver.h> @endcode
+ * 
+ * @brief  
+ *
+ *
+ * @author	Andreas Fritiofson, Anders Runeson
+ * @date	2007
+ */
+
+/**@{*/
+
+#ifndef IRTRANSCEIVER_H_
+#define IRTRANSCEIVER_H_
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+#include <stdio.h>
+
+/*-----------------------------------------------------------------------------
+ * Defines
+ *---------------------------------------------------------------------------*/
+#define IR_R_PORT	PORTB
+#define IR_R_PIN	PINB
+#define IR_R_DDR	DDRB
+#define IR_R_BIT	PB0 // Input Capture Pin
+
+#define IR_T_PORT	PORTD
+#define IR_T_PIN	PIND
+#define IR_T_DDR	DDRD
+#define IR_T_BIT	PD6
+
+#define MAX_NR_TIMES		101 								//max ir pulses+pauses, used for dimensioning the buffer 
+
+#ifndef IRTRANS_NUM_IR_REC
+#define IRTRANS_NUM_IR_REC 1
+#endif
+
+#ifndef IR_TRANS_CH0_MASK
+#define IR_TRANS_CH0_MASK PB0
+#endif
+
+/*-----------------------------------------------------------------------------
+ * Public Function Prototypes
+ *---------------------------------------------------------------------------*/
+void IrTransceiver_Init(void);
+void IrTransceiver_Receive_Start(uint16_t *buffer);
+uint8_t IrTransceiver_Receive_Poll(uint8_t *len);
+uint8_t IrTransceiver_Transmit(uint16_t *buffer, uint8_t length, uint8_t modfreq);
+uint8_t IrTransceiver_Transmit_Poll(void);
+
+
+/*-----------------------------------------------------------------------------
+ * Private Function Prototypes
+ *---------------------------------------------------------------------------*/
+
+void IrTransceiver_Store(uint8_t channel);
+
+#endif /*IRTRANSCEIVER_H_*/
