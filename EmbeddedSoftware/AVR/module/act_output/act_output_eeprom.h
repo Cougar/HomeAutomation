fb0524e037f3463976bd899c4281374258badbb7
Anders Runeson <runeson@gmail.com>
2010-09-12 13:22:00 +0000

    module/template: updated template with new eeprom functionality (store/read of dword, update of data done in libc-eeprom when available) module/act_output: new eeprom functionality integrated
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1557 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/act_output/act_output_eeprom.h
+++ EmbeddedSoftware/AVR/module/act_output/act_output_eeprom.h
@@ -9,8 +9,15 @@
 	#include <avr/eeprom.h>
 	#define EEDATA 			(uint8_t*)&eeprom_act_output.Data
 	#define EEDATA16		(uint16_t*)&eeprom_act_output.Data
+	#define EEDATA32		(uint32_t*)&eeprom_act_output.Data
 	#define EEDATA_CALC_CRC		eeprom_crc8((uint8_t*)&eeprom_act_output.Data,sizeof(struct act_output_Data))
+
+#if ((__AVR_LIBC_MAJOR__ == 1  && __AVR_LIBC_MINOR__ == 6 && __AVR_LIBC_REVISION__ >= 7)||(__AVR_LIBC_MAJOR__ == 1  && __AVR_LIBC_MINOR__ > 6)||__AVR_LIBC_MAJOR__ > 1)
+	#define EEDATA_UPDATE_CRC	eeprom_update_byte((uint8_t*)&eeprom_act_output.crc,eeprom_crc8((uint8_t*)&eeprom_act_output.Data,sizeof(struct act_output_Data)))
+#else
 	#define EEDATA_UPDATE_CRC	eeprom_write_byte((uint8_t*)&eeprom_act_output.crc,eeprom_crc8((uint8_t*)&eeprom_act_output.Data,sizeof(struct act_output_Data)))
+#endif
+
 	#define EEDATA_OK		EEDATA_CALC_CRC == EEDATA_STORED_CRC
 	#define EEDATA_STORED_CRC 	eeprom_read_byte((uint8_t*)&eeprom_act_output.crc)
 	
@@ -22,6 +29,30 @@
 	
 	#define WITH_CRC	1
 	#define WITHOUT_CRC	0
+	
+#if ((__AVR_LIBC_MAJOR__ == 1  && __AVR_LIBC_MINOR__ == 6 && __AVR_LIBC_REVISION__ >= 7)||(__AVR_LIBC_MAJOR__ == 1  && __AVR_LIBC_MINOR__ > 6)||__AVR_LIBC_MAJOR__ > 1)
+	static __inline__ void eeprom_write_byte_crc(uint8_t *__p, uint8_t __value, uint8_t crc) {
+	    eeprom_update_byte(__p, __value);
+	    if (crc == WITH_CRC)
+	    {
+	      EEDATA_UPDATE_CRC;
+	    }
+	}
+	static __inline__ void eeprom_write_word_crc(uint16_t *__p, uint16_t __value, uint8_t crc) {
+	    eeprom_update_word(__p, __value);
+	    if (crc == WITH_CRC)
+	    {
+	      EEDATA_UPDATE_CRC;
+	    }
+	}
+	static __inline__ void eeprom_write_dword_crc(uint32_t *__p, uint32_t __value, uint8_t crc) {
+	    eeprom_update_dword(__p, __value);
+	    if (crc == WITH_CRC)
+	    {
+	      EEDATA_UPDATE_CRC;
+	    }
+	}
+#else
 	static __inline__ void eeprom_write_byte_crc(uint8_t *__p, uint8_t __value, uint8_t crc) {
 	  if (__value != eeprom_read_byte(__p))
 	  {
@@ -42,4 +73,16 @@
 	    }
 	  }
 	}
+	static __inline__ void eeprom_write_dword_crc(uint32_t *__p, uint32_t __value, uint8_t crc) {
+	  if (__value != eeprom_read_dword(__p))
+	  {
+	    eeprom_write_dword(__p, __value);
+	    if (crc == WITH_CRC)
+	    {
+	      EEDATA_UPDATE_CRC;
+	    }
+	  }
+	}
+#endif
+
 #endif
