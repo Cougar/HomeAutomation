dd575b3d2345bb269aa8d48d8a5e15f334863e43
Kristian Holmberg <holmberg.k@gmail.com>
2010-10-31 17:24:59 +0000

    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1584 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- EmbeddedSoftware/AVR/module/sns_nmea/sns_nmea.c
+++ EmbeddedSoftware/AVR/module/sns_nmea/sns_nmea.c
@@ -22,9 +22,8 @@ void nmea_clear_comindex(void) {
 }
 
 void nmea_parse_GGA(void) {
-	if (nmea_buffer[nmea_comindex[1]+1] != ",")
+	if (nmea_buffer[nmea_comindex[1]+1] != 0x2C)
 	{
-		uint8_t i;
 		
 		//Get time in UTC
 		NMEA_data.time_h = 10*a2d(nmea_buffer[6]) + a2d(nmea_buffer[7]);
@@ -86,15 +85,80 @@ void nmea_parse_GGA(void) {
 }
 
 void nmea_parse_GSA(void) {
-	//TODO
+   /*
+      Gets fixtype only, 1=No fix, 2=2D fix, 3=3D fix
+   */
+	NMEA_data.fixtype=a2d(nmea_buffer[nmea_comindex[1]+1]);
 }
 
 void nmea_parse_GSV(void) {
-	//TODO
+	//Information about tracked satellites, angle, snr etc. Ignore for now.
 }
 
 void nmea_parse_RMC(void) {
 	//TODO
+	/*	Follows comindex	length	info
+		 0			6	time of fix
+		 1			1	validity, A=ok, V=invalid
+		 2			X	latitude
+		 3			1	lat CD
+		 4			X	longitude
+		 5			1	lon CD
+		 6			5	Speed over ground, knots, NNN.N
+		 7			5	Course made good, true
+		 8			6	Date of fix, DDMMYY
+		 9			5	Magnetic variation, degrees
+		 10			1	Magnetic variation, CD
+	 
+	*/
+	uint8_t varlen=0;
+	uint8_t validity=0;
+	if (nmea_buffer[nmea_comindex[1]+1]==0x41) //0x41='A', valid fix
+	 validity=1;
+	NMEA_data.speed=0;
+	NMEA_data.cmg_h=0;
+	NMEA_data.cmg_l=0;
+
+	if (validity==1) {
+	   NMEA_data.speed=0;
+	   varlen = nmea_comindex[7]-nmea_comindex[6];
+	   switch(varlen-4) {
+	      case 3:
+		 NMEA_data.speed = 100*a2d(nmea_buffer[nmea_comindex[6]+1])+
+				   10*a2d(nmea_buffer[nmea_comindex[6]+2])+
+				   a2d(nmea_buffer[nmea_comindex[6]+3]);
+		break;
+	      case 2:
+		 NMEA_data.speed = 10*a2d(nmea_buffer[nmea_comindex[6]+1])+
+				   a2d(nmea_buffer[nmea_comindex[6]+2]);
+		break;
+	      case 1:
+		 NMEA_data.speed = a2d(nmea_buffer[nmea_comindex[6]+1]);
+		 break;
+	   }
+
+	   varlen = nmea_comindex[8]-nmea_comindex[7];
+	   switch (varlen-4) {
+	    case 3:
+	       NMEA_data.cmg_h = a2d(nmea_buffer[nmea_comindex[7]+1]);
+	       NMEA_data.cmg_l = 10*a2d(nmea_buffer[nmea_comindex[7]+2])+
+				 a2d(nmea_buffer[nmea_comindex[7]+3]);
+	       break;
+	    case 2:
+		NMEA_data.cmg_l = 10*a2d(nmea_buffer[nmea_comindex[7]+1])+
+				  a2d(nmea_buffer[nmea_comindex[7]+2]);
+	       break;
+	    case 1:
+	       NMEA_data.cmg_l = a2d(nmea_buffer[nmea_comindex[7]+1]);
+	       break;
+	   }
+	      NMEA_data.date_d = 10*a2d(nmea_buffer[nmea_comindex[8]+1])+a2d(nmea_buffer[nmea_comindex[8]+2]);
+	      NMEA_data.date_m = 10*a2d(nmea_buffer[nmea_comindex[8]+3])+a2d(nmea_buffer[nmea_comindex[8]+4]);
+	      NMEA_data.date_y = 10*a2d(nmea_buffer[nmea_comindex[8]+5])+a2d(nmea_buffer[nmea_comindex[8]+6]);
+	}
+	#if CAN_PRINTF==1
+		//printf("%02d-%02d-%02d",NMEA_data.date_y,NMEA_data.date_m,NMEA_data.date_d);
+	#endif
 }
 
 void nmea_make_index(void) {
@@ -144,7 +208,6 @@ void nmea_parse(void) {
 
 void sns_nmea_Init(void)
 {
-	uint8_t i;
 	Serial_Init();	
 	Timer_SetTimeout(sns_nmea_cansend_TIMER, sns_nmea_cansend_SEND_PERIOD , TimerTypeFreeRunning, &nmea_send_can);
 
@@ -251,13 +314,43 @@ void nmea_send_can(uint8_t timer)
 	txMsg.Data[2] = NMEA_data.time_s;
 	txMsg.Data[3] = 0; //Timezone diff from GMT
 
+	while (StdCan_Put(&txMsg) != StdCan_Ret_OK);
+
+	txMsg.Header.Command = CAN_MODULE_CMD_GLOBAL_GPSDATE;
+	txMsg.Length = 3;
+	txMsg.Data[0] = NMEA_data.date_y;
+	txMsg.Data[1] = NMEA_data.date_m;
+	txMsg.Data[2] = NMEA_data.date_d;
+
+	while (StdCan_Put(&txMsg) != StdCan_Ret_OK);
+
+	txMsg.Header.Command = CAN_MODULE_CMD_NMEA_HEADING;
+	txMsg.Length = 6;
+	txMsg.Data[0] = NMEA_data.fixvalid;
+	txMsg.Data[1] = NMEA_data.fixtype;
+	txMsg.Data[2] = NMEA_data.usedsat;
+	txMsg.Data[3] = NMEA_data.speed;
+	txMsg.Data[4] = NMEA_data.cmg_h;
+	txMsg.Data[5] = NMEA_data.cmg_l;
 	
+	while (StdCan_Put(&txMsg) != StdCan_Ret_OK);
 	
 	#if CAN_PRINTF==1
 	  //printf("Lat: %02d %02d.%02d%02d, Lon: %03d %02d.%02d%02d\n",NMEA_data.lat_deg, NMEA_data.lat_min, NMEA_data.lat_sec_h, NMEA_data.lat_sec_l >> 1,NMEA_data.lon_deg, NMEA_data.lon_min, NMEA_data.lon_sec_h, NMEA_data.lon_sec_l >> 1);
-	  printf("Fixtype: %01d\n",NMEA_data.fixtype);
+	  //printf("Time: %01d:%01d:%01d\n",NMEA_data.time_h,NMEA_data.time_m,NMEA_data.time_s);
 	  //printf("Fixvalid: %01d\n",NMEA_data.fixvalid);
 	  //printf("Sat: %02d\n",NMEA_data.usedsat);
+	  //printf("Type:%01d\n",NMEA_data.fixtype);
 	#endif
+
+	/*
+	 Add to msg:
+	 Valid fix,		NMEA_data.fixvalid
+	 # of satellites,	NMEA_data.numsat
+	 fixtype (no/2d/3d)	NMEA_data.fixtype
+	 Speed			NMEA_data.speed
+	 Course made good	NMEA_data.cmg_h, NMEA_data.cmg_l
+	 
+	*/
 }
 
