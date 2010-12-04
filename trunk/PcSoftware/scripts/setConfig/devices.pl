ae795897be52b0e6005910d0572e5b14704f22c7
Anders Runeson <runeson@gmail.com>
2010-12-04 10:48:46 +0000

    setConfig, bios.inc: make-target setconfig now updates MCU-fields if bios start contains such information
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1640 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/PcSoftware/scripts/setConfig/devices.pl
@@ -0,0 +1,10 @@
+#              device name   high    low     ext     boot start
+my @device0 = ('unknown',    '',     '',     '',     ''      );
+my @device1 = ('atmega8',    '0xd4', '0x24', '',     '0x1e00');
+my @device2 = ('atmega48',   '', '', '', '');
+my @device3 = ('atmega88',   '0xdc', '0xe2', '0x04', '0x1e00');
+my @device4 = ('atmega168',  '0xdc', '0xe2', '0x02', '0x3c00');
+my @device5 = ('atmega328p', '0xdc', '0xe2', '0x04', '0x7c00');
+
+@devices = (\@device0, \@device1, \@device2, \@device3, \@device4, \@device5);
+
