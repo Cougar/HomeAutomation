836810752fd1ad73fa045e1bf7b468749850304e
Anders Runeson <runeson@gmail.com>
2010-12-04 10:48:46 +0000

    setConfig, bios.inc: make-target setconfig now updates MCU-fields if bios start contains such information
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1640 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/scripts/setConfig/setConfig.pl
+++ PcSoftware/scripts/setConfig/setConfig.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/perl -w
 ################################################
 #
-# This file should not be edited for settings
-# use settings.pl for that
-#
 # author: arune @ efnet
 #
 ################################################
@@ -18,15 +15,27 @@ use Getopt::Long;
 $host = "localhost";
 $port = 1200;
 
+# Get hostname and port from command line arguments
 if ( @ARGV > 0 ) {
 	GetOptions(	"host=s" 	=> \$host,			#
 			"port=i"	=> \$port, 			#
 						);
 } 
 
-
 my $path = dirname(abs_path($0));
 
+# Read devices from separate file
+if (-e $path."/devices.pl") {
+	require $path."/devices.pl";
+} else {
+	print "Error: No devices.pl was found in ".$path.". \n\n";
+	exit 1;
+}
+
+# Length of devices array
+$numdevices=@devices;
+
+# Connect to candaemon
 $remote = IO::Socket::INET->new(
                     Proto    => "tcp",
                     PeerAddr => $host,
@@ -38,18 +47,63 @@ $biosfile = "bios.inc";
 open (FP, "+<".$biosfile) or die "Error: Cannot open file $biosfile\n";
 @file = <FP>;
 close FP;
-$foundrow = 0;
+
+# Check bios.inc for the settings that should be updated
+my $foundidrow = 0;
+my $foundmcurow = 0;
+my $foundhfrow = 0;
+my $foundlfrow = 0;
+my $foundefrow = 0;
+my $foundbootrow = 0;
 foreach $file (@file) {
 	if ($file =~ m/^NODE_HW_ID=/) {
-		$foundrow = 1;
+		print "\nCurrent id-setting: ".$file."\n";
+		$foundidrow = 1;
+	}
+	if ($file =~ m/^MCU=/) {
+		$foundmcurow = 1;
+	}
+	if ($file =~ m/^HIGHFUSE=/) {
+		$foundhfrow = 1;
+	}
+	if ($file =~ m/^LOWFUSE=/) {
+		$foundlfrow = 1;
+	}
+	if ($file =~ m/^EXTFUSE=/) {
+		$foundefrow = 1;
+	}
+	if ($file =~ m/^BOOT_START=/) {
+		$foundbootrow = 1;
 	}
 }
-if ($foundrow == 0) {
+if ($foundidrow == 0) {
 	print "Error: Did not find NODE_HW_ID= in file $biosfile\n";
 	exit 1;
 }
+if ($foundmcurow == 0) {
+	print "Error: Did not find MCU= in file $biosfile\n";
+	exit 1;
+}
+if ($foundhfrow == 0) {
+	print "Error: Did not find HIGHFUSE= in file $biosfile\n";
+	exit 1;
+}
+if ($foundlfrow == 0) {
+	print "Error: Did not find LOWFUSE= in file $biosfile\n";
+	exit 1;
+}
+if ($foundefrow == 0) {
+	print "Error: Did not find EXTFUSE= in file $biosfile\n";
+	exit 1;
+}
+if ($foundbootrow == 0) {
+	print "Error: Did not find BOOT_START= in file $biosfile\n";
+	exit 1;
+}
 
-#print "\n";
+# Define variable to know if a device was found
+my $devsettings=0;
+
 print "Connect your node to the CAN-network (disconnect it first if it's already connected)\n";
 print "Abort with Ctrl+c\n";
 #read line from socket (blocking)
@@ -68,16 +122,35 @@ while ( $line = <$remote> ) {
 							exit 1;
 						}
 						$hwid = (hex(substr($line, 38, 2))<<24)+(hex(substr($line, 35, 2))<<16)+
-						(hex(substr($line, 32, 2))<<8)+(hex(substr($line, 29, 2))<<0);
+							(hex(substr($line, 32, 2))<<8)+(hex(substr($line, 29, 2))<<0);
 						$hwidhex = sprintf("%08x", $hwid);
 						print "Setting HWID in bios.inc to 0x".$hwidhex."\n";
 						
+						$devtype = hex(substr($line, 26, 2));
+						if ($devtype == 0) 
+						{
+							print "\nWarning: Unknown device type, will not set MCU settings\n\n";
+						}
+						elsif ($devtype < $numdevices) 
+						{
+							print "Setting device type to ".$devices[$devtype][0]."\n";
+							$devsettings = 1;
+						}
+
 						open (FP, "<".$biosfile) or die "Cannot open file $biosfile\n";
 						@file = <FP>;
 						close FP;
 						open (FP, ">".$biosfile) or die "Cannot open file $biosfile\n";
 						foreach $file (@file) {
 							$file =~ s/^NODE_HW_ID=.*/NODE_HW_ID=0x$hwidhex/g;
+							if ($devsettings==1)
+							{
+								$file =~ s/^MCU=.*/MCU=$devices[$devtype][0]/g;
+								$file =~ s/^HIGHFUSE=.*/HIGHFUSE=$devices[$devtype][1]/g;
+								$file =~ s/^LOWFUSE=.*/LOWFUSE=$devices[$devtype][2]/g;
+								$file =~ s/^EXTFUSE=.*/EXTFUSE=$devices[$devtype][3]/g;
+								$file =~ s/^BOOT_START=.*/BOOT_START=$devices[$devtype][4]/g;
+							}
 							print FP $file;
 						}
 						close FP;
