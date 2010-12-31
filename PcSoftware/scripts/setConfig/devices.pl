f841cd1e81e7f79b78388994286a80f50164efbd
Mattias Runge <mattias@runge.se>
2010-12-31 11:46:16 +0000

    Normalized device type names, devices.pl matches data.xml now
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1668 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/scripts/setConfig/devices.pl
+++ PcSoftware/scripts/setConfig/devices.pl
@@ -1,10 +1,10 @@
-#              device name   high    low     ext     boot start protocol device name
-my @device0 = ('unknown',    '',     '',     '',     '',        'N/A');
-my @device1 = ('atmega8',    '0xd4', '0x24', '',     '0x1e00',  'mega8');
-my @device2 = ('atmega48',   '',     '',     '',     '',        'mega48');
-my @device3 = ('atmega88',   '0xdc', '0xe2', '0x04', '0x1e00',  'mega88');
-my @device4 = ('atmega168',  '0xdc', '0xe2', '0x02', '0x3c00',  'mega168');
-my @device5 = ('atmega328p', '0xdc', '0xe2', '0x04', '0x7c00',  'mega328');
+#              device name   high    low     ext     boot start
+my @device0 = ('unknown',    '',     '',     '',     '',     );
+my @device1 = ('atmega8',    '0xd4', '0x24', '',     '0x1e00');
+my @device2 = ('atmega48',   '',     '',     '',     '',     );
+my @device3 = ('atmega88',   '0xdc', '0xe2', '0x04', '0x1e00');
+my @device4 = ('atmega168',  '0xdc', '0xe2', '0x02', '0x3c00');
+my @device5 = ('atmega328p', '0xdc', '0xe2', '0x04', '0x7c00');
 
 @devices = (\@device0, \@device1, \@device2, \@device3, \@device4, \@device5);
 
