efbeaf661bf3ca95d23e4a64aa0db4b97b410247
Anders Runeson <runeson@gmail.com>
2011-01-04 18:23:06 +0000

    atom: more work done on upload to ppa script
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1676 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/scripts/debian/uploadPpaAtom.sh
+++ trunk/PcSoftware/scripts/debian/uploadPpaAtom.sh
@@ -8,15 +8,25 @@ SVNDIRCNF=ConfigSvn
 CNFDIR=Configuration
 REVFILE=./AtomSvnRev
 PPA=ppa:arune/auml-atom-daily
+DEBUILDFLG="-S -sa -k7EA270B4"
+PKGNAME=atom
+DIST=maverick
+PKGMAINT="Anders Runeson <runeson@gmail.com>"
 
 RUNDIR=`dirname $0`
+CHLOGDATE=`date -R`
 
 CURRREV=`svn info $SVNURL |grep '^Revision:' | sed -e 's/^Revision: //'`
-NOSVNDIR=atom-1.5.$CURRREV
-ORIGTAR=atom_1.5.$CURRREV.orig.tar.gz
-SRCCHFILE=atom_1.5.$CURRREV\_source.changes
+VERSION=1.5.$CURRREV
+NOSVNDIR=atom-$VERSION
+ORIGTAR=atom_$VERSION.orig.tar.gz
+SRCCHFILE=atom_$VERSION\_source.changes
+DPUTFLG="auml-atom-daily $SRCCHFILE"
 
-# Run script from cron every five minutes
+#echo $RUNDIR
+#exit 0
+
+# Run script from cron every hour
 # script will check revision of Atom in SVN and if there is a new version the script will
 # download it and create a complete debian source package and upload to launchpad as a ppa
 
@@ -52,26 +62,40 @@ svn export $ATOMDIR/$SVNDIR $ATOMDIR/$NOSVNDIR
 svn export $ATOMDIR/$SVNDIRCNF $ATOMDIR/$NOSVNDIR/$CNFDIR
 
 # Change install-target for data.xml file in CMakeList.txt
-###################################################################### TODO!!!
+cat $ATOMDIR/$NOSVNDIR/CMakeLists.txt | sed -e 's/\/..\/..\/Configuration\/data.xml/\/Configuration\/data.xml/' > $ATOMDIR/$NOSVNDIR/CMakeLists.txt
 
 # Create change log file
-SVNLOG=`svn log -r $CURRREV $SVNURL`
+#SVNLOG=`svn log -r $CURRREV $SVNURL`
 ###################################################################### TODO!!!
+echo "$PKGNAME ($VERSION) $DIST; urgency=low" > $ATOMDIR/$NOSVNDIR/debian/changelog
+echo "" >> $ATOMDIR/$NOSVNDIR/debian/changelog
+echo "  * TODO: Add this log from SVN change log" >> $ATOMDIR/$NOSVNDIR/debian/changelog
+echo "" >> $ATOMDIR/$NOSVNDIR/debian/changelog
+echo " -- $PKGMAINT  $CHLOGDATE" >> $ATOMDIR/$NOSVNDIR/debian/changelog
 
 # Create orig tar gz
 tar -zcf $ATOMDIR/$ORIGTAR $ATOMDIR/$NOSVNDIR
 
 # Create source package with debuild
 #cd $ATOMDIR/$NOSVNDIR
-#debuild -S
+#debuild $DEBUILDFLG
+echo cd $ATOMDIR/$NOSVNDIR
+echo debuild $DEBUILDFLG
 
+# Upload to launchpad with dput
 #cd $ATOMDIR
+#dput $DPUTFLG
+echo cd $ATOMDIR
+echo dput $DPUTFLG
+# Requires ubuntu 9.10 or newer
 #dput $PPA $SRCCHFILE
 
-echo "the end $RUNDIR"
+#echo "the end $RUNDIR"
 
-# Remove all files
+# Remove all temporary files
 #rm -rf $ATOMDIR
 
 # Write the current svn revision to file
 echo -n $CURRREV > $RUNDIR/$REVFILE
+
+cd $RUNDIR
