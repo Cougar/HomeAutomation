20398d1b32beb5e104337e8322f0ed45a35c7484
Anders Runeson <runeson@gmail.com>
2011-02-20 16:23:00 +0000

    pca pwm now supports fading
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1714 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/module/act_pcaPWM/act_pcaPWM.c
+++ trunk/EmbeddedSoftware/AVR/module/act_pcaPWM/act_pcaPWM.c
@@ -5,6 +5,18 @@
 static uint16_t setValue[8]={0,0,0,0,0,0,0,0};
 static uint16_t isValue[8];
 
+int8_t fadeSpeed[8] = {0,0,0,0,0,0,0,0};
+uint8_t fadeSpeedFrac[8] = {0,0,0,0,0,0,0,0};
+uint16_t fadeTarget[8] = {0,0,0,0,0,0,0,0};
+uint8_t fadeSpeedCnt[8] = {0,0,0,0,0,0,0,0};
+
+uint16_t demoEndValue[8] = {0,0,0,0,0,0,0,0};
+uint16_t demoHighValue[8] = {0,0,0,0,0,0,0,0};
+uint8_t demoState[8] = {ACT_PCAPWM_DEMO_STATE_NOT_RUNNING, ACT_PCAPWM_DEMO_STATE_NOT_RUNNING, 
+						ACT_PCAPWM_DEMO_STATE_NOT_RUNNING, ACT_PCAPWM_DEMO_STATE_NOT_RUNNING,
+						ACT_PCAPWM_DEMO_STATE_NOT_RUNNING, ACT_PCAPWM_DEMO_STATE_NOT_RUNNING,
+						ACT_PCAPWM_DEMO_STATE_NOT_RUNNING, ACT_PCAPWM_DEMO_STATE_NOT_RUNNING};
+
 #ifdef act_pcaPWM_USEEEPROM
 #include "act_pcaPWM_eeprom.h"
 struct eeprom_act_pcaPWM EEMEM eeprom_act_pcaPWM =
@@ -22,6 +34,59 @@ struct eeprom_act_pcaPWM EEMEM eeprom_act_pcaPWM =
 	},
 	0	// crc, must be a correct value, but this will also be handled by the EEPROM module or make scripts
 };
+#endif
+
+#if act_pcaPWM_ENABLE_FADE == 1
+void act_pcaPWM_timer_callback(uint8_t timer)
+{
+	uint8_t channel = 0;
+	for (channel = 0; channel < 8; channel++) {
+		/* Check demo states */
+		if (setValue[channel] == fadeTarget[channel]) {
+			switch (demoState[channel])
+			{
+			case ACT_PCAPWM_DEMO_STATE_NOT_RUNNING:
+			break;
+			case ACT_PCAPWM_DEMO_STATE_DECREASE:
+				demoState[channel] = ACT_PCAPWM_DEMO_STATE_INCREASE;
+				fadeTarget[channel] = demoHighValue[channel];
+				fadeSpeed[channel] = -fadeSpeed[channel];
+			break;
+			case ACT_PCAPWM_DEMO_STATE_INCREASE:
+				demoState[channel] = ACT_PCAPWM_DEMO_STATE_GOBACK;
+				fadeTarget[channel] = demoEndValue[channel];
+				fadeSpeed[channel] = -fadeSpeed[channel];
+			break;
+			case ACT_PCAPWM_DEMO_STATE_GOBACK:
+				demoState[channel] = ACT_PCAPWM_DEMO_STATE_NOT_RUNNING;
+			break;
+			}
+		}
+		
+		/* Change the dimmerValue according to the current fading */
+		fadeSpeedCnt[channel]++;
+		if (fadeSpeedCnt[channel] == fadeSpeedFrac[channel] && setValue[channel] != (fadeTarget[channel])) 
+		{
+			fadeSpeedCnt[channel] = 0;
+			uint16_t tempDimVal = setValue[channel];
+			setValue[channel] += (fadeSpeed[channel]*39);
+			if ((fadeSpeed[channel] > 0 && (setValue[channel] < tempDimVal || setValue[channel] >= (fadeTarget[channel]))) || 
+				(fadeSpeed[channel] < 0 && (setValue[channel] > tempDimVal || setValue[channel] <= (fadeTarget[channel])))) 
+			{
+				setValue[channel] = (fadeTarget[channel]);
+			}
+			if (setValue[channel] > 10000) {
+				setValue[channel] = 10000;
+			}
+			/* if targetvalue was reached then send the netinfo packet */
+			if (setValue[channel] == fadeTarget[channel])
+			{
+				//sendInfo[channel] = 1;
+			}
+		}
+	}
+
+}
 #endif
 
 void act_pcaPWM_Init(void)
@@ -66,6 +131,10 @@ void act_pcaPWM_Init(void)
 #ifdef act_pcaPWM_LED_OVERRIDE_IO
 	gpio_set_out(act_pcaPWM_LED_OVERRIDE_IO);
 	gpio_clr_pin(act_pcaPWM_LED_OVERRIDE_IO);
+#endif
+
+#if act_pcaPWM_ENABLE_FADE == 1
+	Timer_SetTimeout(act_pcaPWM_FADE_TIMER, 10, TimerTypeFreeRunning, &act_pcaPWM_timer_callback);
 #endif
 }
 
@@ -139,6 +208,178 @@ void act_pcaPWM_HandleMessage(StdCan_Msg_t *rxMsg)
 				setValue[channel] = (rxMsg->Data[1]<<8)+(rxMsg->Data[2]);
 			}
 		break;
+#if act_pcaPWM_ENABLE_FADE == 1
+		case CAN_MODULE_CMD_PCAPWM_DEMO:		/* Demo(channel, speed, steps) */
+			if (rxMsg->Length == 4) {
+				channel = rxMsg->Data[0];
+				uint8_t speed = rxMsg->Data[1];
+				uint16_t steps = (rxMsg->Data[2]<<8)+(rxMsg->Data[3]);
+				
+				fadeSpeedCnt[channel] = 0;
+				fadeSpeed[channel] = 0;
+				if (speed == 0) {
+					/* do nothing */
+				} else {
+					uint16_t diffToMin = setValue[channel] - ACT_PCAPWM_MIN_DIM;
+					uint16_t diffToMax = ACT_PCAPWM_MAX_DIM - setValue[channel];
+				
+					demoEndValue[channel] = setValue[channel];
+					if (diffToMin >= steps && diffToMax >= steps)
+					{
+						/* not close to min or max */
+						fadeTarget[channel] = setValue[channel] - steps;
+						demoHighValue[channel] = setValue[channel] + steps;
+					}
+					else if (diffToMin >= steps)
+					{
+						/* close to max */
+						fadeTarget[channel] = setValue[channel] - steps - steps + diffToMax;
+						demoHighValue[channel] = ACT_PCAPWM_MAX_DIM;
+					}
+					else if (diffToMax >= steps)
+					{
+						/* close to min */
+						fadeTarget[channel] = ACT_PCAPWM_MIN_DIM;
+						demoHighValue[channel] = setValue[channel] + steps + steps - diffToMin;
+					}
+					demoState[channel] = ACT_PCAPWM_DEMO_STATE_DECREASE;
+				
+					if ((speed&0x80) == 0x80) {
+						fadeSpeed[channel] = (speed&0x7f)+1;
+						fadeSpeedFrac[channel] = 1;
+					} else {
+						fadeSpeed[channel] = 1;
+						fadeSpeedFrac[channel] = 0x80-(speed&0x7f);
+					}
+					if (fadeTarget[channel] <= setValue[channel]) {
+						fadeSpeed[channel] = -fadeSpeed[channel];
+					}
+				}
+			}
+		break;
+		
+		case CAN_MODULE_CMD_PCAPWM_START_FADE:	/* StartFade(channel, speed, direction) */
+			if (rxMsg->Length == 3) {
+				
+				channel = rxMsg->Data[0];
+				uint8_t speed = rxMsg->Data[1];
+				uint8_t direction = rxMsg->Data[2];
+				demoState[channel] = ACT_PCAPWM_DEMO_STATE_NOT_RUNNING;
+				fadeSpeedCnt[channel] = 0;
+				fadeSpeed[channel] = 0;
+				uint16_t endValue = 0;
+				if (direction == CAN_MODULE_ENUM_PCAPWM_START_FADE_DIRECTION_INCREASE) {
+					endValue = ACT_PCAPWM_MAX_DIM;
+				} else if (direction == CAN_MODULE_ENUM_PCAPWM_START_FADE_DIRECTION_DECREASE) {
+					endValue = ACT_PCAPWM_MIN_DIM;
+				}
+					
+				if (speed == 0) {
+					setValue[channel] = endValue;	/* set dimmer value immediately */
+					//sendInfo[channel] = 1;		/* send netinfo with the current dimmervalue*/
+				} else {
+					fadeTarget[channel] = endValue;
+					if (fadeTarget[channel] != setValue[channel]) {
+						if ((speed&0x80) == 0x80) {
+							fadeSpeed[channel] = (speed&0x7f)+1;
+							fadeSpeedFrac[channel] = 1;
+						} else {
+							fadeSpeed[channel] = 1;
+							fadeSpeedFrac[channel] = 0x80-(speed&0x7f);
+						}
+						if (fadeTarget[channel] < setValue[channel]) {
+							fadeSpeed[channel] = -fadeSpeed[channel];
+						}
+					}
+				}
+			}
+		break;
+		
+		case CAN_MODULE_CMD_PCAPWM_STOP_FADE:	/* StopFade(channel) */
+			if (rxMsg->Length == 1) {
+				channel = rxMsg->Data[0];
+				demoState[channel] = ACT_PCAPWM_DEMO_STATE_NOT_RUNNING;
+				fadeSpeed[channel] = 0;
+				//sendInfo[channel] = 1;		/* send netinfo with the current dimmervalue*/
+			}
+		break;
+
+		case CAN_MODULE_CMD_PCAPWM_ABS_FADE:	/* AbsFade(channel, speed, endValue) */
+			if (rxMsg->Length == 4) {
+				channel = rxMsg->Data[0];
+				uint8_t speed = rxMsg->Data[1];
+				uint16_t endValue = (rxMsg->Data[2]<<8)+(rxMsg->Data[3]);
+				demoState[channel] = ACT_PCAPWM_DEMO_STATE_NOT_RUNNING;
+				fadeSpeedCnt[channel] = 0;
+				fadeSpeed[channel] = 0;
+				if (speed == 0) {
+					setValue[channel] = endValue;	/* set dimmer value immediately */
+					//sendInfo[channel] = 1;		/* send netinfo with the current dimmervalue*/
+				} else {
+					fadeTarget[channel] = endValue;
+					if (fadeTarget[channel] != setValue[channel]) {
+						if ((speed&0x80) == 0x80) {
+							fadeSpeed[channel] = (speed&0x7f)+1;
+							fadeSpeedFrac[channel] = 1;
+						} else {
+							fadeSpeed[channel] = 1;
+							fadeSpeedFrac[channel] = 0x80-(speed&0x7f);
+						}
+						if (fadeTarget[channel] < setValue[channel]) {
+							fadeSpeed[channel] = -fadeSpeed[channel];
+						}
+					}
+				}
+				//printf("abs fade %d %d %d!\n",fadeTarget[channel], endValue ,pwmValue[channel] );
+			}
+		break;
+		
+		case CAN_MODULE_CMD_PCAPWM_REL_FADE:	/* RelFade(channel, speed, direction, steps) */
+			if (rxMsg->Length == 5) {
+				
+				channel = rxMsg->Data[0];
+				uint8_t speed = rxMsg->Data[1];
+				uint8_t direction = rxMsg->Data[2];
+				uint16_t steps = (rxMsg->Data[3]<<8)+(rxMsg->Data[4]);
+				
+				demoState[channel] = ACT_PCAPWM_DEMO_STATE_NOT_RUNNING;
+				fadeSpeedCnt[channel] = 0;
+				fadeSpeed[channel] = 0;
+				uint16_t tempDimVal = setValue[channel];
+				uint16_t tempDimVal2 = setValue[channel];
+				if (direction == CAN_MODULE_ENUM_PCAPWM_REL_FADE_DIRECTION_INCREASE) {					/* if increase */
+					tempDimVal2 += steps;				/* calculate new value */
+					if (tempDimVal2 < tempDimVal) {		/* make overflow test */
+						tempDimVal2 = ACT_PCAPWM_MAX_DIM;
+					}
+				} else if (direction == CAN_MODULE_ENUM_PCAPWM_REL_FADE_DIRECTION_DECREASE) {			/* if decrease */
+					tempDimVal2 -= steps;
+					if (tempDimVal2 > tempDimVal) {
+						tempDimVal2 = ACT_PCAPWM_MIN_DIM;
+					}
+				}
+				if (speed == 0) {
+					setValue[channel] = tempDimVal2;		/* set dimmer value immediately */
+					//sendInfo[channel] = 1;				/* send netinfo with the current dimmervalue*/
+				} else {
+					fadeTarget[channel] = tempDimVal2;		/* set the fade target */
+					
+					if (fadeTarget[channel] != setValue[channel]) {
+						if ((speed&0x80) == 0x80) {
+							fadeSpeed[channel] = (speed&0x7f)+1;
+							fadeSpeedFrac[channel] = 1;
+						} else {
+							fadeSpeed[channel] = 1;
+							fadeSpeedFrac[channel] = 0x80-(speed&0x7f);
+						}
+						if (fadeTarget[channel] < setValue[channel]) {
+							fadeSpeed[channel] = -fadeSpeed[channel];
+						}
+					}
+				}
+			}
+		break;
+#endif
 		}
 	}
 }
