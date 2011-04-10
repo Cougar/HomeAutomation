f0322c1767806649aa386cd15c95681972c333f1
pengi <pengi@e69ec2e4-db17-0410-bd4a-d6f28f89cd0d>
2011-04-10 16:34:10 +0000

    Made pins in act_sigmadelta_dimmer configurable
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1725 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/module/act_sigmadelta_dimmer/act_sigmadelta_dimmer.c
+++ trunk/EmbeddedSoftware/AVR/module/act_sigmadelta_dimmer/act_sigmadelta_dimmer.c
@@ -7,11 +7,24 @@
 void act_sigmadelta_dimmer_update( uint16_t channel_id, uint16_t value );
 #endif
 
+#define GPIO_STRUCT_TO_ARGS(_VAR) (_VAR).port, (_VAR).pin, (_VAR).ddr, (_VAR).nr, (_VAR).pcint
+struct gpio_t {
+    volatile uint8_t* port;
+    volatile uint8_t* pin;
+    volatile uint8_t* ddr;
+    uint8_t nr;
+    uint8_t pcint;
+};
+
+
 uint8_t stepper = 0;
 
 uint16_t inval = 0;
 int32_t dim_level[act_sigmadelta_dimmer_channel_count];
 int32_t dim_sigma[act_sigmadelta_dimmer_channel_count];
+const struct gpio_t dim_pins[act_sigmadelta_dimmer_channel_count] = {
+    act_sigmadelta_dimmer_pins
+};
 
 void act_sigmadelta_dimmer_chn_update( uint16_t channel_id, uint16_t value ) {
     channel_id -= act_sigmadelta_dimmer_base_channel; // Overflows for lower id
@@ -21,12 +34,13 @@ void act_sigmadelta_dimmer_chn_update( uint16_t channel_id, uint16_t value ) {
 }
 
 void act_sigmadelta_dimmer_Init(void) {
+    uint8_t i;
 //    Timer_SetTimeout( act_sigmadelta_dimmer_SYS_TIMER, 1 , TimerTypeFreeRunning, 0);
 
 // CDEA GFIH JKLM
 
     // FIXME: port constants, as a list?
-    gpio_set_out( EXP_C );
+/*    gpio_set_out( EXP_C );
     gpio_set_out( EXP_D );
     gpio_set_out( EXP_E );
 
@@ -40,10 +54,14 @@ void act_sigmadelta_dimmer_Init(void) {
 
     gpio_set_out( EXP_B );//K );
     gpio_set_out( EXP_L );
-    gpio_set_out( EXP_M );
+    gpio_set_out( EXP_M );*/
+
+
+    for( i=0; i<act_sigmadelta_dimmer_channel_count; i++ ) {
+        gpio_set_out( GPIO_STRUCT_TO_ARGS( dim_pins[i] ) );
+    }
 
 #ifdef CHN_LOADED
-    uint8_t i;
     for( i=0; i<act_sigmadelta_dimmer_channel_count; i++ ) {
         dim_level[i] = 0;
         dim_sigma[i] = 0;
@@ -67,9 +85,11 @@ void act_sigmadelta_dimmer_Process(void) {
         dim_sigma[i] += delta;
         if( dim_sigma[i] >  655360 ) dim_sigma[i] = 655360;
         if( dim_sigma[i] < -655360 ) dim_sigma[i] = -655360;
+
+        gpio_set_statement( dim_sigma[i]>=0, GPIO_STRUCT_TO_ARGS( dim_pins[i] ) );
     }
 
-    // FIXME: port constants, as a list?
+/*    // FIXME: port constants, as a list?
     gpio_set_statement( dim_sigma[ 0]>=0, EXP_C );
     gpio_set_statement( dim_sigma[ 1]>=0, EXP_D );
     gpio_set_statement( dim_sigma[ 2]>=0, EXP_E );
@@ -84,7 +104,7 @@ void act_sigmadelta_dimmer_Process(void) {
 
     gpio_set_statement( dim_sigma[ 9]>=0, EXP_B ); //K );
     gpio_set_statement( dim_sigma[10]>=0, EXP_L );
-    gpio_set_statement( dim_sigma[11]>=0, EXP_M );
+    gpio_set_statement( dim_sigma[11]>=0, EXP_M );*/
 }
 
 void act_sigmadelta_dimmer_HandleMessage(StdCan_Msg_t *rxMsg) {
