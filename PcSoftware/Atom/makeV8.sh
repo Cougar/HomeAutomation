222a37e76b88ba2e0e46067fc2eda41f9387ecb2
Anders Runeson <runeson@gmail.com>
2011-11-04 14:06:33 +0000

    Temporary fix to remove Werror from v8 scons buildconfig during build
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1781 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/Atom/makeV8.sh
+++ PcSoftware/Atom/makeV8.sh
@@ -3,12 +3,17 @@
 svn checkout http://v8.googlecode.com/svn/tags/3.2.3.1 src/v8
 cd src/v8
 
+cp SConstruct SConstruct.bak
+patch SConstruct < ../atom.v8.diff
+
 arch=`uname -m`
 
 if [ "$arch" = "x86_64" ]; then
-	scons arch=x64 mode=release snapshot=on
+	scons -Wall arch=x64 mode=release snapshot=on
 else
-	scons mode=release snapshot=on
+	scons -Wall mode=release snapshot=on
 fi
+
+mv SConstruct.bak SConstruct
 
 cd ../..
