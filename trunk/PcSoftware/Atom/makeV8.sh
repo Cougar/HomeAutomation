b7036bf7afd1e5d1c2d63c7818c8dbc8f31c23ac
Anders Runeson <runeson@gmail.com>
2011-11-06 19:49:45 +0000

    Fix for v8 build error redone, cred to John A/myrslok
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@1782 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/Atom/makeV8.sh
+++ trunk/PcSoftware/Atom/makeV8.sh
@@ -3,17 +3,17 @@
 svn checkout http://v8.googlecode.com/svn/tags/3.2.3.1 src/v8
 cd src/v8
 
-cp SConstruct SConstruct.bak
-patch SConstruct < ../atom.v8.diff
-
 arch=`uname -m`
 
 if [ "$arch" = "x86_64" ]; then
-	scons -Wall arch=x64 mode=release snapshot=on
+	ARCH='arch=x64'
 else
-	scons -Wall mode=release snapshot=on
+	ARCH=''
 fi
 
-mv SConstruct.bak SConstruct
+patch SConstruct -o AumlSConstruct < ../atom.v8.diff
+scons -f AumlSConstruct $ARCH mode=release snapshot=on
+rm AumlSConstruct
 
 cd ../..
+
