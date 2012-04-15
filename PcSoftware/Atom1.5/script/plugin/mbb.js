14c070db624774fb19a111b3a284023c507275df
Mattias Runge <mattias@runge.se>
2012-04-15 10:03:44 +0000

    Updated MBB js to parse NMEA to a position structure
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@1949 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/Atom1.5/script/plugin/mbb.js
+++ PcSoftware/Atom1.5/script/plugin/mbb.js
@@ -29,6 +29,8 @@ function Mbb_Disconnected(client_id)
 
 function Mbb_ReceivedData(client_id, data)
 {
+  Log(data);
+  
   var json_data = eval("(" + data + ")");
   
   //Log(JSON.stringify(json_data));
@@ -43,13 +45,16 @@ function Mbb_ReceivedData(client_id, data)
     {
       Mbb_Clients[client_id]["LastNmea"]["RMC"] = json_data.params.nmea;
       
+      Mbb_Clients[client_id]["Position"] = _Mbb_NmeaToPosition(client_id);
+      
       for (var n = 0; n < Mbb_Susbscribers.length; n++)
       {
-        Mbb_Susbscribers[n]("nmea", Mbb_Clients[client_id]);
+        Mbb_Susbscribers[n]("position", Mbb_Clients[client_id]);
       }
       
       Mbb_Clients[client_id]["LastNmea"]["GGA"] = "";
       Mbb_Clients[client_id]["LastNmea"]["RMC"] = "";
+      Mbb_Clients[client_id]["Position"] = false;
     }
   }
   else /*if (json_data.method == "MBB.Connected")*/
@@ -60,6 +65,7 @@ function Mbb_ReceivedData(client_id, data)
     
     Mbb_Clients[client_id]["Id"] = client_id;
     Mbb_Clients[client_id]["Info"] = json_data.params;
+    Mbb_Clients[client_id]["Position"] = false;
     Mbb_Clients[client_id]["LastNmea"] = {};
     Mbb_Clients[client_id]["LastNmea"]["GGA"] = "";
     Mbb_Clients[client_id]["LastNmea"]["RMC"] = "";
@@ -78,4 +84,77 @@ function Mbb_ReceivedData(client_id, data)
 function Mbb_SendData(client_id, json_data)
 {
   return MbbExport_SendData(client_id, JSON.Stringify(json_data));
+}
+
+function _Mbb_NmeaToPosition(client_id)
+{
+  /*
+      GGA          Global Positioning System Fix Data
+*     123519       Fix taken at 12:35:19 UTC
+*     4807.038,N   Latitude 48 deg 07.038' N
+*     01131.000,E  Longitude 11 deg 31.000' E
+  1            Fix quality: 0 = invalid
+                            1 = GPS fix (SPS)
+                            2 = DGPS fix
+                            3 = PPS fix
+                            4 = Real Time Kinematic
+                            5 = Float RTK
+                            6 = estimated (dead reckoning) (2.3 feature)
+                            7 = Manual input mode
+                            8 = Simulation mode
+*     08           Number of satellites being tracked
+*     0.9          Horizontal dilution of position
+*     545.4,M      Altitude, Meters, above mean sea level
+*     46.9,M       Height of geoid (mean sea level) above WGS84
+                  ellipsoid
+  (empty field) time in seconds since last DGPS update
+  (empty field) DGPS station ID number
+  *47          the checksum data, always begins with *
+  
+  
+  $GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
+
+      RMC          Recommended Minimum sentence C
+*     123519       Fix taken at 12:35:19 UTC
+*     A            Status A=active or V=Void.
+*     4807.038,N   Latitude 48 deg 07.038' N
+*     01131.000,E  Longitude 11 deg 31.000' E
+*     022.4        Speed over the ground in knots
+*     084.4        Track angle in degrees True
+*     230394       Date - 23rd of March 1994
+*     003.1,W      Magnetic Variation
+  *6A          The checksum data, always begins with *
+*/
+  
+  
+  var nmea_rmc_parts = Mbb_Clients[client_id]["LastNmea"]["RMC"].split(",");
+  
+  if (nmea_rmc_parts[2] == "A")
+  {
+    var nmea_gga_parts = Mbb_Clients[client_id]["LastNmea"]["GGA"].split(",");
+    var position = {};
+  
+    position["Datetime"]            = "20" + nmea_rmc_parts[9].substr(4, 2) + "-" + nmea_rmc_parts[9].substr(2, 2) + "-" + nmea_rmc_parts[9].substr(0, 2);
+    position["Datetime"]            += " " + nmea_rmc_parts[1].substr(0, 2) + ":" + nmea_rmc_parts[1].substr(2, 2) + ":" + nmea_rmc_parts[1].substr(4, 2) + " UTC";
+  
+    position["Latitude"]            = nmea_rmc_parts[3] + "," + nmea_rmc_parts[4];
+    position["Longitude"]           = nmea_rmc_parts[5] + "," + nmea_rmc_parts[6];
+    position["HorizontalDilution"]  = nmea_gga_parts[8];
+    
+    position["Altitude"]            = nmea_gga_parts[9] + "," + nmea_gga_parts[10];
+    position["HeightOfGeoid"]       = nmea_gga_parts[11] + "," + nmea_gga_parts[12];
+    
+    position["SpeedKnots"]          = nmea_rmc_parts[7];
+    
+    position["Angle"]               = nmea_rmc_parts[8];
+    
+    if (nmea_rmc_parts[10] != "")
+    {
+      position["MagneticVariation"]   = nmea_rmc_parts[10] + "," + nmea_rmc_parts[11].substr(0, 1);
+    }
+    
+    return position;
+  }
+  
+  return false;
 }
