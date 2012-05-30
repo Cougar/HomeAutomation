1bfdbcd9f2a9d0e86af4569244cf948f1881c9e2
Anders Runeson <runeson@gmail.com>
2012-05-30 20:01:04 +0000

    sns_DHT11 now supports two sensors
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@2007 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/module/sns_DHT11/sns_DHT11.c
+++ trunk/EmbeddedSoftware/AVR/module/sns_DHT11/sns_DHT11.c
@@ -1,8 +1,9 @@
 
 #include "sns_DHT11.h"
 uint8_t sendNewTemp = 0;
-uint8_t temperature = 0;
-uint8_t humidity = 0;
+uint8_t temperature[DHT11_NUM_SENSORS];
+uint8_t humidity[DHT11_NUM_SENSORS];
+uint8_t sns_DHT11_currentSensor = DHT11_NUM_SENSORS-1;
 
 #if sns_DHT11_USEEEPROM==1
 #include "sns_DHT11_eeprom.h"
@@ -23,13 +24,29 @@ void ConvertTemperature2_callback(uint8_t timer)
 	uint8_t word[5];
 	if (dht11_readData((uint8_t*) word)) {
 		sendNewTemp = 1;
-		temperature = word[2];
-		humidity = word[0];
+		temperature[sns_DHT11_currentSensor] = word[2];
+		humidity[sns_DHT11_currentSensor] = word[0];
 	}
 }
 
 void ConvertTemperature_callback_DHT11(uint8_t timer)
 {
+#ifndef DHT11_ONE_BUS
+	sns_DHT11_currentSensor++;
+	if (sns_DHT11_currentSensor >= DHT11_NUM_SENSORS)
+	{sns_DHT11_currentSensor = 0;}
+
+	switch (sns_DHT11_currentSensor)
+	{
+		case 0:
+			dht11_set_bus(DHT11_PIN_ch0);
+			break;
+		case 1:
+			dht11_set_bus(DHT11_PIN_ch1);
+			break;
+	}
+#endif
+
 	dht11_start();
 	Timer_SetTimeout(sns_DHT11_18MS_TIMER, 18, TimerTypeOneShot, &ConvertTemperature2_callback);
 }
@@ -51,10 +68,6 @@ void sns_DHT11_Init(void)
 	  EEDATA_UPDATE_CRC;
 	}
 #endif
-	///TODO: Initialize hardware etc here
-
-	// to use PCINt lib, call this function: (the callback function look as a timer callback function)
-	// Pcint_SetCallbackPin(sns_DHT11_PCINT, EXP_C , &sns_DHT11_pcint_callback);
 	Timer_SetTimeout(sns_DHT11_TIMER, 5000, TimerTypeFreeRunning, &ConvertTemperature_callback_DHT11);
 }
 
@@ -74,7 +87,7 @@ void sns_DHT11_Process(void)
 		txMsg.Length = 3;
 
 		txMsg.Data[0] = 1;
-		temp = temperature*64;
+		temp = temperature[sns_DHT11_currentSensor]*64;
 		txMsg.Data[1] = (uint8_t)(temp >> 8);
 		txMsg.Data[2] = (uint8_t)(temp & 0xff);
 		while (StdCan_Put(&txMsg) != StdCan_Ret_OK);
@@ -87,21 +100,19 @@ void sns_DHT11_Process(void)
 		txMsg.Length = 3;
 
 		txMsg.Data[0] = 1;
-		temp = humidity*64;
+		temp = humidity[sns_DHT11_currentSensor]*64;
 		txMsg.Data[1] = (uint8_t)(temp >> 8);
 		txMsg.Data[2] = (uint8_t)(temp & 0xff);
 		while (StdCan_Put(&txMsg) != StdCan_Ret_OK);
 
 	}
-
-	///TODO: Stuff that needs doing is done here
 }
 
 void sns_DHT11_HandleMessage(StdCan_Msg_t *rxMsg)
 {
-	if (	StdCan_Ret_class(rxMsg->Header) == CAN_MODULE_CLASS_SNS && ///TODO: Change this to the actual class type
+	if (	StdCan_Ret_class(rxMsg->Header) == CAN_MODULE_CLASS_SNS &&
 		StdCan_Ret_direction(rxMsg->Header) == DIRECTIONFLAG_FROM_OWNER &&
-		rxMsg->Header.ModuleType == CAN_MODULE_TYPE_SNS_DHT11 && ///TODO: Change this to the actual module type
+		rxMsg->Header.ModuleType == CAN_MODULE_TYPE_SNS_DHT11 &&
 		rxMsg->Header.ModuleId == sns_DHT11_ID)
 	{
 		StdCan_Msg_t txMsg;
@@ -131,9 +142,9 @@ void sns_DHT11_List(uint8_t ModuleSequenceNumber)
 {
 	StdCan_Msg_t txMsg;
 
-	StdCan_Set_class(txMsg.Header, CAN_MODULE_CLASS_SNS); ///TODO: Change this to the actual class type
+	StdCan_Set_class(txMsg.Header, CAN_MODULE_CLASS_SNS);
 	StdCan_Set_direction(txMsg.Header, DIRECTIONFLAG_FROM_OWNER);
-	txMsg.Header.ModuleType = CAN_MODULE_TYPE_SNS_DHT11; ///TODO: Change this to the actual module type
+	txMsg.Header.ModuleType = CAN_MODULE_TYPE_SNS_DHT11;
 	txMsg.Header.ModuleId = sns_DHT11_ID;
 	txMsg.Header.Command = CAN_MODULE_CMD_GLOBAL_LIST;
 	txMsg.Length = 6;
