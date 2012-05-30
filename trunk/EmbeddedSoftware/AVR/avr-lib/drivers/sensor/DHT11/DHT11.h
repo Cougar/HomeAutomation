1226845fdf2f36fb6df111928e30b9b44a3be7a1
Anders Runeson <runeson@gmail.com>
2012-05-30 20:19:44 +0000

    DHT11 driver now supports changing port
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@2008 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/avr-lib/drivers/sensor/DHT11/DHT11.h
+++ trunk/EmbeddedSoftware/AVR/avr-lib/drivers/sensor/DHT11/DHT11.h
@@ -9,32 +9,29 @@
 /* Hardware connection                     */
 /*******************************************/
 
+#if DHT11_NUM_SENSORS==1
 /* Define DHT11_ONE_BUS if only one 1-Wire-Bus is used
    in the application -> shorter code.
    If not defined make sure to call dht11_set_bus() before using
-   a bus. Runtime bus-select increases code size by around 300 
-   bytes so use DHT11_ONE_BUS if possible */
+   a bus. */
 #define DHT11_ONE_BUS
-
-#ifdef DHT11_ONE_BUS
-
-#ifndef DHT11_PIN
-  #warning DHT11_PIN shall be defined in config.inc, using DHT11_PIN=EXP_F...
-  #define DHT11_PIN  EXP_F
 #endif
 
-#define DHT11_CONF_DELAYOFFSET 0
+#ifndef DHT11_PIN_ch0
+  #warning DHT11_PIN_ch0 shall be defined in config.inc, using DHT11_PIN_ch0=EXP_F...
+  #define DHT11_PIN_ch0  EXP_F
+#endif
 
-#else
-#error The lib does not support not defining DHT11_ONE_BUS
-#if F_CPU<1843200
- #warning | experimental multi-bus-mode is not tested for 
- #warning | frequencies below 1,84MHz - use DHT11_ONE_WIRE or
- #warning | faster clock-source (i.e. internal 2MHz R/C-Osc.)
-#endif
-#define DHT11_CONF_CYCLESPERACCESS 13
-#define DHT11_CONF_DELAYOFFSET ( (uint16_t)( ((DHT11_CONF_CYCLESPERACCESS)*1000000L) / F_CPU  ) )
-#endif
+//#define DHT11_CONF_DELAYOFFSET 0
+
+//#if F_CPU<1843200
+// #warning | experimental multi-bus-mode is not tested for 
+// #warning | frequencies below 1,84MHz - use DHT11_ONE_WIRE or
+// #warning | faster clock-source (i.e. internal 2MHz R/C-Osc.)
+//#endif
+//#define DHT11_CONF_CYCLESPERACCESS 13
+//#define DHT11_CONF_DELAYOFFSET ( (uint16_t)( ((DHT11_CONF_CYCLESPERACCESS)*1000000L) / F_CPU  ) )
+
 
 /*******************************************/
 
@@ -43,10 +40,11 @@ extern uint8_t dht11_readData(uint8_t* data);
 
 
 #ifndef DHT11_ONE_BUS
-extern void dht11_set_bus(volatile uint8_t* in,
-	volatile uint8_t* out,
+extern void dht11_set_bus(volatile uint8_t* out,
+	volatile uint8_t* in,
 	volatile uint8_t* ddr,
-	uint8_t pin);
+	uint8_t pin,
+	uint8_t pcint);
 #endif
 
 #endif
