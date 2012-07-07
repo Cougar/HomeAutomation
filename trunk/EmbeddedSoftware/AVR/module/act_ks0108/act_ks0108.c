75115a74b68bca66aec27763743335781dc2794f
Linus Lundin <linus.lundin@gmail.com>
2012-07-07 08:54:06 +0000

    Fixed bug...
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@2044 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/EmbeddedSoftware/AVR/module/act_ks0108/act_ks0108.c
+++ trunk/EmbeddedSoftware/AVR/module/act_ks0108/act_ks0108.c
@@ -563,8 +563,8 @@ void act_ks0108_HandleMessage(StdCan_Msg_t *rxMsg)
 					OCR0A = rxMsg->Data[0];
 				    #endif
 			            #if GRAPHICS_DRIVER==DOTMATRIX
-					TCCR0B &= ~(1<<COM0B0);
-					TCCR0B &= ~(1<<COM0B1);
+					TCCR0A &= ~(1<<COM0B0);
+					TCCR0A &= ~(1<<COM0B1);
 					PORTD |= (1<<PD5);
 					OCR0B = 0xff-rxMsg->Data[0];
 				    #endif
@@ -574,7 +574,7 @@ void act_ks0108_HandleMessage(StdCan_Msg_t *rxMsg)
 					OCR0A = rxMsg->Data[0];
 				    #endif
 			            #if GRAPHICS_DRIVER==DOTMATRIX
-					TCCR0B |= (1<<COM0B1)|(1<<WGM01)|(1<<WGM00);
+					TCCR0A |= (1<<COM0B1)|(1<<WGM01)|(1<<WGM00);
 					OCR0B = 0xff-rxMsg->Data[0];
 				    #endif
 				}
