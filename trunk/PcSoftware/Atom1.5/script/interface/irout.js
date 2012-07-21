99b8e8ceb536f3b2294caa550281b26d58d0ba97
Linus Lundin <linus.lundin@gmail.com>
2012-07-21 17:27:48 +0000

    Added dimmer function to irout.js
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@2123 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- trunk/PcSoftware/Atom1.5/script/interface/irout.js
+++ trunk/PcSoftware/Atom1.5/script/interface/irout.js
@@ -158,6 +158,102 @@ function IROut_SendBurst_Raw(alias_name, protocol, data)
 }
 Console_RegisterCommand(IROut_SendBurst_Raw, function(arg_index, args) { return Console_StandardAutocomplete(arg_index, args, IROut_Aliases(), IROut_Protocols()); });
 
+function IROut_SendDimmer(alias_name, remote_name, button_name, level)
+{
+	if (arguments.length < 4)
+	{
+		Log("\033[31mNot enough parameters given.\033[0m\n");
+		return false;
+	}
+	
+	var remote_storage_name = Storage_GetParameter("RemoteList", remote_name);
+	
+	if (!remote_storage_name)
+	{
+		Log("\033[31mNo such remote name in list, " + remote_name + ".\033[0m\n");
+		return false;
+	}
+	
+	var button_string = Storage_GetParameter(remote_storage_name, button_name);
+	
+	if (!button_string)
+	{
+		Log("\033[31mNo such button " + button_name + " found on remote " + remote + ".\033[0m\n");
+		return false;
+	}
+	
+	var button_data = eval("(" + button_string + ")");
+	
+	if (typeof button_data["protocol"] == 'undefined' || typeof button_data["data"] == 'undefined')
+	{
+		Log("\033[31mButton data is corrupt.\033[0m\n");
+		return false;
+	}	
+	levelInt = parseInt(level);
+	dataInt = parseInt(button_data["data"]);
+	if (levelInt  >= 0 && levelInt <= 15) {
+		switch(levelInt) {
+		  case 0:
+		    levelInt = 15;
+		    break;
+		  case 1:
+		    levelInt = 7;
+		    break;
+		  case 2:
+		    levelInt = 11;
+		    break;
+		  case 3:
+		    levelInt = 3;
+		    break;
+		  case 4:
+		    levelInt = 13;
+		    break;
+		  case 5:
+		    levelInt = 5;
+		    break;
+		  case 6:
+		    levelInt = 9;
+		    break;
+		  case 7:
+		    levelInt = 1;
+		    break;
+		  case 8:
+		    levelInt = 14;
+		    break;
+		  case 9:
+		    levelInt = 6;
+		    break;
+		  case 10:
+		    levelInt = 10;
+		    break;
+		  case 11:
+		    levelInt = 2;
+		    break;
+		  case 12:
+		    levelInt = 12;
+		    break;
+		  case 13:
+		    levelInt = 4;
+		    break;
+		  case 14:
+		    levelInt = 8;
+		    break;
+		  case 15:
+		    levelInt = 0;
+		    break;
+		}
+		dataInt += 549755813888; //Set MSB
+		dataInt += levelInt*4294967296//Add dimmer level
+		return IROut_Send_Raw(alias_name, button_data["protocol"], dataInt.toString(), "Burst");	
+	} else {
+		Log("\033[31mDimmer level out of range [0 -> 16].\033[0m\n");
+		return false;
+	}
+}
+Console_RegisterCommand(IROut_SendDimmer, IROut_StartSendAutoComplete);
+
+
+
 function IROut_SendDuration(alias_name, remote_name, button_name, duration_ms)
 {
 	IROut_Send(alias_name, remote_name, button_name, "Pressed");
