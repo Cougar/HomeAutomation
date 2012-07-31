a6389795f80ffa556252942f0354173c1cc38621
Anders Runeson <runeson@gmail.com>
2012-07-31 18:52:11 +0000

    Added web interface page for irdevicestate
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@2144 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/PcSoftware/scripts/Atom1.5-webgui/pages/irdevicestate/script.js
@@ -0,0 +1,60 @@
+pages.irdevicestate = {
+  init: function(pageInstance)
+  {
+    /* List to keep referenses to all the different switch elements */
+    pageInstance.pageSwitchElements = {};
+
+
+    /* Function to update values of switches based on new server data */
+    function handleServerData(jsonData)
+    {
+      /* Loop through all results */
+      jQuery.each(jsonData, function(device, data)
+      {
+    //$("#debug").html($("#debug").html()+"Got data "+device+" "+data+"<br />\n");
+        /* Only do something if we know the device and have a value */
+        if (pageInstance.pageSwitchElements[device] && data)
+        {
+          /* Enable the switch if it was disabled by default */
+          pageInstance.pageSwitchElements[device].find("select").slider("enable");
+          /* Update the slider with the new value */
+          pageInstance.pageSwitchElements[device].find("select").val(data).slider("refresh");
+        }
+      });
+    }
+    
+    
+    /* Function to request new values from the server */
+    function requestServerData()
+    {
+      sendCommand("JSON.stringify(IrDeviceState_GetDataNative('" + pageInstance.devices.join("','") + "'));", handleServerData);
+    }
+    
+    
+    /* Loop through all the devices and create switches */
+    jQuery.each(pageInstance.devices, function(n, device)
+    {
+      var name = device;
+
+      if (aliasNames[device])
+      {
+        name = aliasNames[device];
+      }
+      
+      /* Create the slider HTML from the template and add it to the DOM */
+      pageInstance.pageSwitchElements[device] = $("#page-irdevicestate-template").tmpl({ device: device, name: name }).appendTo(pageInstance.pageContentElement).trigger("create");
+      
+      /* Before we have any value it should be disabled */
+      pageInstance.pageSwitchElements[device].find("select").slider("disable");
+    });
+    
+    
+    /* Request the initial value from the server */
+    requestServerData();
+    
+    
+    /* Set timer for continious polling of the current values */
+    pageInstance.pollTimer = setInterval(requestServerData, 4000);
+  }
+};
+
