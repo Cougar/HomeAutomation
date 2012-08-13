dce034720504687d85f7bf2c03fa7bda3ba896bf
Anders Runeson <runeson@gmail.com>
2012-08-13 22:20:55 +0000

    Added module for measuring water flow on flow meter with gearwheel
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@2167 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ EmbeddedSoftware/AVR/module/sns_water/sns_water.h
@@ -0,0 +1,44 @@
+#ifndef SNS_WATER
+#define SNS_WATER
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+/* system files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+/* lib files */
+#include <config.h>
+#include <bios.h>
+#include <drivers/can/stdcan.h>
+#include <drivers/timer/timer.h>
+#include <drivers/mcu/gpio.h>
+#include <drivers/adc/adc.h>
+#include <drivers/mcu/pcint.h>
+
+void sns_water_Init(void);
+void sns_water_Process(void);
+void sns_water_HandleMessage(StdCan_Msg_t *rxMsg);
+void sns_water_List(uint8_t ModuleSequenceNumber);
+
+
+#ifndef sns_water_USEEEPROM
+#define sns_water_USEEEPROM 0
+#endif
+
+#define HIGH		1
+#define LOW			2
+//#define NOCHANGE 	0
+//#define HIGH_NOCH	3
+//#define LOW_NOCH	4
+
+#if sns_water_USEEEPROM==1
+	struct sns_water_Data{
+		///TODO: Define EEPROM variables needed by the module
+		uint8_t x;
+		uint16_t y;
+	};	
+#endif
+
+#endif // SNS_WATER
