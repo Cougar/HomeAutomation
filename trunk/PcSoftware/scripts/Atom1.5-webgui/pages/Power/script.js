b32aed679f1cc5d0c34b3bb91decabcbeae332d4
Linus Lundin <linus.lundin@gmail.com>
2012-08-27 16:05:05 +0000

    Created Power page for webgui
    git-svn-id: https://svn.arune.se/svn/HomeAutomation@2195 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ trunk/PcSoftware/scripts/Atom1.5-webgui/pages/Power/script.js
@@ -0,0 +1,79 @@
+
+pages.Power = {
+  init: function(pageInstance)
+  {
+    this.pageshow = function(arg) {
+	/* Request the initial value from the server */
+      requestServerData();
+      /* Set timer for continious polling of the current values */
+      pageInstance.pollTimer = setInterval(reloadImage, 60000);
+      pageInstance.pollTimer2 = setInterval(requestServerData, 2000);
+    }
+    
+    this.pagehide = function(arg) {
+      clearInterval(pageInstance.pollTimer);
+      clearInterval(pageInstance.pollTimer2);
+    }
+    
+    
+    /* List to keep referenses to all the different slider elements */
+    pageInstance.pagePowerElements = {};
+    
+    /* Function to update values based on new server data */
+    function handleServerData(jsonData)
+    {
+      var g1,g2,g3;
+      /* Loop through all results */
+      jQuery.each(jsonData, function(alias, data)
+      {
+	  /* Loop through all values */
+	  jQuery.each(data, function(name, value)
+	  {
+	    if (name == "Power") 
+	    {
+		g1 = value.value;
+	    }
+	    if (name == "Power32") 
+	    {
+		g2 = value.value;
+	    }
+	    if (name == "EnergySum") 
+	    {
+		g3 = value.value;
+	    }
+	  });
+      });
+      pageInstance.pageInputElements["grid"].html($("#page-Power-grid-template").tmpl({ grid1: "Power:  "+g1, grid2: "Power32:  "+g2,grid3: "EnergySum:  "+g3}));
+    }
+        
+    /* Function to request new values from the server */
+    function requestServerData()
+    {
+      sendCommand("JSON.stringify(Module_GetLastDataNative('" + pageInstance.alias + "'));", handleServerData);
+    }
+    
+    function reloadImage()
+    {
+	pageInstance.pageImageElements["bild"].find("img").attr('src', pageInstance.picture + '?' + Math.random());
+    }
+    
+    var name = pageInstance.alias;
+    
+    if (aliasNames[pageInstance.alias])
+    {
+      name = aliasNames[pageInstance.alias];
+    }
+    
+    /* Create the Power HTML from the template and add it to the DOM */
+   
+    pageInstance.pageInputElements = {};
+    pageInstance.pageImageElements = {};
+    
+    
+    pageInstance.pageImageElements["bild"] = $("#page-Power-image-template").tmpl({url: pageInstance.picture}).appendTo(pageInstance.pageContentElement).trigger("create");
+    
+    pageInstance.pageInputElements["grid"] = $("#page-Power-grid-template").tmpl({ grid1: "Power:  ", grid2: "Power32:  ",grid3: "EnergySum:  "}).appendTo(pageInstance.pageContentElement).trigger("create");
+    
+  }
+ };
+  
