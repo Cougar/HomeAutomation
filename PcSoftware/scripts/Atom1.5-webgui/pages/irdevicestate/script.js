07e00e111245ab173b1ba5e45b5ded16a0daf2b1
Anders Runeson <runeson@gmail.com>
2012-09-02 20:54:49 +0000

    Changed devicestate web interface to work with devicestate in sensor.js
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@2208 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/scripts/Atom1.5-webgui/pages/irdevicestate/script.js
+++ PcSoftware/scripts/Atom1.5-webgui/pages/irdevicestate/script.js
@@ -19,19 +19,19 @@ pages.irdevicestate = {
     function handleServerData(jsonData)
     {
       /* Loop through all results */
-      jQuery.each(jsonData, function(device, data)
+      jQuery.each(jsonData, function(alias, data)
       {
         /* Only do something if we know the device and have a value */
-        if (pageInstance.pageSwitchElements[device] && data.state  && data.timestamp)
+        if (pageInstance.pageSwitchElements[alias] && data.State && data.State.value  && data.State.timestamp)
         {
           /* Enable the switch if it was disabled by default */
-          pageInstance.pageSwitchElements[device].find("select").slider("enable");
+          pageInstance.pageSwitchElements[alias].find("select").slider("enable");
           /* Update the slider with the new value */
-          pageInstance.pageSwitchElements[device].find("select").val(data.state).slider("refresh");
+          pageInstance.pageSwitchElements[alias].find("select").val(data.State.value.toLowerCase()).slider("refresh");
           
-          var date = new Date(data.timestamp*1000);
+          var date = new Date(data.State.timestamp*1000);
           //pageInstance.pageSwitchElements[device].find(":jqmData(id=page-irdevice-timestamp-"+device+")").text(date.toDateString()+", "+date.toLocaleTimeString());
-          pageInstance.pageSwitchElements[device].find(":jqmData(id=page-irdevice-timestamp-"+device+")").text(jQuery.timeago(date));
+          pageInstance.pageSwitchElements[alias].find(":jqmData(id=page-irdevice-timestamp-"+alias+")").text(jQuery.timeago(date));
         }
       });
     }
@@ -40,25 +40,25 @@ pages.irdevicestate = {
     /* Function to request new values from the server */
     function requestServerData()
     {
-      sendCommand("JSON.stringify(IrDeviceState_GetDataNative('" + pageInstance.devices.join("','") + "'));", handleServerData);
+      sendCommand("JSON.stringify(Module_GetLastDataNative('" + pageInstance.aliases.join("','") + "'));", handleServerData);
     }
     
     
     /* Loop through all the devices and create switches */
-    jQuery.each(pageInstance.devices, function(n, device)
+    jQuery.each(pageInstance.aliases, function(n, alias)
     {
-      var name = device;
+      var name = alias;
 
-      if (aliasNames[device])
+      if (aliasNames[alias])
       {
-        name = aliasNames[device];
+        name = aliasNames[alias];
       }
       
       /* Create the slider HTML from the template and add it to the DOM */
-      pageInstance.pageSwitchElements[device] = $("#page-irdevicestate-template").tmpl({ device: device, name: name }).appendTo(pageInstance.pageContentElement).trigger("create");
+      pageInstance.pageSwitchElements[alias] = $("#page-irdevicestate-template").tmpl({ device: alias, name: name }).appendTo(pageInstance.pageContentElement).trigger("create");
       
       /* Before we have any value it should be disabled */
-      pageInstance.pageSwitchElements[device].find("select").slider("disable");
+      pageInstance.pageSwitchElements[alias].find("select").slider("disable");
     });
   }
 };
