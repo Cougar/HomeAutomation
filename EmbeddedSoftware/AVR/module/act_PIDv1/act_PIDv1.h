85e7cacae4e81034804b735ee7385d556ae98056
Linus Lundin <linus.lundin@gmail.com>
2014-01-09 20:42:57 +0000

    Added new PID module that uses the new driver, untested
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@2263 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

---
+++ EmbeddedSoftware/AVR/module/act_PIDv1/act_PIDv1.h
@@ -0,0 +1,49 @@
+#ifndef ACT_PIDV1
+#define ACT_PIDV1
+
+/*-----------------------------------------------------------------------------
+ * Includes
+ *---------------------------------------------------------------------------*/
+/* system files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+/* lib files */
+#include <config.h>
+#include <bios.h>
+#include <drivers/can/stdcan.h>
+#include <drivers/timer/timer.h>
+#include <drivers/mcu/gpio.h>
+#include <drivers/adc/adc.h>
+#include <drivers/mcu/pcint.h>
+
+void act_PIDv1_Init(void);
+void act_PIDv1_Process(void);
+void act_PIDv1_HandleMessage(StdCan_Msg_t *rxMsg);
+void act_PIDv1_List(uint8_t ModuleSequenceNumber);
+
+
+#ifndef act_PIDv1_USEEEPROM
+#define act_PIDv1_USEEEPROM 0
+#endif
+
+#if act_PIDv1_USEEEPROM==1
+	struct act_PIDv1_Data{
+		///TODO: Define EEPROM variables needed by the module
+		float referenceValue;
+		uint8_t sensorModuleType;
+		uint8_t sensorModuleId;
+		uint8_t sensorId;
+		float K_P;
+		float K_I;
+		float K_D;
+		uint8_t TimeMsOrS;
+		uint16_t Time;
+		uint8_t actuatorModuleType;
+		uint8_t actuatorModuleId;
+		uint8_t actuatorId;
+		uint8_t sendPeriod;
+	};	
+#endif
+
+#endif // ACT_PIDV1
