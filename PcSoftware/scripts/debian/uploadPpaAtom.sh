f863bf06482589f2556c61e6843e609d11803198
Anders Runeson <runeson@gmail.com>
2014-05-30 05:03:33 +0000

    Fixed copy paste typo. Updated ppa-upload-script with trusty etc
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@2293 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/scripts/debian/uploadPpaAtom.sh
+++ PcSoftware/scripts/debian/uploadPpaAtom.sh
@@ -50,6 +50,18 @@ case $DIST in
     DEPS="libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libboost-thread1.49.0, libboost-system1.49.0, libboost-signals1.49.0, libboost-filesystem1.49.0, libboost-date-time1.49.0, libboost-program-options1.49.0, libv8-3.8.9.20, libmysqlclient18"
     BDEPS="debhelper (>= 7.0.50~), cmake, libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libv8-dev (>= 2.2.18), libboost-date-time1.49-dev, libboost-filesystem1.49-dev, libboost-program-options1.49-dev, libboost-signals1.49-dev, libboost-thread1.49-dev, libreadline-dev, libx11-dev (>= 2:1.3), libmysqlclient-dev"
     ;;
+  raring)
+    DEPS="libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libboost-thread1.53.0, libboost-system1.53.0, libboost-signals1.53.0, libboost-filesystem1.53.0, libboost-date-time1.53.0, libboost-program-options1.53.0, libv8-3.8.9.20, libmysqlclient18"
+    BDEPS="debhelper (>= 7.0.50~), cmake, libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libv8-dev (>= 2.2.18), libboost-date-time1.53-dev, libboost-filesystem1.53-dev, libboost-program-options1.53-dev, libboost-signals1.53-dev, libboost-thread1.53-dev, libreadline-dev, libx11-dev (>= 2:1.3), libmysqlclient-dev"
+    ;;
+  saucy)
+    DEPS="libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libboost-thread1.54.0, libboost-system1.54.0, libboost-signals1.54.0, libboost-filesystem1.54.0, libboost-date-time1.54.0, libboost-program-options1.54.0, libv8-3.14.5, libmysqlclient18"
+    BDEPS="debhelper (>= 7.0.50~), cmake, libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libv8-dev (>= 2.2.18), libboost-date-time1.54-dev, libboost-filesystem1.54-dev, libboost-program-options1.54-dev, libboost-signals1.54-dev, libboost-thread1.54-dev, libreadline-dev, libx11-dev (>= 2:1.3), libmysqlclient-dev"
+    ;;
+  trusty)
+    DEPS="libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libboost-thread1.54.0, libboost-system1.54.0, libboost-signals1.54.0, libboost-filesystem1.54.0, libboost-date-time1.54.0, libboost-program-options1.54.0, libv8-3.14.5, libmysqlclient18"
+    BDEPS="debhelper (>= 7.0.50~), cmake, libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libv8-dev (>= 2.2.18), libboost-date-time1.54-dev, libboost-filesystem1.54-dev, libboost-program-options1.54-dev, libboost-signals1.54-dev, libboost-thread1.54-dev, libreadline-dev, libx11-dev (>= 2:1.3), libmysqlclient-dev"
+    ;;
 esac
 
 # Run script from cron every hour
@@ -64,7 +76,8 @@ date
 #rm $REVFILE
 
 # Check if script is already running
-if [ -e $TMPFILE ]; then
+if [ -e $TMPFILE ]
+then
   echo "Error: Already running?"
   exit 1
 fi
@@ -100,30 +113,35 @@ touch $TMPFILE
 
 mkdir $ATOMDIR
 
+echo "******** Exporting SVN ********"
 # Export atom to get rid of .svn subfolders
 svn export $SVNURL $ATOMDIR/$NOSVNDIR
 # Export configuration folder into the exported Atom-folder
 svn export $SVNURLCNF $ATOMDIR/$NOSVNDIR/$CNFDIR
 
+echo "******** Creating debian dir ********"
 mkdir -p $ATOMDIR/$NOSVNDIR/debian
 
+echo "******** Adding SVN revision file ********"
 # Add a svn revision file, atom need the svn revision when building
 echo -n $CURRREV > $ATOMDIR/$NOSVNDIR/AtomSvnRev
 
 # Change install-target for data.xml file in CMakeList.txt, not needed, done in CMakeList.txt
 #cat $ATOMDIR/$NOSVNDIR/CMakeLists.txt | sed -e 's/\/..\/..\/Configuration\/data.xml/\/Configuration\/data.xml/' > $ATOMDIR/$NOSVNDIR/CMakeLists.txt
 
-
+echo "******** Copying debian files ********"
 cp $RUNDIR/files/atom.conf $ATOMDIR/$NOSVNDIR/debian/
 cp $RUNDIR/files/compat $ATOMDIR/$NOSVNDIR/debian/
 cp $RUNDIR/files/copyright $ATOMDIR/$NOSVNDIR/debian/
 cp $RUNDIR/files/rules $ATOMDIR/$NOSVNDIR/debian/
 cp $RUNDIR/files/control $ATOMDIR/$NOSVNDIR/debian/
 
+echo "******** Set the correct depends and build depends ********"
 # Set the correct depends and build depends, different for different distributions
 sed -i "s/^Build-Depends/Build-Depends: ${BDEPS}/g" $ATOMDIR/$NOSVNDIR/debian/control
 sed -i "s/^Depends/Depends: ${DEPS}/g" $ATOMDIR/$NOSVNDIR/debian/control
 
+echo "******** Create debian changelog from SVN log ********"
 # Create the debian changelog file from svn log
 echo "$PKGNAME ($VERSION) $DIST; urgency=low" > $ATOMDIR/$NOSVNDIR/debian/changelog
 echo "" >> $ATOMDIR/$NOSVNDIR/debian/changelog
@@ -145,15 +163,17 @@ IFS="$OLDIFS"
 echo "" >> $ATOMDIR/$NOSVNDIR/debian/changelog
 echo " -- $PKGMAINT  $CHLOGDATE" >> $ATOMDIR/$NOSVNDIR/debian/changelog
 
-
+echo "******** Create orig tar.gz ********"
 # Create orig tar gz
 cd $ATOMDIR
 tar -zcf $ORIGTAR $NOSVNDIR
 
+echo "******** Create source package with debuild ********"
 # Create source package with debuild
 cd $ATOMDIR/$NOSVNDIR
 debuild $DEBUILDFLG
 
+echo "******** Upload to launchpad with dput ********"
 # Upload to launchpad with dput
 cd $ATOMDIR
 dput $DPUTFLG
@@ -163,6 +183,7 @@ dput $DPUTFLG
 # Remove all temporary files
 #rm -rf $ATOMDIR
 
+echo "******** Write current SVN rev to file ********"
 # Write the current svn revision to file
 echo -n $CURRREV > $REVFILE
 
