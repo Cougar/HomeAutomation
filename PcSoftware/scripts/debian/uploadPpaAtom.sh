7b8e3090755d7161912639aaf9adca2767feaf8d
Anders Runeson <runeson@gmail.com>
2016-06-06 19:50:36 +0000

    Fixed problem with dependency for mysqlclient in xenial
    git-svn-id: http://svn.arune.se/svn/HomeAutomation/trunk@2304 e69ec2e4-db17-0410-bd4a-d6f28f89cd0d

--- PcSoftware/scripts/debian/uploadPpaAtom.sh
+++ PcSoftware/scripts/debian/uploadPpaAtom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -lt 1 ] 
+if [ $# -lt 1 ]
 then
   echo "Error: need to run script with ubuntu releasename as argument"
   exit 1
@@ -67,7 +67,7 @@ case $DIST in
     BDEPS="debhelper (>= 7.0.50~), cmake, libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libv8-dev (>= 2.2.18), libboost-date-time1.55-dev, libboost-filesystem1.55-dev, libboost-program-options1.55-dev, libboost-signals1.55-dev, libboost-thread1.55-dev, libreadline-dev, libx11-dev (>= 2:1.3), libmysqlclient-dev"
     ;;
   xenial)
-    DEPS="libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libboost-thread1.58.0, libboost-system1.58.0, libboost-signals1.58.0, libboost-filesystem1.58.0, libboost-date-time1.58.0, libboost-program-options1.58.0, libv8-3.14.5, libmysqlclient18, libx11-6"
+    DEPS="libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libboost-thread1.58.0, libboost-system1.58.0, libboost-signals1.58.0, libboost-filesystem1.58.0, libboost-date-time1.58.0, libboost-program-options1.58.0, libv8-3.14.5, libmysqlclient20, libx11-6"
     BDEPS="debhelper (>= 7.0.50~), cmake, libc6 (>= 2.3.6), libgcc1 (>= 1:4.1), libv8-dev (>= 2.2.18), libboost-date-time1.58-dev, libboost-filesystem1.58-dev, libboost-program-options1.58-dev, libboost-signals1.58-dev, libboost-thread1.58-dev, libreadline-dev, libx11-dev (>= 2:1.3), libmysqlclient-dev"
     ;;
 esac
